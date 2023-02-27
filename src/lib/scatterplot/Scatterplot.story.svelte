c61a88ef89f4c52238aa8d9289b3e9728ad7e4bc
Chris Knight <chris.knight@london.gov.uk>
2023-02-27 15:45:59 +0000

    build: add story for scatterplot component

---
+++ src/lib/scatterplot/Scatterplot.story.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import { data } from '../../data/testData';
+	import Scatterplot from './Scatterplot.svelte';
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="ldn-viz/Scatterplot">
+	<Hst.Variant title="default"><Scatterplot {data} xKey="year" yKey="value" /></Hst.Variant>
+	<Hst.Variant title="with title"
+		><Scatterplot {data} xKey="year" yKey="value" title="Chart Title" /></Hst.Variant
+	>
+</Hst.Story>
