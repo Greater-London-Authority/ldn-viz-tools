4447de736a3dac12bfe4296bb10adbc0b763ffef
Chris Knight <chris.knight@london.gov.uk>
2023-02-27 15:44:13 +0000

    build: custom color and logo for histoire

--- histoire.config.ts
+++ histoire.config.ts
@@ -3,5 +3,26 @@ import { defineConfig } from 'histoire';
 
 export default defineConfig({
 	plugins: [HstSvelte()],
-	setupFile: '/src/histoire.setup.ts'
+	setupFile: '/src/histoire.setup.ts',
+	theme: {
+		colors: {
+			primary: {
+				50: '#EEF5FB',
+				100: '#E2EEF8',
+				200: '#C5DCF2',
+				300: '#A8CBEB',
+				400: '#8BB9E5',
+				500: '#6DA7DE',
+				600: '#3787D2',
+				700: '#2566A2',
+				800: '#18446C',
+				900: '#0C2236'
+			}
+		},
+		logo: {
+			// square: './static/square.png',
+			light: './static/ciu-logo-light.svg',
+			dark: './static/ciu-logo-light.svg'
+		}
+	}
 });
