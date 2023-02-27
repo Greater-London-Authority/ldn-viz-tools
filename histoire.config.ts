86dcf835b492c9f6430dda7dc8993970cdb4e681
Chris Knight <chris.knight@london.gov.uk>
2023-02-27 11:18:49 +0000

    test: add historie

---
+++ histoire.config.ts
@@ -0,0 +1,7 @@
+import { HstSvelte } from '@histoire/plugin-svelte';
+import { defineConfig } from 'histoire';
+
+export default defineConfig({
+	plugins: [HstSvelte()],
+	setupFile: '/src/histoire.setup.ts'
+});
---
+++ src/lib/button/Button.story.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import Button from './Button.svelte';
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="ldn-viz/Button">
+	<!-- Typed! -->
+	<Hst.Variant title="default"><Button primary>Primary button</Button></Hst.Variant>
+	<Hst.Variant title="Fast"><Button>Seconday Button</Button></Hst.Variant>
+</Hst.Story>
