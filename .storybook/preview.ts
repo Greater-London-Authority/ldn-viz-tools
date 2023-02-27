1b5f445b9a7c4914ad107b7cb9479168c614d35e
Chris Knight <chris.knight@london.gov.uk>
2023-02-27 10:59:57 +0000

    test: add storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from '@storybook/svelte';
+import '../src/app.postcss';
+const preview: Preview = {
+	parameters: {
+		backgrounds: {
+			default: 'light'
+		},
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/
+			}
+		}
+	}
+};
+
+export default preview;
