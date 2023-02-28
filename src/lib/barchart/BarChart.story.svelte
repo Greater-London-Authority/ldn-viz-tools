a9d852a1f27e7bdf9b61949f1ee1347c97439334
Chris Knight <chris.knight@london.gov.uk>
2023-02-28 10:18:15 +0000

    build: add story for BarChart

---
+++ src/lib/barchart/BarChart.story.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import { data } from '../../data/testData';
+	import BarChart from './BarChart.svelte';
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="ldn-viz/BarChart">
+	<Hst.Variant title="default"><BarChart {data} xKey="year" yKey="value" /></Hst.Variant>
+	<Hst.Variant title="with title"
+		><BarChart {data} xKey="year" yKey="value" title="Chart Title" /></Hst.Variant
+	>
+</Hst.Story>
