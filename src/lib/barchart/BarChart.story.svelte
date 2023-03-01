4b5d4347291343392ff34e13d4949dcf510f2a2a
Chris Knight <chris.knight@london.gov.uk>
2023-03-01 09:22:55 +0000

    test: add alt text example

--- src/lib/barchart/BarChart.story.svelte
+++ src/lib/barchart/BarChart.story.svelte
@@ -6,8 +6,19 @@
 </script>
 
 <Hst.Story title="Charts/BarChart">
-	<Hst.Variant title="default"><BarChart {data} xKey="year" yKey="value" /></Hst.Variant>
-	<Hst.Variant title="with title"
-		><BarChart {data} xKey="year" yKey="value" title="Chart Title" /></Hst.Variant
-	>
+	<Hst.Variant title="default">
+		<BarChart {data} xKey="year" yKey="value" />
+	</Hst.Variant>
+	<Hst.Variant title="with title">
+		<BarChart {data} xKey="year" yKey="value" title="Chart Title" />
+	</Hst.Variant>
+	<Hst.Variant title="with alt text">
+		<BarChart
+			{data}
+			xKey="year"
+			yKey="value"
+			title="Chart Title"
+			alt="This is a alt text description"
+		/>
+	</Hst.Variant>
 </Hst.Story>
