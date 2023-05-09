1feeca54a8b6acf0adb3ecfc754e2e3cdd476aa7
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-05-09 12:27:09 +0100

    add first version of modal component

---
+++ packages/ui/src/lib/modal/Modal.stories.svelte
@@ -0,0 +1,37 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+
+    import Modal from "./Modal.svelte";
+
+    let isOpen;
+</script>
+
+<Meta title="Ui/Modal" component={Modal}/>
+
+<Template let:args>
+    <Modal title="My title" description="DEscription...">
+        Foo!
+    </Modal>
+</Template>
+
+<Story name="Description only">
+    <button class="btn bg-core-grey-500 p-4" on:click={() => isOpen = true}>Open modal!</button>
+
+    <Modal bind:isOpen title="A modal with contents!" description="This modal has a description, but no contents!" />
+</Story>
+
+
+<Story name="With Contents">
+    <button class="btn bg-core-grey-500 p-4" on:click={() => isOpen = true}>Open modal!</button>
+
+    <Modal bind:isOpen title="A modal with contents!" description="This modal has some contents!">
+
+        <p>A list</p>
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+        </ul>
+    </Modal>
+</Story>
+
