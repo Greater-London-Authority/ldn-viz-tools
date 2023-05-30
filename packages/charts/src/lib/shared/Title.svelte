6e511ed7ef5b2ad093402bc136a1a3494d86215a
Chris Knight <chris.knight@london.gov.uk>
2023-05-30 17:54:36 +0100

    dev: import button from ui package to charts

--- packages/charts/src/lib/shared/Title.svelte
+++ packages/charts/src/lib/shared/Title.svelte
@@ -1,7 +1,1 @@
-<div class="title"><slot /></div>
-
-<style>
-	.title {
-		@apply font-bold mb-4 text-xl text-core-grey-600;
-	}
-</style>
+<div class="font-bold mb-4 text-xl text-core-grey-600 dark:text-white"><slot /></div>
