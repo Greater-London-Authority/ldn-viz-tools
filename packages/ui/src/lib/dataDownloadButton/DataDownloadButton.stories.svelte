20bad969c20bf72ffb3d7a8665df27e68d364208
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-07 14:36:39 +0100

    add a data downlaod button

---
+++ packages/ui/src/lib/dataDownloadButton/DataDownloadButton.stories.svelte
@@ -0,0 +1,35 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+
+    import DataDownloadButton from "./DataDownloadButton.svelte";
+
+    const data = [
+        {A: 1, B: 2, C: 3},
+        {A: 4, B: 5, C: 6},
+        {A: 7, B: 8, C: 9}
+    ];
+</script>
+
+<Meta title="Ui/DataDownloadButton" component={DataDownloadButton}/>
+
+<Template let:args>
+    <DataDownloadButton {...args}>
+        I'm a button from UI!
+    </DataDownloadButton>
+</Template>
+
+<Story name="Default">
+    <DataDownloadButton {data}/>
+</Story>
+
+<Story name="Download as JSON">
+    <DataDownloadButton {data} format="JSON"/>
+</Story>
+
+<Story name="Label on button and custom data">
+    <DataDownloadButton {data} label="Download!" tooltip="Download this - it's very important!"/>
+</Story>
+
+<Story name="Disabled">
+    <DataDownloadButton {data} disabled={true}/>
+</Story>
---
+++ packages/ui/src/lib/dataDownloadButton/DataDownloadButton.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+
+
+  import { DocumentDownloadIcon } from "@rgossiaux/svelte-heroicons/outline";
+
+    import {csvFormat} from "d3-dsv";
+
+    export let format = "CSV" | "JSON" | undefined;
+    export let label = "";
+    export let title = "Download data";
+    export let data;
+    export let filename: string;
+
+    export let disabled = false;
+
+    const downloadFromURL = (url, name) => {
+        const link = document.createElement("a");
+        link.setAttribute("href", url);
+        link.setAttribute("target", "_blank");
+        link.setAttribute("download", name);
+        link.dispatchEvent(new MouseEvent("click"));
+    };
+
+    const downloadJSON = () => {
+        const dataString = JSON.stringify(data, null, 4);
+        const dataURL = "data:application/json;base64," + window.btoa(dataString);
+        downloadFromURL(dataURL, filename || `data.json`);
+    }
+
+    const downloadCSV = () => {
+        const dataString = csvFormat(data);
+        const dataURL = "data:application/csv;base64," + window.btoa(dataString);
+        downloadFromURL(dataURL, filename || `data.csv`);
+    }
+
+    const download = (format === "JSON") ? downloadJSON : downloadCSV;
+</script>
+
+<button {title} on:click={download} {disabled} class="w-fit border border-core-grey-700 disabled:bg-core-grey-300">
+    {label}<DocumentDownloadIcon class="color-gray-900 w-4 h-4 inline" />
+</button>
