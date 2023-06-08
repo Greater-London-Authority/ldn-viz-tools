13c014e9f42d293426db626e81c570258c124981
Chris Knight <chris.knight@london.gov.uk>
2023-06-08 12:07:07 +0100

    build: add button component

--- packages/ui/src/lib/button/Button.stories.svelte
+++ packages/ui/src/lib/button/Button.stories.svelte
@@ -1,16 +1,47 @@
 <script>
-    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+	import { ArrowCircleDownIcon } from '@rgossiaux/svelte-heroicons/solid';
 
-    import Button from "./Button.svelte";
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
 
+	import Button from './Button.svelte';
 </script>
 
-<Meta title="Ui/Button" component={Button}/>
+<Meta title="Ui/Button" component={Button} />
 
 <Template let:args>
-    <Button {...args}>
-        I'm a button from UI!
-    </Button>
+	<Button {...args}>I'm a button from UI!</Button>
 </Template>
 
 <Story name="Default" />
+
+<Story name="Primary">
+	<Button style="primary">Primary Style</Button>
+</Story>
+
+<Story name="Secondary">
+	<Button style="secondary">Secondary Style</Button>
+</Story>
+
+<Story name="Ghost">
+	<Button style="ghost">Ghost Style</Button>
+</Story>
+
+<Story name="Disabled" args={{ disabled: true }} />
+
+<Story name="Sizes">
+	<Button size="xs">xs</Button>
+	<Button size="sm">sm</Button>
+	<Button size="base">base</Button>
+	<Button size="lg">lg</Button>
+	<Button size="xl">xl</Button>
+</Story>
+
+<Story name="With Icon">
+	<Button>Icon via slot<ArrowCircleDownIcon class="ml-2 w-6 h-6" aria-hidden="true" /></Button>
+</Story>
+
+<Story name="With class prop">
+	<Button class="text-core-darkPink-500 border-core-pink-700 bg-yellow-500"
+		>Custom classes applied</Button
+	>
+</Story>
--- packages/ui/src/lib/button/Button.svelte
+++ packages/ui/src/lib/button/Button.svelte
@@ -1,3 +1,49 @@
-<button class="btn bg-core-blue-600 p-4">
-    <slot></slot>
-</button>
+<script lang="ts">
+	import classNames from 'classnames';
+
+	export let style: 'primary' | 'secondary' | 'ghost' = 'primary';
+	export let size: 'xs' | 'sm' | 'base' | 'lg' | 'xl' = 'base';
+	export let href: string | undefined = undefined;
+	export let type: 'button' | 'submit' = 'button';
+
+	const styleClasses = {
+		primary: 'bg-core-grey-500 border-b-4 border-core-blue-600 text-white hover:bg-core-grey-400',
+		secondary: 'bg-white border-b-4 border-core-blue-600 hover:bg-core-grey-100',
+		ghost: 'bg-transparent hover:bg-core-grey-100 dark:text-white dark:hover:bg-core-grey-400'
+	};
+
+	const sizeClasses = {
+		xs: 'text-xs',
+		sm: 'text-sm',
+		base: 'text-base',
+		lg: 'text-lg',
+		xl: 'text-xl'
+	};
+
+	let buttonClass: string;
+
+	$: buttonClass = classNames(
+		'px-4 py-2 text-center inline-flex',
+		sizeClasses[size],
+		styleClasses[style],
+		$$props.disabled && 'cursor-not-allowed text-core-grey-300 border-core-grey-200 opacity-50',
+		$$props.class
+	);
+</script>
+
+<svelte:element
+	this={href ? 'a' : 'button'}
+	type={href ? undefined : type}
+	class={buttonClass}
+	on:click
+	on:change
+	on:keydown
+	on:keyup
+	on:touchstart
+	on:touchend
+	on:touchcancel
+	on:mouseenter
+	on:mouseleave
+>
+	<slot />
+</svelte:element>
