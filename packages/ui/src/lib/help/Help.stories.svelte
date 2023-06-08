f7dbf8e52dd1882225463ac472ed036eca202c44
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-08 12:03:50 +0100

    add initial implementation fo help component

---
+++ packages/ui/src/lib/help/Help.stories.svelte
@@ -0,0 +1,82 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+
+    import Help from "./Help.svelte";
+
+</script>
+
+<Meta title="Ui/Help" component={Help}/>
+
+<Template let:args>
+    <Help {...args}>
+        I'm a button from UI!
+    </Help>
+</Template>
+
+<Story name="Default">
+    <p>Mouse-over the icon to see the message.</p>
+
+    <Help>
+        This is some helpful text...
+    </Help>
+</Story>
+
+
+<Story name="Require click">
+    <p>Click the icon to see the message.</p>
+
+    <Help requireClick>
+        This is some helpful text...
+    </Help>
+
+</Story>
+
+<Story name="Longer content">
+    <Help>
+        <div class="space-y-2">
+            <h2 class="text-xl">
+                And now a more complex example
+            </h2>
+
+            <p>
+                <i>This</i> is a paragraph...
+            </p>
+
+            <p>Followed by a list:</p>
+            <ul class="list-disc">
+                <li>An important point</li>
+                <li>Something to remember</li>
+                <li>And something you mustn't forget!</li>
+            </ul>
+        </div>
+    </Help>
+
+</Story>
+
+
+<Story name="Multiple">
+
+    <p class="flex align-middle">Vehicle type:
+    <Help>
+        Select the type of vehicle you use</Help></p>
+
+        <p class="flex align-middle">Car
+            <Help>This has 4 wheels</Help>
+        </p>
+        <p class="flex align-middle">Bicycle
+            <Help>You cycle this</Help>
+        </p>
+        <p class="flex align-middle">Motorbike
+            <Help>Like a bicycle, but with an engine</Help>
+        </p>
+
+        <p class="flex align-middle">Scooter
+            <Help>Don't fall off!</Help>
+        </p>
+
+
+
+</Story>
+
+<style>
+</style>
---
+++ packages/ui/src/lib/help/Help.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+    import {InformationCircleIcon} from "@rgossiaux/svelte-heroicons/outline";
+    import {shift} from "svelte-floating-ui/dom";
+    import {createFloatingActions} from "svelte-floating-ui";
+
+    export let requireClick = false;
+
+    let iconRef;
+
+    const [floatingRef, floatingContent, update] = createFloatingActions({
+        strategy: "absolute",
+        placement: "bottom",
+        middleware: [
+            shift()
+        ],
+
+
+    });
+
+    let showTooltip = false;
+</script>
+
+<div
+        style="width: fit-content; height: fit-content; display: inline flex;"
+        on:mouseenter={() => (!requireClick && (showTooltip = true))}
+        on:mouseleave={() => (!requireClick && (showTooltip = false))}
+        on:click={() => showTooltip = !showTooltip}
+        bind:this={iconRef}
+        use:floatingRef
+>
+    <InformationCircleIcon style="height: 2em"/>
+
+    {#if showTooltip}
+        <div class="arrow"
+             style={`left: ${ iconRef.getBoundingClientRect().x + 5 }px; top: ${iconRef.getBoundingClientRect().y + 30}px`}/>
+    {/if}
+
+</div>
+
+
+{#if showTooltip}
+
+    <div style="position:absolute" use:floatingContent class="popover" on:click={() => showTooltip = false}>
+        <slot/>
+    </div>
+{/if}
+
+
+<style>
+    .arrow {
+        position: absolute;
+        width: 20px;
+        height: 20px;
+        background: white;
+
+        border-top: 1px solid gray;
+        border-left: 1px solid gray;
+
+        transform: rotate(45deg);
+    }
+
+    .popover {
+        border: 1px solid gray;
+        background: white;
+        padding: 1em;
+    }
+</style>
