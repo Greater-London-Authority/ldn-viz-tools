42cfba039d16e5befe4ea0d3c12dc71473605b91
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-08 10:55:43 +0100

    add component for downloading SVGs as images

---
+++ packages/ui/src/lib/imageDownloadButton/ImageDownloadButton.stories.svelte
@@ -0,0 +1,63 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+
+    import ImageDownloadButton from "./ImageDownloadButton.svelte";
+
+    let svgRef;
+    let svgRef2;
+    let svgRef3;
+    let svgRef4;
+</script>
+
+<Meta title="Ui/ImageDownloadButton" component={ImageDownloadButton}/>
+
+<Template let:args>
+    <ImageDownloadButton {...args}>
+        I'm a button from UI!
+    </ImageDownloadButton>
+</Template>
+
+<Story name="Default">
+    <svg bind:this={svgRef} width="300" height="300">
+        <rect x="0" y="0" width="100" height="100" fill="red"/>
+        <circle cx="10" cy="10" r="10" fill="blue"/>
+    </svg>
+
+    <ImageDownloadButton svgNode={svgRef}/>
+</Story>
+
+
+<Story name="Disabled">
+    <ImageDownloadButton svgNode={svgRef} disabled="true"/>
+</Story>
+
+
+<Story name="Set label, tooltip, filenmae">
+    <svg bind:this={svgRef4} width="300" height="300">
+        <rect x="0" y="0" width="100" height="100" fill="red"/>
+        <circle cx="10" cy="10" r="10" fill="blue"/>
+    </svg>
+
+    <ImageDownloadButton svgNode={svgRef4} label="Download" tooltip="Click to download!"
+                         filename="I was downloaded from Storybook.png"/>
+</Story>
+
+
+<Story name="Image with no explicit size">
+    <svg bind:this={svgRef2} width="300" height="300">
+        <rect x="0" y="0" width="100" height="100" fill="red"/>
+        <circle cx="10" cy="10" r="10" fill="blue"/>
+    </svg>
+
+    <ImageDownloadButton svgNode={svgRef2}/>
+</Story>
+
+
+<Story name="Download as an SVG">
+    <svg bind:this={svgRef3} width="300" height="300">
+        <rect x="0" y="0" width="100" height="100" fill="red"/>
+        <circle cx="10" cy="10" r="10" fill="blue"/>
+    </svg>
+
+    <ImageDownloadButton svgNode={svgRef3} format="SVG"/>
+</Story>
---
+++ packages/ui/src/lib/imageDownloadButton/ImageDownloadButton.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+    import {DocumentDownloadIcon} from "@rgossiaux/svelte-heroicons/outline";
+
+    export let svgNode;
+    export let scaleFactor = 2;
+    export let label = "";
+    export let title = "Download image";
+    export let filename;
+    export let disabled = false;
+    export let format: "PNG" | "SVG" | undefined;
+
+    const downloadFromURL = (url) => {
+        if (!filename){
+            filename = (format === "SVG") ? "Image.svg" : "Image.png";
+        }
+
+        const link = document.createElement("a");
+        link.setAttribute("href", url);
+        link.setAttribute("target", "_blank");
+        link.setAttribute("download", filename);
+        link.dispatchEvent(new MouseEvent("click"));
+    };
+
+    const createHTMLImageFromURL = async (url: string) => {
+        let img;
+
+        await new Promise(resolve => {
+            img = new Image();
+            img.onload = resolve;
+            img.src = url;
+        });
+
+        return img;
+    };
+
+    const download = async () => {
+
+        // This hack is necessary because .drawImage() doesn't work
+        // unless the SVG that is being copied has an explicitly-set size
+        const svgNodeClone = svgNode.cloneNode(true);
+        svgNodeClone.setAttribute("width", svgNode.clientWidth)
+        svgNodeClone.setAttribute("height", svgNode.clientHeight)
+
+        const svgString = new XMLSerializer().serializeToString(svgNodeClone);
+        const svgURL = 'data:image/svg+xml;base64,' + window.btoa(svgString);
+        // the non base-64 encoded URL would be: "data:image/svg+xml;utf8," + svgString;
+
+        // an alternative is:
+        // const svgURL = URL.createObjectURL(new Blob([svgNodeClone.outerHTML],{type:'image/svg+xml;charset=utf-8'}))
+
+        const img = await createHTMLImageFromURL(svgURL);
+
+        if (format === "SVG") {
+            downloadFromURL(svgURL);
+        } else {
+            img.src = svgURL;
+
+            const w = svgNode.clientWidth * scaleFactor;
+            const h = svgNode.clientHeight * scaleFactor;
+
+            const canvas = new OffscreenCanvas(w, h);
+            canvas.getContext("2d").drawImage(img, 0, 0, w, h);
+
+            canvas.convertToBlob().then((blob) => {
+                downloadFromURL(URL.createObjectURL(blob));
+            });
+        }
+    }
+</script>
+
+
+<button {title} on:click={download} {disabled}
+        class="flex items-center w-fit border border-core-grey-700 disabled:bg-core-grey-300">
+    {label}
+    <DocumentDownloadIcon class="color-gray-900 w-4 h-4 inline"/>
+</button>
