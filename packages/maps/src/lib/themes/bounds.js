bc4e6f8645f855c7274ab4fece6bf0abdaaf9dbe
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-12 13:04:19 +0100

    add alternative format of bounding box

--- packages/maps/src/lib/themes/bounds.js
+++ packages/maps/src/lib/themes/bounds.js
@@ -1,9 +1,13 @@
 export const GREATER_LONDON_ZOOM = 11;
 export const GREATER_LONDON_CENTER = [0.05, 51.5151];
+
 export const GREATER_LONDON_BOUNDS = [
 	[-0.489, 51.28],
 	[0.236, 51.686],
 ];
+
+// These coordinates are in the form needed by the option to MapboxGeocoder
+export const GREATER_LONDON_BOUNDS_FLAT = [-0.489, 51.28, 0.236, 51.686];
 
 export default {
 	GREATER_LONDON_ZOOM,
