d68f962a892411804dd1b6c44a620933e24bfa98
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-14 13:10:16 +0100

    a11y improvements

--- packages/ui/src/lib/sideIconSidebar/SideIconSidebar.svelte
+++ packages/ui/src/lib/sideIconSidebar/SideIconSidebar.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
     export let selected: string;
     export let tabs = []
+
+    	const keydownHandler = (ev: KeyboardEvent, val: string) => {
+		if (ev.key === 'Enter' || ev.key === ' ') {
+			selected = val;
+		}
+	};
+
+
 </script>
 
 
@@ -10,10 +18,15 @@
         {#each tabs as tab}
             <div
                     on:click={() => selected = tab.id}
+                    on:keydown={(ev) => keydownHandler(ev, tab.id)}
+                    tabindex="0"
+                    role="button"
+
+
                     class:bg-core-blue-500={tab.id===selected}
-                    class="w-20 h-20 flex flex-col items-center justify-center text-white text-base leading-4 text-center select-none hover:bg-core-blue-300 "
+                    class="w-20 h-20 flex flex-col items-center justify-center text-white text-base leading-4 text-center select-none hover:bg-core-blue-300"
             >
-                <svelte:component this={tab.icon} class="h-5 w-5" style="fill: white"/>
+                <svelte:component this={tab.icon} class="h-5 w-5" style="fill: white" aria-hidden="true" />
                 {tab.label}
             </div>
         {/each}
