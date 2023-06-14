3b0db1d08928cdc180c972790e96314ea346b06b
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-14 13:03:54 +0100

    initial commit of a sidebar with icons on the left

---
+++ packages/ui/src/lib/sideIconSidebar/SideIconSidebar.stories.svelte
@@ -0,0 +1,62 @@
+<script>
+    import {Meta, Story, Template} from '@storybook/addon-svelte-csf';
+
+    import SideIconSidebar from "./SideIconSidebar.svelte";
+    import {ChartBarIcon, FilterIcon, LocationMarkerIcon, MapIcon} from "@rgossiaux/svelte-heroicons/solid";
+
+    let selected = "filters";
+
+    let tabs = [
+        {id: "markers", label: "Data Markers", icon: LocationMarkerIcon},
+        {id: "filters", label: "Filters", icon: FilterIcon},
+        {id: "analysis", label: "Analysis", icon: ChartBarIcon},
+        {id: "layers", label: "Layers", icon: MapIcon}
+    ]
+
+</script>
+
+<Meta title="Ui/SideIconSidebar" component={SideIconSidebar}/>
+
+<Template let:args>
+    <SideIconSidebar {...args}/>
+</Template>
+
+<Story name="Default">
+    <SideIconSidebar {tabs} bind:selected>
+        <p class="text-white"> Selected tab: {selected} </p>
+    </SideIconSidebar>
+</Story>
+
+
+<Story name="With contents">
+
+    <p>
+        In actual use, would probably have a separate component for each pane of the tab, and render conditionally.
+    </p>
+
+    <SideIconSidebar {tabs} bind:selected>
+
+        {#if selected === "markers"}
+            <div class="text-white">Markers is selected, so we'd render a
+                <code> &lt;Marker /&gt;</code>
+                component
+            </div>
+        {:else if selected === "filters"}
+            <div class="text-white">Filter is selected, so we'd render a
+                <code> &lt;Filters /&gt;</code>
+                component
+            </div>
+        {:else if selected === "analysis"}
+            <div class="text-white">Analysis is selected, so we'd render a
+                <code> &lt;Analysis /&gt;</code>
+                component
+            </div>
+        {:else if selected === "layers"}
+            <div class="text-white">Layers is selected, so we'd render a
+                <code> &lt;Layer /&gt;</code>
+                component
+            </div>
+        {/if}
+
+    </SideIconSidebar>
+</Story>
---
+++ packages/ui/src/lib/sideIconSidebar/SideIconSidebar.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    export let selected: string;
+    export let tabs = []
+</script>
+
+
+<div id="sidebar" class="bg-core-grey-800 h-screen flex">
+
+    <div class="w-20 bg-core-grey-800 flex flex-col items-center">
+        {#each tabs as tab}
+            <div
+                    on:click={() => selected = tab.id}
+                    class:bg-core-blue-500={tab.id===selected}
+                    class="w-20 h-20 flex flex-col items-center justify-center text-white text-base leading-4 text-center select-none hover:bg-core-blue-300 "
+            >
+                <svelte:component this={tab.icon} class="h-5 w-5" style="fill: white"/>
+                {tab.label}
+            </div>
+        {/each}
+    </div>
+
+    <div class="p-6">
+        <slot/>
+    </div>
+</div>
+
+
+<style>
+    #sidebar {
+        width: 488px;
+    }
+</style>
