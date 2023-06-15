e11f1cb92caf8e8dbed3eea675b620c8fc1483d3
Chris Knight <chris.knight@london.gov.uk>
2023-06-15 12:14:26 +0100

    docs: update stories

--- packages/ui/src/lib/button/Button.stories.svelte
+++ packages/ui/src/lib/button/Button.stories.svelte
@@ -16,28 +16,51 @@
 
 <Story name="Primary">
 	<Button style="primary">Primary Style</Button>
+	<Button style="primary" disabled>Primary Style (disabled)</Button>
 </Story>
 
 <Story name="Secondary">
 	<Button style="secondary">Secondary Style</Button>
+	<Button style="secondary" disabled>Secondary Style (disabled)</Button>
 </Story>
 
 <Story name="Ghost">
 	<Button style="ghost">Ghost Style</Button>
+	<Button style="ghost" disabled>Ghost Style (disabled)</Button>
 </Story>
 
-<Story name="Disabled" args={{ disabled: true }} />
+<Story name="Disabled">
+	<Button disabled>Default Style</Button>
+	<Button style="primary" disabled>Primary Style</Button>
+	<Button style="secondary" disabled>Secondary Style</Button>
+	<Button style="ghost" disabled>Ghost Style</Button>
+</Story>
 
 <Story name="Sizes">
-	<Button size="xs">xs</Button>
-	<Button size="sm">sm</Button>
-	<Button size="base">base</Button>
-	<Button size="lg">lg</Button>
-	<Button size="xl">xl</Button>
+	<div class="flex items-end space-x-2">
+		<Button size="xs">xs</Button>
+		<Button size="sm">sm</Button>
+		<Button size="base">base</Button>
+		<Button size="lg">lg</Button>
+		<Button size="xl">xl</Button>
+	</div>
 </Story>
 
 <Story name="With Icon">
-	<Button>Icon via slot<ArrowCircleDownIcon class="ml-2 w-6 h-6" aria-hidden="true" /></Button>
+	<div class="flex space-y-2 flex-col w-40">
+		<Button
+			>Icon via slot
+			<ArrowCircleDownIcon class="w-6 h-6 ml-2" aria-hidden="true" />
+		</Button>
+		<Button>
+			<ArrowCircleDownIcon class="w-6 h-6 mr-2" aria-hidden="true" />
+			Icon via slot
+		</Button>
+		<Button class="flex-col items-center">
+			Icon via slot
+			<ArrowCircleDownIcon class="w-6 h-6" aria-hidden="true" />
+		</Button>
+	</div>
 </Story>
 
 <Story name="With class prop">
