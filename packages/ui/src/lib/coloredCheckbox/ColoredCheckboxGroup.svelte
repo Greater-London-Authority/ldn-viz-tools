575b8d41eae3860ee4eee39da4a4e1321e3c11e0
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-15 12:46:07 +0100

    add wrapper components for generating colored checkbox/radio button groups

---
+++ packages/ui/src/lib/coloredCheckbox/ColoredCheckboxGroup.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import ColoredCheckbox from "./ColoredCheckbox.svelte";
+
+    export let options: { id: string, label: string }[] = [];
+
+    export let selectedOptions = [];
+    let selectionState = Object.fromEntries(options.map(o => [o.id, false]));
+
+    $: {
+        selectedOptions = options.map(o => o.id).filter(id => selectionState[id]);
+    }
+</script>
+
+<div>
+    {#each options as option}
+        <ColoredCheckbox id={option.id} label={option.label} color={option.color}
+                         bind:checked={selectionState[option.id]}/>
+    {/each}
+</div>
---
+++ packages/ui/src/lib/coloredRadio/ColoredRadioButtonGroup.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import ColoredRadio from "./ColoredRadio.svelte";
+    export let selectedId: string;
+    export let name: string;
+    export let options: { id: string, label: string }[] = [];
+    export let hideClearButton = false;
+</script>
+
+<div>
+    {#if !hideClearButton}
+        <button on:click={() => selectedId = undefined}>Clear</button>
+    {/if}
+
+    {#each options as option}
+        <ColoredRadio id={option.id} label={option.label} color={option.color} bind:selectedId={selectedId}
+                      name={name}/>
+    {/each}
+</div>
