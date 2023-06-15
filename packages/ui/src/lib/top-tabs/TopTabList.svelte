f27c688ea8ae8881297efb9724653dfca1b31a7d
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-15 16:39:16 +0100

    initial commit of top tab component

---
+++ packages/ui/src/lib/top-tabs/TopTabList.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import {setContext} from 'svelte';
+    import {writable, type Writable} from 'svelte/store';
+
+    export let selectedValue: string | undefined = undefined;
+    const val: Writable<string | undefined> = writable(selectedValue);
+    val.subscribe((newVal) => (selectedValue = newVal));
+    setContext('selectedValue', {
+        selectedValue: val
+    });
+</script>
+
+
+<div class="flex bg-core-grey-800 border-b-4 border-b-core-blue-600 w-full h-16 pt-5 pr-0 pb-0 pl-4 gap-2.5 items-end"
+>
+    <slot/>
+</div>
---
+++ packages/ui/src/lib/top-tabs/TopTabs.stories.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+	import TopTabList from "./TopTabList.svelte";
+	import TopTabLabel from "./TopTabLabel.svelte";
+
+	let selectedValue = 'aggregates';
+</script>
+
+<Meta title="Ui/TopTabList" component={TopTabList} />
+
+<Story name="Default">
+
+	<TopTabList bind:selectedValue>
+		<TopTabLabel tabId="aggregates">Aggregated counts across London</TopTabLabel>
+		<TopTabLabel tabId="chargers">Details of chargers</TopTabLabel>
+		<TopTabLabel tabId="averages">Averages of charge events</TopTabLabel>
+		<TopTabLabel tabId="histograms">Histograms of charge events</TopTabLabel>
+	</TopTabList>
+
+	<p>Selected value is: <code>{selectedValue}</code></p>
+
+</Story>
