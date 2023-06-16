5cdb75a60523f2c075da5ffe366aa2b2d68d33c0
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-16 15:31:51 +0100

    creat logo components

---
+++ packages/ui/src/lib/logos/Logos.stories.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {Meta, Story} from '@storybook/addon-svelte-csf';
+
+    import LogoLOTI from './LogoLOTI.svelte';
+    import LogoMayor from './LogoMayor.svelte';
+    import LogoCIU from "./LogoCIU.svelte";
+</script>
+
+<Meta title="Ui/Logos" component={LogoLOTI}/>
+
+
+<Story name="Mayor of London">
+    <div class="bg-core-grey-600 h-14 flex items-center justify-center w-fit px-4">
+        <LogoMayor class="h-10"/>
+    </div>
+</Story>
+
+<Story name="CIU">
+    <LogoCIU class="h-10"/>
+</Story>
+
+<Story name="LOTI">
+    <LogoLOTI class="h-10"/>
+</Story>
