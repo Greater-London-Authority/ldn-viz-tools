8c4f7a3d48a416fec0cce85f17b1378da739f1a8
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-16 12:22:34 +0100

    add select/unselect all buttons to colored checkbox group

--- packages/ui/src/lib/coloredCheckbox/ColoredCheckbox.stories.svelte
+++ packages/ui/src/lib/coloredCheckbox/ColoredCheckbox.stories.svelte
@@ -22,19 +22,26 @@
     <ColoredCheckbox {...args}>This content is the child of the modal!</ColoredCheckbox>
 </Template>
 
-<Story name="Default">
+<Story name="ColoredCheckboxGroup">
+    <p>selectedOptions: {JSON.stringify(selectedOptions)}</p>
+    <ColoredCheckboxGroup options={optionsForGroup} bind:selectedOptions={selectedOptions} />
+</Story>
+
+<Story name="ColoredCheckboxGroup - disbaled buttons">
+    <p>selectedOptions: {JSON.stringify(selectedOptions)}</p>
+    <ColoredCheckboxGroup options={optionsForGroup} bind:selectedOptions={selectedOptions} buttonsHidden />
+</Story>
+
+
+<Story name="Single checkbox">
     <p>Checked: {checked}</p>
     <ColoredCheckbox color="#008D48" bind:checked label="Foo"/>
 </Story>
 
-<Story name="Multiple checkboxes">
+<Story name="Multiple checkboxes not in group">
 
     <ColoredCheckbox color="#00AEEF" label="Foo" id="foo"/>
     <ColoredCheckbox color="#008D48" label="Bar" id="bar"/>
     <ColoredCheckbox color="#9E0059" label="Baz" id="baz"/>
 </Story>
 
-<Story name="ColoredCheckboxGroup">
-    <p>selectedOptions: {JSON.stringify(selectedOptions)}</p>
-    <ColoredCheckboxGroup options={optionsForGroup} bind:selectedOptions={selectedOptions} />
-</Story>
--- packages/ui/src/lib/coloredCheckbox/ColoredCheckboxGroup.svelte
+++ packages/ui/src/lib/coloredCheckbox/ColoredCheckboxGroup.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
+    import Button from "../button/Button.svelte";
     import ColoredCheckbox from "./ColoredCheckbox.svelte";
 
     export let options: { id: string, label: string }[] = [];
+    export let buttonsHidden = false;
 
     export let selectedOptions = [];
     let selectionState = Object.fromEntries(options.map(o => [o.id, false]));
+
+    const selectAll = () => {
+        selectionState = Object.fromEntries(options.map(o => [o.id, true]));
+    }
+
+    const clearAll = () => {
+        selectionState = Object.fromEntries(options.map(o => [o.id, false]));
+    }
 
     $: {
         selectedOptions = options.map(o => o.id).filter(id => selectionState[id]);
@@ -12,6 +22,13 @@
 </script>
 
 <div>
+
+    {#if !buttonsHidden}
+        <Button style="ghost" on:click={selectAll} disabled={selectedOptions.length === options.length}>Select all
+        </Button>
+        <Button style="ghost" on:click={clearAll} disabled={selectedOptions.length === 0}>Clear all</Button>
+    {/if}
+
     {#each options as option}
         <ColoredCheckbox id={option.id} label={option.label} color={option.color}
                          bind:checked={selectionState[option.id]}/>
