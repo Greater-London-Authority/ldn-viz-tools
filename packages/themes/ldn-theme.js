05df63b1adf2b224c41e13ac77b7350de7894c95
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-20 13:52:18 +0100

    remove old (now misleading) comments

--- packages/themes/ldn-theme.js
+++ packages/themes/ldn-theme.js
@@ -1,10 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-// core-grey-600: LDN grey
-// core-grey-700: BTN
-// core-grey-800: Dark Panel
-// core-grey-900: Dark App
-
 const ldnColors = require("./colors.json");
 
 const config = {
