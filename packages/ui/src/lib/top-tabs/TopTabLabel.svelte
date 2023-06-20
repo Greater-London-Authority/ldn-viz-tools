1fe018178412b9b6f8638f8eb917cc058a886a5c
Chris Knight <chris.knight@london.gov.uk>
2023-06-20 17:13:50 +0100

    style: closer to figma

--- packages/ui/src/lib/top-tabs/TopTabLabel.svelte
+++ packages/ui/src/lib/top-tabs/TopTabLabel.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
-    import {getContext} from 'svelte';
-    import type {Writable} from 'svelte/types/runtime/store';
+	import { getContext } from 'svelte';
+	import type { Writable } from 'svelte/types/runtime/store';
 
-    export let tabId: string;
-    const {selectedValue} = getContext<{ selectedValue: Writable<string> }>('selectedValue');
+	export let tabId: string;
+	const { selectedValue } = getContext<{ selectedValue: Writable<string> }>('selectedValue');
 
-    const handleSelection = () => {
-        $selectedValue = tabId;
-    };
-    const keydownHandler = (ev: KeyboardEvent) => {
-        if (ev.key === 'Enter' || ev.key === ' ') {
-            handleSelection();
-        }
-    };
-    const classNames = (...classes: string[]) => {
-        return classes.filter(Boolean).join(' ');
-    };
+	const handleSelection = () => {
+		$selectedValue = tabId;
+	};
+	const keydownHandler = (ev: KeyboardEvent) => {
+		if (ev.key === 'Enter' || ev.key === ' ') {
+			handleSelection();
+		}
+	};
+	const classNames = (...classes: string[]) => {
+		return classes.filter(Boolean).join(' ');
+	};
 </script>
 
-
 <div
-        on:click={handleSelection}
-        on:keydown={keydownHandler}
-        tabindex="0"
-        role="tab"
-        class={classNames(
-		tabId === $selectedValue ? 'bg-core-blue-600 hover:bg-core-blue-700' : 'bg-core-grey-700 hover:bg-core-grey-600',
-		'h-8 text-white text-base py-2 px-4 flex items-center select-none'
+	on:click={handleSelection}
+	on:keydown={keydownHandler}
+	tabindex="0"
+	role="tab"
+	class={classNames(
+		tabId === $selectedValue
+			? 'bg-core-blue-600 cursor-default'
+			: 'bg-core-grey-600 hover:bg-core-grey-500 cursor-pointer',
+		'text-white text-base py-2 px-4 flex items-center select-none '
 	)}
 >
-    <slot/>
+	<slot />
 </div>
