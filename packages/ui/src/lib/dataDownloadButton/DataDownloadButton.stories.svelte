29a2bebc174c4d7a85b719979b7457f24e0dc2da
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-20 14:09:30 +0100

    Data download component (#42)
    * add a data download button
    
    * autoformat and center icon and label in button
    
    * commit the updated pacakge.json file
    
    * dev: update package name in charts
    
    * types
    
    * add types for d3-dsv package
    
    * add changeset file
    
    * accept label as slot rather than prop
    
    * build: d3-csv install
    
    * package-lock
    
    * refactor: api to match button. Icon flexible
    
    ---------
    
    Co-authored-by: Chris Knight <chris.knight@london.gov.uk>
    Co-authored-by: Chris Knight <111759008+ChrisKnightLDN@users.noreply.github.com>

---
+++ packages/ui/src/lib/dataDownloadButton/DataDownloadButton.stories.svelte
@@ -0,0 +1,67 @@
+<script>
+	import { ArrowCircleDownIcon, DocumentDownloadIcon } from '@rgossiaux/svelte-heroicons/solid';
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import DataDownloadButton from './DataDownloadButton.svelte';
+
+	const data = [
+		{ A: 1, B: 2, C: 3 },
+		{ A: 4, B: 5, C: 6 },
+		{ A: 7, B: 8, C: 9 }
+	];
+</script>
+
+<Meta title="Ui/DataDownloadButton" component={DataDownloadButton} />
+
+<Template let:args>
+	<DataDownloadButton {...args}>I'm a button from UI!</DataDownloadButton>
+</Template>
+
+<Story name="Default">
+	<DataDownloadButton {data} filename="download" format="CSV">
+		<ArrowCircleDownIcon class="w-6 h-6" aria-hidden="true" />
+	</DataDownloadButton>
+</Story>
+
+<Story name="Button types">
+	<DataDownloadButton {data} filename="download" format="CSV" style="primary">
+		Primary<ArrowCircleDownIcon class="ml-2 w-6 h-6" aria-hidden="true" />
+	</DataDownloadButton>
+	<DataDownloadButton {data} filename="download" format="CSV" style="secondary">
+		Secondary<DocumentDownloadIcon class="ml-2 w-6 h-6" aria-hidden="true" />
+	</DataDownloadButton>
+	<DataDownloadButton {data} filename="download" format="CSV" style="ghost">
+		Ghost<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="ml-2 w-6 h-6"
+			aria-hidden="true"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+			/>
+		</svg>
+	</DataDownloadButton>
+</Story>
+
+<Story name="Download as JSON">
+	<DataDownloadButton {data} filename="download" format="JSON">
+		<ArrowCircleDownIcon class="w-6 h-6" aria-hidden="true" />
+	</DataDownloadButton>
+</Story>
+
+<Story name="Label on button and custom data">
+	<DataDownloadButton {data} filename="download" format="CSV">
+		Download<ArrowCircleDownIcon class="w-6 h-6 ml-2" aria-hidden="true" />
+	</DataDownloadButton>
+</Story>
+
+<Story name="Disabled">
+	<DataDownloadButton {data} disabled={true} filename="download" format="CSV">
+		<ArrowCircleDownIcon class="w-6 h-" aria-hidden="true" />
+	</DataDownloadButton>
+</Story>
---
+++ packages/ui/src/lib/dataDownloadButton/DataDownloadButton.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import Button from '../button/Button.svelte';
+
+	import { csvFormat } from 'd3-dsv';
+
+	export let format: 'CSV' | 'JSON' | undefined;
+	export let data: any;
+	export let filename: string;
+
+	export let disabled = false;
+
+	const downloadFromURL = (url: string, name: string) => {
+		const link = document.createElement('a');
+		link.setAttribute('href', url);
+		link.setAttribute('target', '_blank');
+		link.setAttribute('download', name);
+		link.dispatchEvent(new MouseEvent('click'));
+	};
+
+	const downloadJSON = () => {
+		const dataString = JSON.stringify(data, null, 4);
+		const dataURL = 'data:application/json;base64,' + window.btoa(dataString);
+		downloadFromURL(dataURL, filename || `data.json`);
+	};
+
+	const downloadCSV = () => {
+		const dataString = csvFormat(data);
+		const dataURL = 'data:application/csv;base64,' + window.btoa(dataString);
+		downloadFromURL(dataURL, filename || `data.csv`);
+	};
+
+	const download = format === 'JSON' ? downloadJSON : downloadCSV;
+</script>
+
+<Button on:click={download} {disabled} {...$$restProps}>
+	<slot />
+</Button>
