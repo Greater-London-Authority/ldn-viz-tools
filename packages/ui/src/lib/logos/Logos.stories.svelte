6dad0776602a372bb01525b395cf50d7368f371f
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-20 16:50:28 +0100

    create logo components (#62)
    * creat logo components
    
    * delete empty file
    
    * refactor: clean up svgs
    
    * docs: update stories with colors
    
    * default to correct coloring of loti logo
    
    * make all logos behave consistently, and make all examples grayscale
    
    * fix: typo on strory
    
    * docs: use fill color on custom story
    
    ---------
    
    Co-authored-by: Chris Knight <chris.knight@london.gov.uk>

---
+++ packages/ui/src/lib/logos/Logos.stories.svelte
@@ -0,0 +1,51 @@
+<script>
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+	import LogoCIU from './LogoCIU.svelte';
+	import LogoLOTI from './LogoLOTI.svelte';
+	import LogoMayor from './LogoMayor.svelte';
+</script>
+
+<Meta title="Ui/Logos" component={LogoLOTI} />
+
+<Story name="Mayor of London">
+	<div class="text-core-grey-800 dark:text-white">
+		<LogoMayor class="w-40" />
+	</div>
+</Story>
+
+<Story name="CIU">
+	<div class="text-core-grey-800 dark:text-white">
+		<LogoCIU class="w-40" />
+	</div>
+</Story>
+
+<Story name="LOTI">
+	<div class="text-core-grey-800 dark:text-white">
+		<LogoLOTI class="h-10" />
+	</div>
+</Story>
+
+<Story name="Inherit font color (default)">
+	<div class="text-core-grey-800 dark:text-white flex space-x-6">
+		<LogoMayor class="w-40" />
+		<LogoCIU class="w-40" />
+		<LogoLOTI class="h-10" />
+	</div>
+</Story>
+
+<Story name="Custom fill color">
+	<div class="space-y-4">
+		<div class="flex space-x-6">
+			<LogoMayor class="w-40 fill-core-grey-400 dark:fill-core-grey-50 " />
+			<LogoCIU class="w-40 fill-core-grey-400 dark:fill-core-grey-50" />
+			<LogoLOTI class="h-10 fill-core-grey-400 dark:fill-core-grey-50" />
+		</div>
+
+		<div class="flex space-x-6">
+			<LogoMayor class="w-40 fill-core-grey-800 dark:fill-core-grey-400" />
+			<LogoCIU class="w-40 fill-core-grey-800 dark:fill-core-grey-400" />
+			<LogoLOTI class="h-10 fill-core-grey-800 dark:fill-core-grey-400" />
+		</div>
+	</div>
+</Story>
