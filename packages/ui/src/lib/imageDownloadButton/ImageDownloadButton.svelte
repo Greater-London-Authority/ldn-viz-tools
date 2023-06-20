6a10ab3bbb2b6a3073f4c791a2ca9ac3507c77ec
Chris Knight <chris.knight@london.gov.uk>
2023-06-20 15:22:20 +0100

    types: fix type errors

--- packages/ui/src/lib/imageDownloadButton/ImageDownloadButton.svelte
+++ packages/ui/src/lib/imageDownloadButton/ImageDownloadButton.svelte
@@ -1,72 +1,70 @@
 <script lang="ts">
-    import Button from '../button/Button.svelte';
-    export let svgNode;
-    export let scaleFactor = 2;
-    export let filename;
-    export let disabled = false;
-    export let format: "PNG" | "SVG" | undefined;
-    export let tooltip;
+	import Button from '../button/Button.svelte';
+	export let svgNode: SVGElement;
+	export let scaleFactor = 2;
+	export let filename: string | undefined = undefined;
+	export let disabled = false;
+	export let format: 'PNG' | 'SVG' | undefined = 'PNG';
 
-    const downloadFromURL = (url) => {
-        if (!filename){
-            filename = (format === "SVG") ? "Image.svg" : "Image.png";
-        }
+	const downloadFromURL = (url: string) => {
+		if (!filename) {
+			filename = format === 'SVG' ? 'Image.svg' : 'Image.png';
+		}
 
-        const link = document.createElement("a");
-        link.setAttribute("href", url);
-        link.setAttribute("target", "_blank");
-        link.setAttribute("download", filename);
-        link.dispatchEvent(new MouseEvent("click"));
-    };
+		const link = document.createElement('a');
+		link.setAttribute('href', url);
+		link.setAttribute('target', '_blank');
+		link.setAttribute('download', filename);
+		link.dispatchEvent(new MouseEvent('click'));
+	};
 
-    const createHTMLImageFromURL = async (url: string) => {
-        let img;
+	const createHTMLImageFromURL = async (url: string) => {
+		let img: HTMLImageElement;
 
-        await new Promise(resolve => {
-            img = new Image();
-            img.onload = resolve;
-            img.src = url;
-        });
+		await new Promise((resolve) => {
+			img = new Image();
+			img.onload = resolve;
+			img.src = url;
+		});
+		//@ts-ignore
+		return img;
+	};
 
-        return img;
-    };
+	const download = async () => {
+		// This hack is necessary because .drawImage() doesn't work
+		// unless the SVG that is being copied has an explicitly-set size
+		const svgNodeClone = svgNode.cloneNode(true) as SVGElement;
+		svgNodeClone.setAttribute('width', svgNode.clientWidth.toString());
+		svgNodeClone.setAttribute('height', svgNode.clientHeight.toString());
 
-    const download = async () => {
+		const svgString = new XMLSerializer().serializeToString(svgNodeClone);
+		const svgURL = 'data:image/svg+xml;base64,' + window.btoa(svgString);
+		// the non base-64 encoded URL would be: "data:image/svg+xml;utf8," + svgString;
 
-        // This hack is necessary because .drawImage() doesn't work
-        // unless the SVG that is being copied has an explicitly-set size
-        const svgNodeClone = svgNode.cloneNode(true);
-        svgNodeClone.setAttribute("width", svgNode.clientWidth)
-        svgNodeClone.setAttribute("height", svgNode.clientHeight)
+		// an alternative is:
+		// const svgURL = URL.createObjectURL(new Blob([svgNodeClone.outerHTML],{type:'image/svg+xml;charset=utf-8'}))
 
-        const svgString = new XMLSerializer().serializeToString(svgNodeClone);
-        const svgURL = 'data:image/svg+xml;base64,' + window.btoa(svgString);
-        // the non base-64 encoded URL would be: "data:image/svg+xml;utf8," + svgString;
+		const img = await createHTMLImageFromURL(svgURL);
 
-        // an alternative is:
-        // const svgURL = URL.createObjectURL(new Blob([svgNodeClone.outerHTML],{type:'image/svg+xml;charset=utf-8'}))
+		if (format === 'SVG') {
+			downloadFromURL(svgURL);
+		} else {
+			img.src = svgURL;
 
-        const img = await createHTMLImageFromURL(svgURL);
+			const w = svgNode.clientWidth * scaleFactor;
+			const h = svgNode.clientHeight * scaleFactor;
 
-        if (format === "SVG") {
-            downloadFromURL(svgURL);
-        } else {
-            img.src = svgURL;
+			const canvas = new OffscreenCanvas(w, h);
 
-            const w = svgNode.clientWidth * scaleFactor;
-            const h = svgNode.clientHeight * scaleFactor;
+			canvas.getContext('2d')?.drawImage(img, 0, 0, w, h);
 
-            const canvas = new OffscreenCanvas(w, h);
-            canvas.getContext("2d").drawImage(img, 0, 0, w, h);
-
-            canvas.convertToBlob().then((blob) => {
-                downloadFromURL(URL.createObjectURL(blob));
-            });
-        }
-    }
+			canvas.convertToBlob().then((blob) => {
+				downloadFromURL(URL.createObjectURL(blob));
+			});
+		}
+	};
 </script>
 
-
 <Button on:click={download} {disabled} {...$$restProps}>
-    <slot />
+	<slot />
 </Button>
