fee98197bd0bea1b31636814a108e9e5973dd610
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-21 12:47:59 +0100

    initial commit of top tab component (#60)
    * initial commit of top tab component
    
    * toggle text color in story in dark mode
    
    * remove a console.log() statement
    
    * style: closer to figma
    
    * rename TopTab -> Tab
    
    * rename story
    
    * use classnames utility function
    
    ---------
    
    Co-authored-by: Chris Knight <chris.knight@london.gov.uk>

---
+++ packages/ui/src/lib/tabs/TabList.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { setContext } from 'svelte';
+	import { writable, type Writable } from 'svelte/store';
+
+	export let selectedValue: string | undefined = undefined;
+	const val: Writable<string | undefined> = writable(selectedValue);
+	val.subscribe((newVal) => (selectedValue = newVal));
+	setContext('selectedValue', {
+		selectedValue: val
+	});
+</script>
+
+<div
+	class="flex bg-core-grey-800 border-b-4 border-b-core-blue-600 w-full pt-5 px-4 pb-0 space-x-0.5 items-end"
+>
+	<slot />
+</div>
