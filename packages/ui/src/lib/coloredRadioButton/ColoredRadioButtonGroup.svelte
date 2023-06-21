bafbf1678a9b97cd98bda4896b7c77a34c4b351d
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-06-21 11:44:50 +0100

    fixes

--- packages/ui/src/lib/coloredRadioButton/ColoredRadioButtonGroup.svelte
+++ packages/ui/src/lib/coloredRadioButton/ColoredRadioButtonGroup.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-    import ColoredRadioButton from "./ColoredRadioButton.svelte";
-    export let selectedId: string;
-    export let name: string;
-    export let options: { id: string, label: string, disabled?: boolean }[] = [];
-    export let buttonsHidden = false;
+	import ColoredRadioButton from './ColoredRadioButton.svelte';
+	export let selectedId: string;
+	export let name: string;
+	export let options: { id: string; label: string; color: string; disabled?: boolean }[] = [];
+	export let buttonsHidden = false;
 </script>
 
 <div>
-    {#if !buttonsHidden}
-        <button on:click={() => selectedId = undefined}>Clear</button>
-    {/if}
+	{#if !buttonsHidden}
+		<button on:click={() => (selectedId = '')}>Clear</button>
+	{/if}
 
-    {#each options as option}
-        <ColoredRadioButton id={option.id} label={option.label} color={option.color} disabled={option.disabled}
-                            bind:selectedId={selectedId}
-                      name={name}/>
-    {/each}
+	{#each options as option}
+		<ColoredRadioButton
+			id={option.id}
+			label={option.label}
+			color={option.color}
+			disabled={option.disabled}
+			bind:selectedId
+			{name}
+		/>
+	{/each}
 </div>
