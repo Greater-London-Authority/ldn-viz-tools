dd54b2a31ff3b14b1194abbeabe3178cf3158c8c
Chris Knight <chris.knight@london.gov.uk>
2023-06-30 17:54:25 +0100

    style: stretch to siblings height

--- packages/ui/src/lib/radioButtonSolid/RadioButtonSolid.svelte
+++ packages/ui/src/lib/radioButtonSolid/RadioButtonSolid.svelte
@@ -14,7 +14,7 @@
 	const { selectedId } = getContext<{ selectedId: Writable<string> }>('selectedId');
 </script>
 
-<div class="w-1/3 relative">
+<div class="w-1/3 flex">
 	<input
 		id={inputID}
 		type="radio"
