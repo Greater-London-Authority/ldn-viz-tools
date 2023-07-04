f667638494ee1cf320a3d21d6b754e7f5fa70b94
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-07-04 08:51:47 +0100

    Rename tabSelector to `RadioButtonGroupSolid`  (#75)
    * rename tabSelector
    
    * start work on variants
    
    * implement old tab-selector radio butotns and new bluegrey radio buttons
    
    * Radio button group as radio buttons
    
    * refactor: accept slots or options object
    
    * style: stretch to siblings height
    
    * remove comment
    
    ---------
    
    Co-authored-by: Chris Knight <chris.knight@london.gov.uk>
    Co-authored-by: Chris Knight <111759008+ChrisKnightLDN@users.noreply.github.com>

---
+++ packages/ui/src/lib/radioButtonSolid/RadioButtonSolid.stories.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import {
+		FilterIcon,
+		MapIcon,
+		PresentationChartLineIcon
+	} from '@rgossiaux/svelte-heroicons/solid';
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+	import RadioButtonGroupSolid from './RadioButtonGroupSolid.svelte';
+	import RadioButtonSolid from './RadioButtonSolid.svelte';
+
+	let selectedId = 'bus';
+
+	let optionsForGroup = [
+		{ id: 'bus', label: 'Bus stops' },
+		{ id: 'train', label: 'Train stations' },
+		{ id: 'underground', label: 'Underground stations' },
+		{ id: 'taxi', label: 'Taxi ranks', disabled: true }
+	];
+</script>
+
+<Meta title="Ui/RadioButtonSolid" component={RadioButtonGroupSolid} />
+
+<Story name="RadioGroup">
+	<RadioButtonGroupSolid options={optionsForGroup} name="station-type" bind:selectedId />
+	<p class="mt-8 text-core-grey-500 dark:text-core-grey-200 italic">Selected id: {selectedId}</p>
+</Story>
+
+<Story name="RadioGroup with Icons above">
+	<RadioButtonGroupSolid name="station-type" bind:selectedId>
+		<RadioButtonSolid id="bus" name="station-type-bus">
+			<FilterIcon class="w-6 h-6 mb-1" aria-hidden="true" />
+			Bus
+		</RadioButtonSolid>
+		<RadioButtonSolid id="train" name="station-type-train">
+			<MapIcon class="w-6 h-6 mb-1" aria-hidden="true" />
+			Train
+		</RadioButtonSolid>
+		<RadioButtonSolid id="plane" name="station-type-plane">
+			<PresentationChartLineIcon class="w-6 h-6 mb-1" aria-hidden="true" />
+			Plane
+		</RadioButtonSolid>
+	</RadioButtonGroupSolid>
+	<p class="mt-8 text-core-grey-500 dark:text-core-grey-200 italic">Selected id: {selectedId}</p>
+</Story>
+
+<Story name="RadioGroup with Icons below">
+	<RadioButtonGroupSolid name="station-type" bind:selectedId>
+		<RadioButtonSolid id="bus" name="station-type-bus">
+			Bus
+			<FilterIcon class="w-6 h-6 mt-1" aria-hidden="true" />
+		</RadioButtonSolid>
+		<RadioButtonSolid id="train" name="station-type-train">
+			Train
+			<MapIcon class="w-6 h-6 mt-1" aria-hidden="true" />
+		</RadioButtonSolid>
+		<RadioButtonSolid id="plane" name="station-type-plane">
+			Plane
+			<PresentationChartLineIcon class="w-6 h-6 mt-1" aria-hidden="true" />
+		</RadioButtonSolid>
+	</RadioButtonGroupSolid>
+	<p class="mt-8 text-core-grey-500 dark:text-core-grey-200 italic">Selected id: {selectedId}</p>
+</Story>
---
+++ packages/ui/src/lib/radioButtonSolid/RadioButtonSolid.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import type { Writable } from 'svelte/store';
+	import { classNames } from '../utils/classNames';
+
+	export let label = '';
+	export let id: string;
+	export let name: string | undefined;
+
+	export let disabled = false;
+
+	let inputID = `input-${id}`;
+
+	const { selectedId } = getContext<{ selectedId: Writable<string> }>('selectedId');
+</script>
+
+<div class="w-1/3 flex">
+	<input
+		id={inputID}
+		type="radio"
+		{name}
+		bind:group={$selectedId}
+		value={id}
+		{disabled}
+		class="peer absolute top-0 left-0 opacity-0"
+	/>
+	<label
+		for={inputID}
+		class={classNames(
+			disabled
+				? 'cursor-not-allowed bg-core-grey-200 !text-core-grey-400'
+				: 'cursor-pointer bg-core-grey-100 hover:bg-core-grey-200 dark:bg-core-grey-700 dark:hover:bg-core-grey-600',
+			'form-label flex flex-col justify-center items-center p-2 w-full ring-1 ring-white peer-checked:bg-core-blue-600 hover:peer-checked:bg-core-blue-600 peer-checked:text-white  dark:ring-core-grey-800'
+		)}
+	>
+		<slot>{label}</slot>
+	</label>
+</div>
