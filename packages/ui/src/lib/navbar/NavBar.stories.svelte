c8f1be89703bdb61e83bad0a88647b22a6eac87f
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-08-03 12:05:57 +0100

    add navbar component

---
+++ packages/ui/src/lib/navbar/NavBar.stories.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+    import {Meta, Story, Template} from '@storybook/addon-svelte-csf';
+
+    import NavBarContainer from "./NavBarContainer.svelte";
+    import NavLink from "./NavLink.svelte";
+    import NavLinks from "./NavLinks.svelte";
+    import NavTitle from "./NavTitle.svelte";
+    import NavAuth from "./NavAuth.svelte";
+    import NavItem from "./NavItem.svelte";
+
+</script>
+
+<Meta title="Ui/NavBar" component={NavBarContainer}/>
+
+<Template let:args>
+    <NavBarContainer {...args}/>
+</Template>
+
+<Story name="NavBar - title only">
+    <NavBarContainer>
+        <NavTitle>An interesting app</NavTitle>
+    </NavBarContainer>
+</Story>
+
+
+<Story name="A NavBar - title and links to pages">
+    <NavBarContainer>
+        <NavTitle>EV Charger Dashboard</NavTitle>
+
+        <NavLinks>
+            <NavLink target="map">
+                Map
+            </NavLink>
+
+            <NavLink target="trends">
+                Trends
+            </NavLink>
+        </NavLinks>
+    </NavBarContainer>
+</Story>
+
+<Story name="A NavBar - title, links, and auth section">
+    <NavBarContainer>
+        <NavTitle>EV Charger Dashboard</NavTitle>
+
+        <NavLinks>
+            <NavLink target="map">
+                Map
+            </NavLink>
+
+            <NavLink target="trends">
+                Trends
+            </NavLink>
+        </NavLinks>
+
+
+        <NavAuth>
+            <NavItem>
+                <span>Logged in as <b>Bob Smith</b></span>
+            </NavItem>
+
+
+            <NavItem>
+                <a href="/#">Log Out</a>
+            </NavItem>
+        </NavAuth>
+    </NavBarContainer>
+</Story>
+
---
+++ packages/ui/src/lib/navbar/NavLink.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {base} from '$app/paths';
+    import {page} from "$app/stores";
+
+    export let target = "";
+
+    let classes;
+    $:{
+        if (($page.url?.pathname || "").endsWith(target)) {
+            classes = "flex h-[50px] bg-core-blue-500 items-center px-4 py-2 items-center";
+        } else {
+            classes = "flex h-[50px] bg-core-grey-700 items-center px-4 py-2 items-center";
+        }
+    }
+</script>
+
+
+<div class={classes}>
+    <a href={`${base}/${target}`}>
+        <slot/>
+    </a>
+</div>
