0919b10a4a6ffc457985962e3b35ca02e03973e8
Paul Williams <paul.williams@london.gov.uk>
2023-08-25 16:25:28 +0100

    Add initial spinner

---
+++ packages/ui/src/lib/spinners/Spinner.stories.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+	import Spinner from './Spinner.svelte';
+</script>
+
+<Meta title="Ui/Spinners" component={Spinner} />
+
+<Story name="Default">
+	<div class="text-core-grey-800">
+		<Spinner class="w-40" />
+	</div>
+</Story>
+
+<Story name="Thickness">
+	<div class="text-core-grey-800">
+		<Spinner class="w-40 stroke-[16]" />
+	</div>
+</Story>
+
+<Story name="Color">
+	<div class="text-core-grey-800">
+		<Spinner class="w-40" circleClass="stroke-core-blue-200" arcClass="stroke-core-darkPink-500" />
+	</div>
+</Story>
---
+++ packages/ui/src/lib/spinners/Spinner.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let circleClass = '';
+	export let arcClass = '';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 100 100"
+	fill="none"
+	stroke-width="8"
+	{...$$restProps}
+>
+	<circle stroke="#515a5e" cx="50" cy="50" r="38" class={circleClass} />
+	<path stroke="#6DA7DE" stroke-linecap="round" d="M 50,12 a 38,38 0 0,1 38,38" class={arcClass}>
+		<animateTransform
+			attributeName="transform"
+			type="rotate"
+			from="0 50 50"
+			to="360 50 50"
+			dur="1s"
+			repeatCount="indefinite"
+		/>
+	</path>
+</svg>
