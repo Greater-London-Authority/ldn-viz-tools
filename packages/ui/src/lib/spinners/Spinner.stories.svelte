ad68e0fd938e0349cc0e4d4f177666e48977527b
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-10-06 10:00:25 +0100

    add story showing spinner in a button

--- packages/ui/src/lib/spinners/Spinner.stories.svelte
+++ packages/ui/src/lib/spinners/Spinner.stories.svelte
@@ -2,6 +2,7 @@
 	import { Meta, Story } from '@storybook/addon-svelte-csf';
 
 	import Spinner from './Spinner.svelte';
+	import Button from '../button/Button.svelte';
 </script>
 
 <Meta title="Ui/Spinners" component={Spinner} />
@@ -21,5 +22,13 @@
 <Story name="Color">
 	<div class="text-core-grey-800">
 		<Spinner class="w-40" circleClass="stroke-core-blue-200" arcClass="stroke-core-glaPink-600" />
+	</div>
+</Story>
+
+<Story name="In a button">
+	<div class="text-core-grey-800">
+		<Button>I'm loading...
+			<Spinner class="h-6 w-6"/>
+		</Button>
 	</div>
 </Story>
