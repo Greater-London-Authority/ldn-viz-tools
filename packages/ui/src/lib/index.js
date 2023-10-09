3af933f97745d1376141fd24eec5e336b0edf00b
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-10-09 08:22:04 +0000

    fix package exports

--- packages/ui/src/lib/index.js
+++ packages/ui/src/lib/index.js
@@ -14,18 +14,18 @@ export { default as LogoMayor } from './logos/LogoMayor.svelte';
 
 export { default as Modal } from './modal/Modal.svelte';
 
-export { default as NavAuth } from './navbar/NavAuth.svelte';
-export { default as NavBarContainer } from './navbar/NavBarContainer.svelte';
-export { default as NavItem } from './navbar/NavItem.svelte';
-export { default as NavLink } from './navbar/NavLink.svelte';
-export { default as NavLinks } from './navbar/NavLinks.svelte';
-export { default as NavTitle } from './navbar/NavTitle.svelte';
+export { default as Header } from './header/Header.svelte';
+export { default as HeaderItem } from './header/HeaderItem.svelte';
+export { default as HeaderRight } from './header/HeaderRight.svelte';
+export { default as HeaderTitle } from './header/HeaderTitle.svelte';
+export { default as NavLink } from './header/NavLink.svelte';
+export { default as NavLinks } from './header/NavLinks.svelte';
 
 export { default as RadioButton } from './radioButton/RadioButton.svelte';
 export { default as RadioButtonGroup } from './radioButton/RadioButtonGroup.svelte';
 
-export { default as RadioButtonSolid } from './radioButtonSolid/RadioButtonSolid.svelte';
 export { default as RadioButtonGroupSolid } from './radioButtonSolid/RadioButtonGroupSolid.svelte';
+export { default as RadioButtonSolid } from './radioButtonSolid/RadioButtonSolid.svelte';
 
 export { default as Select } from './select/Select.svelte';
 export { default as Spinner } from './spinners/Spinner.svelte';
