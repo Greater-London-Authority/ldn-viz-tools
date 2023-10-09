692e8a0721b3c27e878649077dce13e6c0e57060
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-10-09 09:51:00 +0100

    Update Storybook and Svelte (#131)

--- packages/maps/.eslintrc.cjs
+++ packages/maps/.eslintrc.cjs
@@ -1,13 +1,25 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'plugin:svelte/recommended'
+	],
+	plugins: ['svelte', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			// Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
+	settings: {},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
