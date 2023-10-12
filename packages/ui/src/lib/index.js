a9cf9b8213e9189155f6c41fbf9c189350b30cf9
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-10-12 21:14:00 +0100

    change double-quotes to single-quotes

--- packages/ui/src/lib/index.js
+++ packages/ui/src/lib/index.js
@@ -1,7 +1,7 @@
 // Reexport your entry components here
 export { default as Button } from './button/Button.svelte';
 
-export { default as CardLink } from "./cardLink/CardLink.svelte";
+export { default as CardLink } from './cardLink/CardLink.svelte';
 
 export { default as Checkbox } from './checkBox/Checkbox.svelte';
 export { default as CheckboxGroup } from './checkBox/CheckboxGroup.svelte';
