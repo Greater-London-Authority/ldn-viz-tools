7748e3208922ca4a53ee8dfa9d478c82921fb0f5
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-10-12 15:36:27 +0000

    add a link card component based on Survey of Londoners

---
+++ packages/ui/src/lib/cardLink/CardLink.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+	import CardLink from './CardLink.svelte';
+</script>
+
+<Meta title="Ui/CardLink" component={CardLink} />
+
+<Story name="Default">
+	<CardLink>
+		<span slot="body">
+			The full dataset that this explorer is based on is available for download from the London
+			Datastore, along with other documents like the Technical Report, which contains the
+			questionnaires from the survey.
+		</span>
+		<span slot="footer">The Survey of Londoners on London Datastore</span>
+	</CardLink>
+</Story>
---
+++ packages/ui/src/lib/cardLink/CardLink.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+		import { ArrowRightIcon } from '@rgossiaux/svelte-heroicons/solid';
+
+	import { classNames } from '../utils/classNames';
+
+	export let href = '';
+	export let newWindow = false;
+</script>
+
+<a
+	{href}
+	target={newWindow ? '_blank' : '_self'}
+	rel={newWindow ? 'noopener noreferrer' : ''}
+	class={classNames(
+		'bg-core-grey-700 hover:bg-core-grey-800 text-white',
+		'dark:bg-core-grey-200 dark:group dark:hover:bg-core-grey-300 dark:text-black',
+		'group transition h-full flex flex-col max-w-xl'
+	)}
+>
+	<div class="p-4 max-w-xl h-full">
+		<slot name="body" />
+	</div>
+	<div
+		class={classNames(
+			'bg-core-grey-800 group-hover:bg-core-blue-600',
+			'dark:bg-core-grey-300 group-hover:bg-core-blue-600',
+			'p-4 mt-4 flex justify-between  transition'
+		)}
+	>
+		<slot name="footer" />
+		<ArrowRightIcon class="h-6 w-6" />
+	</div>
+</a>
