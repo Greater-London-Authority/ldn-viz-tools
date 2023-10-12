f5d63e57f1cdd12b04cb06c9117122c2d5f0f6d6
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-10-12 15:51:51 +0000

    more display options

--- packages/ui/src/lib/cardLink/CardLink.stories.svelte
+++ packages/ui/src/lib/cardLink/CardLink.stories.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { DocumentDownloadIcon } from '@rgossiaux/svelte-heroicons/solid';
 	import { Meta, Story } from '@storybook/addon-svelte-csf';
 
 	import CardLink from './CardLink.svelte';
@@ -14,5 +15,21 @@
 			questionnaires from the survey.
 		</span>
 		<span slot="footer">The Survey of Londoners on London Datastore</span>
+	</CardLink>
+</Story>
+
+<Story name="Rounded">
+	<CardLink rounded>
+		<span slot="body" class="text-3xl font-serif mb-2"> Download the User Guide. </span>
+		<span slot="footer">PDF</span>
+		<span slot="icon"><DocumentDownloadIcon class="h-6 w-6" /></span>
+	</CardLink>
+</Story>
+
+<Story name="Green Rounded">
+	<CardLink rounded green>
+		<span slot="body" class="text-3xl font-serif mb-2"> Download the User Guide. </span>
+		<span slot="footer">PDF</span>
+		<span slot="icon"><DocumentDownloadIcon class="h-6 w-6" /></span>
 	</CardLink>
 </Story>
--- packages/ui/src/lib/cardLink/CardLink.svelte
+++ packages/ui/src/lib/cardLink/CardLink.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-		import { ArrowRightIcon } from '@rgossiaux/svelte-heroicons/solid';
+	import { ArrowRightIcon } from '@rgossiaux/svelte-heroicons/solid';
 
 	import { classNames } from '../utils/classNames';
 
 	export let href = '';
 	export let newWindow = false;
+	export let rounded = false;
+
+	export let green = false;
 </script>
 
 <a
@@ -12,9 +15,12 @@
 	target={newWindow ? '_blank' : '_self'}
 	rel={newWindow ? 'noopener noreferrer' : ''}
 	class={classNames(
-		'bg-core-grey-700 hover:bg-core-grey-800 text-white',
-		'dark:bg-core-grey-200 dark:group dark:hover:bg-core-grey-300 dark:text-black',
-		'group transition h-full flex flex-col max-w-xl'
+		green
+			? 'bg-core-green-600 hover:bg-core-green-700 text-white'
+			: 'bg-core-grey-700 hover:bg-core-grey-800 text-white',
+		green ? '' : 'dark:bg-core-grey-200 dark:group dark:hover:bg-core-grey-300 dark:text-black',
+		'group transition h-full flex flex-col max-w-xl',
+		rounded ? 'rounded-lg' : ''
 	)}
 >
 	<div class="p-4 max-w-xl h-full">
@@ -22,12 +28,15 @@
 	</div>
 	<div
 		class={classNames(
-			'bg-core-grey-800 group-hover:bg-core-blue-600',
-			'dark:bg-core-grey-300 group-hover:bg-core-blue-600',
-			'p-4 mt-4 flex justify-between  transition'
+			green ? 'bg-core-green-800' : 'bg-core-grey-800 group-hover:bg-core-blue-600',
+			green ? '' : 'dark:bg-core-grey-300 dark:group-hover:bg-core-blue-600',
+			'p-4 mt-4 flex justify-between  transition',
+			rounded ? 'rounded-b-lg' : ''
 		)}
 	>
 		<slot name="footer" />
-		<ArrowRightIcon class="h-6 w-6" />
+		<slot name="icon">
+			<ArrowRightIcon class="h-6 w-6" />
+		</slot>
 	</div>
 </a>
