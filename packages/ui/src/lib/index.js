f0d85a72430905d77b25ecd0985ba5c8bffea172
Chris Knight <chris.knight@london.gov.uk>
2023-10-13 18:18:09 +0100

    reorder exports

--- packages/ui/src/lib/index.js
+++ packages/ui/src/lib/index.js
@@ -1,38 +1,29 @@
 // Reexport your entry components here
 export { default as Button } from './button/Button.svelte';
-
 export { default as Checkbox } from './checkBox/Checkbox.svelte';
 export { default as CheckboxGroup } from './checkBox/CheckboxGroup.svelte';
-
 export { default as DataDownloadButton } from './dataDownloadButton/DataDownloadButton.svelte';
-
-export { default as ImageDownloadButton } from './imageDownloadButton/ImageDownloadButton.svelte';
-export { default as LogoByCIU } from './logos/LogoByCIU.svelte';
-export { default as LogoCIU } from './logos/LogoCIU.svelte';
-export { default as LogoLOTI } from './logos/LogoLOTI.svelte';
-export { default as LogoMayor } from './logos/LogoMayor.svelte';
-
-export { default as Modal } from './modal/Modal.svelte';
-
 export { default as Header } from './header/Header.svelte';
 export { default as HeaderItem } from './header/HeaderItem.svelte';
 export { default as HeaderRight } from './header/HeaderRight.svelte';
 export { default as HeaderTitle } from './header/HeaderTitle.svelte';
 export { default as NavLink } from './header/NavLink.svelte';
 export { default as NavLinks } from './header/NavLinks.svelte';
-
+export { default as ImageDownloadButton } from './imageDownloadButton/ImageDownloadButton.svelte';
+export { default as LogoByCIU } from './logos/LogoByCIU.svelte';
+export { default as LogoCIU } from './logos/LogoCIU.svelte';
+export { default as LogoLOTI } from './logos/LogoLOTI.svelte';
+export { default as LogoMayor } from './logos/LogoMayor.svelte';
+export { default as Modal } from './modal/Modal.svelte';
 export { default as RadioButton } from './radioButton/RadioButton.svelte';
 export { default as RadioButtonGroup } from './radioButton/RadioButtonGroup.svelte';
-
 export { default as RadioButtonGroupSolid } from './radioButtonSolid/RadioButtonGroupSolid.svelte';
 export { default as RadioButtonSolid } from './radioButtonSolid/RadioButtonSolid.svelte';
-
 export { default as Select } from './select/Select.svelte';
 export { default as Spinner } from './spinners/Spinner.svelte';
 export { default as TabbedSidebar } from './tabbedSidebar/TabbedSidebar.svelte';
 export { default as TabbedSidebarTabLabel } from './tabbedSidebar/TabbedSidebarTabLabel.svelte';
 export { default as TabbedSidebarTabList } from './tabbedSidebar/TabbedSidebarTabList.svelte';
 export { default as TabbedSidebarWrapper } from './tabbedSidebar/TabbedSidebarWrapper.svelte';
-
 export { default as TabLabel } from './tabs/TabLabel.svelte';
 export { default as TabList } from './tabs/TabList.svelte';
