3776f3cef3231071d2ae3baca3f9bb636ed63860
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-10-13 14:23:23 +0000

    fix

--- packages/ui/src/lib/tabbedSidebar/TabbedSidebarWrapper.svelte
+++ packages/ui/src/lib/tabbedSidebar/TabbedSidebarWrapper.svelte
@@ -4,7 +4,7 @@
 	import TabbedSidebarTabLabel from './TabbedSidebarTabLabel.svelte';
 	import TabbedSidebarTabList from './TabbedSidebarTabList.svelte';
 
-	import type { IconThemeSource } from '@steeze-ui/heroicons/dist/types';
+	import type { IconSource } from '@steeze-ui/heroicons/dist/types';
 	import { setContext, type ComponentType } from 'svelte';
 	import { writable, type Writable } from 'svelte/store';
 
@@ -26,7 +26,7 @@
 	export let tabs: {
 		id: string;
 		label: string;
-		icon?: IconThemeSource;
+		icon?: IconSource;
 		rawIcon?: ComponentType;
 	}[] = [];
 </script>
@@ -43,7 +43,7 @@
 				{#if tab.icon}
 					<Icon src={tab.icon} theme="solid" class="h-5 w-5 mb-1" aria-hidden="true" />
 				{:else if tab.rawIcon}
-					<svelte:component this={tab.icon} class="h-5 w-5 mb-1" aria-hidden="true" />
+					<svelte:component this={tab.rawIcon} class="h-5 w-5 mb-1" aria-hidden="true" />
 				{/if}
 				{tab.label}
 			</TabbedSidebarTabLabel>
