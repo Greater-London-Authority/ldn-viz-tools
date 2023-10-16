e911906c6567a4187c174269a7f6d80ac16db159
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-10-16 08:32:04 +0000

    use classNAmes() rather than @apply to resolve prettier error

--- packages/ui/src/lib/messenger/Messenger.stories.svelte
+++ packages/ui/src/lib/messenger/Messenger.stories.svelte
@@ -1,8 +1,14 @@
 <script>
 	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import { classNames } from '../util';
 
 	import Messenger, { postMessage } from './Messenger.svelte';
 	import { MessageType } from './Message.svelte';
+
+	const classesGLaBorder = 'border-2 border-solid border-black dark:border-white';
+	const classesGLAPanel = 'relative m-4 space-y-2 w-[800px] h-[300px]';
+	const classesGLADescription = 'text-core-grey-500 dark:text-core-grey-200 ml-2';
+	const classesGLAButton = classNames('ml-2 p-2 text-lg', classesGLaBorder);
 </script>
 
 <Meta title="Ui/Messenger" component={Messenger} />
@@ -12,13 +18,13 @@
 </Template>
 
 <Story name="Notice">
-	<div class="gla-panel">
-		<p class="gla-description">
+	<div class={classesGLAPanel}>
+		<p class={classesGLADescription}>
 			Post an information message that gets the users attention but can be ignored and will go away
 			on its own. Noticeable but ignorable.
 		</p>
 
-		<button class="gla-button bg-core-blue-500" on:click={() => postMessage('Notice me!')}>
+		<button class={classNames(classesGLAButton, 'bg-core-blue-500')} on:click={() => postMessage('Notice me!')}>
 			Post message
 		</button>
 
@@ -27,11 +33,11 @@
 </Story>
 
 <Story name="Success">
-	<div class="gla-panel">
-		<p class="gla-description">Post a success message.</p>
+	<div class={classesGLAPanel}>
+		<p class={classesGLADescription}>Post a success message.</p>
 
 		<button
-			class="gla-button bg-core-green-400"
+			class={classNames(classesGLAButton, 'bg-core-green-400')}
 			on:click={() => postMessage('Something done successfully.', MessageType.Success)}
 		>
 			Post message
@@ -42,11 +48,11 @@
 </Story>
 
 <Story name="Warning">
-	<div class="gla-panel">
-		<p class="gla-description">Post a warning message.</p>
+	<div class={classesGLAPanel}>
+		<p class={classesGLADescription}>Post a warning message.</p>
 
 		<button
-			class="gla-button bg-core-orange-500"
+			class={classNames(classesGLAButton, 'bg-core-organge-500')}
 			on:click={() => postMessage('Consider this a warning.', MessageType.Warning)}
 		>
 			Post message
@@ -57,11 +63,11 @@
 </Story>
 
 <Story name="Error">
-	<div class="gla-panel">
-		<p class="gla-description">Post an error message.</p>
+	<div class={classesGLAPanel}>
+		<p class={classesGLADescription}>Post an error message.</p>
 
 		<button
-			class="gla-button bg-core-red-400"
+			class={classNames(classesGLAButton, 'bg-core-red-400')}
 			on:click={() => postMessage('You ignored the warning.', MessageType.Error)}
 		>
 			Post message
@@ -72,16 +78,16 @@
 </Story>
 
 <Story name="Absolute positioning">
-	<div class="gla-panel gla-border">
-		<p class="gla-description">By default messages are fixed to the bottom right.</p>
+	<div class={classNames(classesGLAPanel, classesGLaBorder)}>
+		<p class={classesGLADescription}>By default messages are fixed to the bottom right.</p>
 
-		<p class="gla-description">
+		<p class={classesGLADescription}>
 			We can change this by providing the messenger with classes to reposition it. For example,
 			bottom middle of the parent container.
 		</p>
 
 		<button
-			class="gla-button bg-core-blue-500"
+			class={classNames(classesGLAButton, 'bg-core-blue-500')}
 			on:click={() => postMessage('Positioned absolutely.')}
 		>
 			Post message
@@ -92,17 +98,17 @@
 </Story>
 
 <Story name="Timeout: long lived">
-	<div class="gla-panel">
-		<p class="gla-description">By default messages disappear after three and a half seconds.</p>
+	<div class={classesGLAPanel}>
+		<p class={classesGLADescription}>By default messages disappear after three and a half seconds.</p>
 
-		<p class="gla-description">
+		<p class={classesGLADescription}>
 			We can change this on a message by message basis by setting providing a timeout in
 			milliseconds when posting. For example, you may want to display a longer message for a longer
 			time to allow the user to read it. Eight seconds in this story.
 		</p>
 
 		<button
-			class="gla-button bg-core-blue-500"
+			class={classNames(classesGLAButton, 'bg-core-blue-500')}
 			on:click={() =>
 				postMessage('Once upon a time on a planet far far away...', MessageType.Notice, 8000)}
 		>
@@ -114,21 +120,21 @@
 </Story>
 
 <Story name="Timeout: never">
-	<div class="gla-panel">
-		<p class="gla-description">By default messages disappear after three and a half seconds.</p>
+	<div class={classesGLAPanel}>
+		<p class={classesGLADescription}>By default messages disappear after three and a half seconds.</p>
 
-		<p class="gla-description">
+		<p class={classesGLADescription}>
 			We can disable this by setting the timeout to zero or a negative number when posting. The user
 			will have to manually dismiss the message.
 		</p>
 
-		<p class="gla-description">
+		<p class={classesGLADescription}>
 			This should be avoided as the Messenger is primarily intended for noticeable but ignorable
 			messaging. If the users attention is not optional then a Model would be better.
 		</p>
 
 		<button
-			class="gla-button bg-core-blue-500"
+			class={classNames(classesGLAButton, 'bg-core-blue-500')}
 			on:click={() => postMessage('Never ending story.', MessageType.Notice, 0)}
 		>
 			Post message
@@ -137,23 +143,3 @@
 		<Messenger />
 	</div>
 </Story>
-
-<style>
-	.gla-border {
-		@apply border-2 border-solid border-black dark:border-white;
-	}
-
-	.gla-panel {
-		@apply relative m-4 space-y-2;
-		width: 800px;
-		height: 300px;
-	}
-
-	.gla-description {
-		@apply text-core-grey-500 dark:text-core-grey-200 ml-2;
-	}
-
-	.gla-button {
-		@apply ml-2 p-2 text-lg gla-border;
-	}
-</style>
