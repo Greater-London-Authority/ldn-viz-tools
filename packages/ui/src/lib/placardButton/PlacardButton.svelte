1713d7939dfd924ca743c539058898036a27fcf7
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2023-10-18 09:46:27 +0100

    add a link card component based on Survey of Londoners (#145)
    ---------
    
    Co-authored-by: Chris Knight <chris.knight@london.gov.uk>

---
+++ packages/ui/src/lib/placardButton/PlacardButton.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { ArrowRightIcon } from '@rgossiaux/svelte-heroicons/solid';
+
+	export let href = '';
+	export let newWindow = false;
+</script>
+
+<a
+	{href}
+	target={newWindow ? '_blank' : '_self'}
+	rel={newWindow ? 'noopener noreferrer' : ''}
+	class="bg-core-grey-700 group hover:bg-core-grey-800 transition h-full flex flex-col text-white"
+>
+	<div class="p-4 h-full space-y-2">
+		{#if $$slots.title}
+			<div class="text-2xl font-bold">
+				<slot name="title" />
+			</div>
+		{/if}
+
+		{#if $$slots.body}
+			<div class="h-full text-core-grey-100 prose leading-snug">
+				<slot name="body" />
+			</div>
+		{/if}
+	</div>
+	<div
+		class="bg-core-grey-600 group-hover:bg-core-blue-600 dark:group-hover:bg-core-blue-600 p-4 mt-4 flex justify-between transition"
+	>
+		<slot name="footer" />
+		<slot name="icon">
+			<ArrowRightIcon class="h-6 w-6" />
+		</slot>
+	</div>
+</a>
