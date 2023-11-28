58cba3a08d4d0d6a8ef1514fe22076ed536188d6
Paul Williams <paul.williams@london.gov.uk>
2023-11-28 16:09:53 +0000

    Expose new components via index.js

--- packages/maps/src/lib/index.js
+++ packages/maps/src/lib/index.js
@@ -1,3 +1,5 @@
 // Reexport your entry components here
+export * from './map/Map.svelte';
 export { default as Map } from './map/Map.svelte';
+export { default as MapApp } from './map/MapApp.svelte';
 export { default as bounds } from './themes/bounds.js';
