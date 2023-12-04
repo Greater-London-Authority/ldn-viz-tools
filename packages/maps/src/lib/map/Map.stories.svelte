89b6666df8151812107552021db0482eb390c14d
Paul Williams <paul.williams@london.gov.uk>
2023-12-04 11:05:34 +0000

    Add Maplibre Map component (#170)

--- packages/maps/src/lib/map/Map.stories.svelte
+++ packages/maps/src/lib/map/Map.stories.svelte
@@ -1,101 +1,169 @@
 <script>
 	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
-	import { MapLibre } from 'svelte-maplibre';
 
-	import { GREATER_LONDON_ZOOM, GREATER_LONDON_CENTER } from '../themes/bounds.js';
-
-	import * as lightStyle from '../themes/os_light_vts.json';
+	import * as os_light_vts from '../themes/os_light_vts.json';
 	import * as greyStyle from '../themes/os_greyscale.json';
 	import * as darkGreyMutedStyle from '../themes/os_dark_grey_muted_buildings.json';
 	import * as darkStyle from '../themes/os_dark.json';
 
-	import Map from './Map.svelte';
+	import MapApp from './MapApp.svelte';
+	import Map, { appendOSKeyToUrl } from './Map.svelte';
 
 	const OS_KEY = 'vmRzM4mAA1Ag0hkjGh1fhA2hNLEM6PYP';
+	let clickedLayerIDs = ['Click a point on the map to list the vector layer IDs'];
+	let map = null;
 
-	const transformRequest = (url) => {
-		if (!url.toLowerCase().includes('api.os')) return { url: url };
-		if (!/[?&]key=/.test(url)) url += '?key=' + OS_KEY;
-		return {
-			url: url + '&srs=3857'
-		};
+	const updateClickedLayers = (event) => {
+		if (map) {
+			clickedLayerIDs = map.queryRenderedFeatures(event.point).map((f) => f.layer.id);
+		}
+	};
+
+	const whenMapLoads = (newMap) => {
+		map = newMap;
+		newMap.getCanvas().style.cursor = 'pointer';
+		newMap.on('click', updateClickedLayers);
+	};
+
+	const whenMapUnloads = (oldMap) => {
+		oldMap.getCanvas().style.cursor = '';
+		oldMap.off('click', updateClickedLayers);
+		map = null;
 	};
 </script>
 
-<Meta title="Maps/Map" component={Map}>foo</Meta>
+<Meta
+	title="Maps/Map"
+	component={Map}
+	parameters={{
+		layout: 'full'
+	}}
+/>
+
 <Template let:args>
-	<Map {...args}>I'm a map!</Map>
+	<MapApp>
+		<Map {...args}>I'm a map!</Map>
+	</MapApp>
 </Template>
 
 <Story name="Light OS Basemap">
-	<p>
-		This is our default light basemap - it's the OS's <a
-			class="underline"
-			href="https://github.com/OrdnanceSurvey/OS-Vector-Tile-API-Stylesheets"
-		>
-			OS_VTS_3857_Light.json</a
-		> stylesheet.
-	</p>
+	<MapApp>
+		<div class="text-white space-y-4 m-2">
+			<p>
+				This is our default light basemap - it's the OS's <a
+					class="underline"
+					href="https://github.com/OrdnanceSurvey/OS-Vector-Tile-API-Stylesheets"
+				>
+					OS_VTS_3857_Light.json</a
+				> stylesheet. Like all OS base maps it requires an OS access token.
+			</p>
 
-	<MapLibre
-		style={lightStyle}
-		class="relative w-full aspect-[9/16] max-h-[70vh] sm:max-h-full sm:aspect-video"
-		standardControls
-		zoom={GREATER_LONDON_ZOOM}
-		center={GREATER_LONDON_CENTER}
-		{transformRequest}
-	/>
+			<p>
+				If you're creating a full page map, as opposed to a map integrated into a dashboard or data
+				explorer, then wrap the <code>{'<Map>'}</code>,
+				<code>{'<Sidebar>'}</code> and other map components within a
+				<code>{'<MapApp>'}</code> component. It manages no-script messages and layout that accounts for
+				different browser overlays.
+			</p>
+		</div>
+
+		<Map
+			options={{
+				style: os_light_vts,
+				transformRequest: appendOSKeyToUrl(OS_KEY)
+			}}
+		/>
+	</MapApp>
 </Story>
 
 <Story name="Greyscale OS Basemap">
-	<p>
-		This is the greyscale basemap used on the Cool Spaces map. - it's very similar to the OS's <a
-			class="underline"
-			href="https://github.com/OrdnanceSurvey/OS-Vector-Tile-API-Stylesheets"
-		>
-			OS_VTS_3857_Greyscale.json</a
-		> stylesheet, with a few tweaks.
-	</p>
+	<MapApp>
+		<div class="text-white space-y-4 m-2">
+			<p>
+				This is the greyscale basemap used on the Cool Spaces map. - it's very similar to the OS's <a
+					class="underline"
+					href="https://github.com/OrdnanceSurvey/OS-Vector-Tile-API-Stylesheets"
+				>
+					OS_VTS_3857_Greyscale.json</a
+				> stylesheet, with a few tweaks.
+			</p>
+		</div>
 
-	<MapLibre
-		style={greyStyle}
-		class="relative w-full aspect-[9/16] max-h-[70vh] sm:max-h-full sm:aspect-video"
-		standardControls
-		zoom={GREATER_LONDON_ZOOM}
-		center={GREATER_LONDON_CENTER}
-		{transformRequest}
-	/>
+		<Map
+			options={{
+				style: greyStyle,
+				transformRequest: appendOSKeyToUrl(OS_KEY)
+			}}
+		/>
+	</MapApp>
 </Story>
 
 <Story name="Dark Grey, muted buildings">
-	<p>This was created by Mike Brondbjerg based on the OS greyscale theme.</p>
+	<MapApp>
+		<div class="text-white space-y-4 m-2">
+			<p>This was created by Mike Brondbjerg based on the OS greyscale theme.</p>
+		</div>
 
-	<MapLibre
-		style={darkGreyMutedStyle}
-		class="relative w-full aspect-[9/16] max-h-[70vh] sm:max-h-full sm:aspect-video"
-		standardControls
-		zoom={GREATER_LONDON_ZOOM}
-		center={GREATER_LONDON_CENTER}
-		{transformRequest}
-	/>
+		<Map
+			options={{
+				style: darkGreyMutedStyle,
+				transformRequest: appendOSKeyToUrl(OS_KEY)
+			}}
+		/>
+	</MapApp>
 </Story>
 
 <Story name="Dark OS Basemap">
-	<p>
-		This was created by the OS, inspired by Mike Brondbjerg's dark gray theme with muted buildings.
-		It is <a
-			class="underline"
-			href="https://github.com/OrdnanceSurvey/OS-Vector-Tile-API-Stylesheets"
-			>OS_VTS_3857_Dark.json</a
-		>
-	</p>
+	<MapApp>
+		<div class="text-white space-y-4 m-2">
+			<p>
+				This was created by the OS, inspired by Mike Brondbjerg's dark gray theme with muted
+				buildings. It is <a
+					class="underline"
+					href="https://github.com/OrdnanceSurvey/OS-Vector-Tile-API-Stylesheets"
+					>OS_VTS_3857_Dark.json</a
+				>
+			</p>
+		</div>
 
-	<MapLibre
-		style={darkStyle}
-		class="relative w-full aspect-[9/16] max-h-[70vh] sm:max-h-full sm:aspect-video"
-		standardControls
-		zoom={GREATER_LONDON_ZOOM}
-		center={GREATER_LONDON_CENTER}
-		{transformRequest}
-	/>
+		<Map
+			options={{
+				style: darkStyle,
+				transformRequest: appendOSKeyToUrl(OS_KEY)
+			}}
+		/>
+	</MapApp>
+</Story>
+
+<Story name="Properties">
+	<MapApp>
+		<div class="text-white space-y-4 m-2">
+			<p>
+				The <code>{'whenMapLoads'}</code> and <code>{'whenMapUnloads'}</code> props are functions
+				called when the <code>{'<Map>'}</code> component mounts and unmounts respectively. In this example
+				a simple click listener is attached to the map which prints out the layer IDs where a feature
+				exists at the click location.
+			</p>
+
+			<p>
+				The <code>{'options'}</code> prop allows users to override and extend the GLA default MapLibre
+				options. Use it to specify a map style specification and append the OS key.
+			</p>
+		</div>
+
+		<Map
+			{whenMapLoads}
+			{whenMapUnloads}
+			options={{
+				style: os_light_vts,
+				transformRequest: appendOSKeyToUrl(OS_KEY)
+			}}
+		>
+			<ul class="absolute top-0 left-0 z-10 bg-core-grey-800/[0.8] text-white text-sm m-2 p-2">
+				{#each clickedLayerIDs as id (id)}
+					<li>{id}</li>
+				{/each}
+			</ul>
+		</Map>
+	</MapApp>
 </Story>
--- packages/maps/src/lib/map/Map.svelte
+++ packages/maps/src/lib/map/Map.svelte
@@ -1,1 +1,89 @@
-<p>I'm a map from the map package</p>
+<script context="module">
+	export const appendOSKeyToUrl = (osKey) => {
+		return (url) => {
+			url = new URL(url);
+
+			if (!url.hostname === 'api.os.uk') {
+				return { url: url.href };
+			}
+
+			const WEB_MERCATOR_EPSG = 3857;
+			url.searchParams.set('srs', WEB_MERCATOR_EPSG);
+			url.searchParams.set('key', osKey);
+
+			return { url: url.href };
+		};
+	};
+</script>
+
+<script>
+	import { onMount } from 'svelte';
+	import maplibre_gl from 'maplibre-gl';
+	import 'maplibre-gl/dist/maplibre-gl.css';
+
+	import { GREATER_LONDON_BOUNDS, GREATER_LONDON_BOUNDS_MAX } from '../themes/bounds';
+	import * as os_light_vts from '../themes/os_light_vts.json';
+
+	export let disabled = false;
+	export let options = {};
+
+	export let whenMapLoads = null;
+	export let whenMapUnloads = null;
+
+	const defaultOptions = {
+		style: os_light_vts,
+		bounds: GREATER_LONDON_BOUNDS,
+		maxBounds: GREATER_LONDON_BOUNDS_MAX,
+		attributionControl: true,
+		clickTolerance: 6
+	};
+
+	let container;
+
+	onMount(() => {
+		if (disabled) {
+			return;
+		}
+
+		const map = new maplibre_gl.Map({
+			...defaultOptions,
+			...options,
+			container
+		});
+
+		map.once('load', () => {
+			whenMapLoads && whenMapLoads(map, maplibre_gl);
+		});
+
+		return () => whenMapUnloads && whenMapUnloads(map, maplibre_gl);
+	});
+</script>
+
+<section
+	bind:this={container}
+	id="ldn-viz-map-container"
+	class="w-full h-full relative"
+	{...$$restProps}
+>
+	<slot />
+</section>
+
+<style global>
+	/*
+		Override top level MapLibre & MapBox styling with ldn-viz styling so we
+		don't have to do it separately within each map sub-component, e.g.
+		map controls, popups, etc.
+	*/
+	.mapboxgl-map,
+	.maplibregl-map {
+		font-size: inherit;
+		font-family: inherit;
+		font-style: inherit;
+		line-height: inherit;
+	}
+
+	.maplibregl-ctrl-attrib-inner,
+	.mapboxgl-ctrl-attrib-inner {
+		@apply text-sm;
+	}
+</style>
