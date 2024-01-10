e7c4acf119500e85d37c5c03b660ef4ad36beeab
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-01-10 17:01:12 +0000

    replace @rgossiaux/svelte-headlessui with svelte-headlessui

--- packages/ui/src/lib/modal/Modal.stories.svelte
+++ packages/ui/src/lib/modal/Modal.stories.svelte
@@ -6,6 +6,12 @@
 	import Button from '../button/Button.svelte';
 
 	let isOpen = true;
+
+	let dialog1;
+	let dialog2;
+	let dialog3;
+	let dialog4;
+	let dialog5;
 </script>
 
 <Meta title="Ui/Modal" component={Modal} />
@@ -14,23 +20,22 @@
 	<Modal {...args}>This content is the child of the modal!</Modal>
 </Template>
 
-<Story name="Default" args={{ title: 'Title', description: 'Description', isOpen: true }} />
 
 <Story name="Description only">
-	<Button on:click={() => (isOpen = true)}>Open modal!</Button>
+	<Button on:click={() => dialog1.open()}>Open modal!</Button>
 
 	<Modal
-		bind:isOpen
+		bind:dialog={dialog1}
 		title="A modal with contents!"
 		description="This modal has a description, but no contents!"
 	/>
 </Story>
 
 <Story name="Description and Contents">
-	<Button on:click={() => (isOpen = true)}>Open modal!</Button>
+	<Button on:click={() => dialog2.open()}>Open modal!</Button>
 
 	<Modal
-		bind:isOpen
+		bind:dialog={dialog2}
 		title="A modal with contents!"
 		description="This modal has a description and contents!"
 	>
@@ -44,9 +49,9 @@
 </Story>
 
 <Story name="Contents without description">
-	<Button on:click={() => (isOpen = true)}>Open modal!</Button>
+	<Button on:click={() => dialog3.open()}>Open modal!</Button>
 
-	<Modal bind:isOpen title="A modal with contents!">
+	<Modal bind:dialog={dialog3} title="A modal with contents!">
 		<p>A list</p>
 		<ul>
 			<li>One</li>
@@ -57,26 +62,26 @@
 </Story>
 
 <Story name="Modal with close button">
-	<Button on:click={() => (isOpen = true)}>Open modal!</Button>
+	<Button on:click={() => dialog4.open()}>Open modal!</Button>
 
-	<Modal bind:isOpen title="A modal with close button!">
+	<Modal bind:dialog={dialog4} title="A modal with close button!">
 		<div class="mb-4">
 			In a real example, these buttons would be appropriately styled, and potentially perform some
 			action in addition to closing the modal.
 		</div>
 
-		<Button variant="text" class="bg-core-green-400" on:click={() => (isOpen = false)}
+		<Button variant="text" class="bg-core-green-400" on:click={dialog4.close()}
 			>Accept</Button
 		>
-		<Button variant="text" class="bg-core-red-400" on:click={() => (isOpen = false)}>Cancel</Button>
+		<Button variant="text" class="bg-core-red-400" on:click={dialog4.close()}>Cancel</Button>
 	</Modal>
 </Story>
 
 <Story name="Modal width">
-	<Button on:click={() => (isOpen = true)}>Open modal!</Button>
+	<Button on:click={() => dialog5.open()}>Open modal!</Button>
 
 	<Modal
-		bind:isOpen
+		bind:dialog={dialog5}
 		width="6xl"
 		title="A wider modal!"
 		description="We can use the width prop to set different max-widths from xs though 7xl and full. Default is md"
--- packages/ui/src/lib/modal/Modal.svelte
+++ packages/ui/src/lib/modal/Modal.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import {
-		Dialog,
-		DialogDescription,
-		DialogOverlay,
-		DialogTitle
-	} from '@rgossiaux/svelte-headlessui';
+	import { createDialog } from 'svelte-headlessui';
+	import Transition from 'svelte-transition';
+
 	import { classNames } from '../utils/classNames';
 
+	export let dialog = createDialog({ label: '' });
+
 	export let isOpen = false;
+
 	export let title: string;
 	export let description: string;
 	export let width:
@@ -47,45 +47,68 @@
 	);
 </script>
 
-<Dialog open={isOpen} on:close={() => (isOpen = false)} class="relative z-10 overflow-y-auto">
-	<DialogOverlay class="fixed inset-0 bg-black bg-opacity-40" />
+<div class="flex w-full flex-col items-center justify-center">
+	<div class="relative z-10">
+		<Transition show={$dialog.expanded}>
+			<Transition
+				enter="ease-out duration-300"
+				enterFrom="opacity-0"
+				enterTo="opacity-100"
+				leave="ease-in duration-200"
+				leaveFrom="opacity-100"
+				leaveTo="opacity-0"
+			>
+				<button class="fixed inset-0 bg-black bg-opacity-25" on:click={dialog.close} />
+			</Transition>
 
-	<!--Full-screen container to center the panel -->
-	<div class="fixed inset-0 flex items-center justify-center p-4 pointer-events-none">
-		<div class={modalClass}>
-			<div class="bg-core-grey-600 text-white p-2 relative">
-				<DialogTitle>{title}</DialogTitle>
-				<button
-					on:click={() => (isOpen = false)}
-					class="bg-core-grey-500 absolute top-2 right-2 hover:bg-core-grey-800"
-				>
-					<span class="sr-only">Close</span>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6"
+			<div class="fixed inset-0 overflow-y-auto">
+				<div class="flex min-h-full items-center justify-center p-4 text-center">
+					<Transition
+						enter="ease-out duration-300"
+						enterFrom="opacity-0 scale-95"
+						enterTo="opacity-100 scale-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100 scale-100"
+						leaveTo="opacity-0 scale-95"
 					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
-				</button>
+						<div class={modalClass} use:dialog.modal>
+							<div class="bg-core-grey-600 text-white p-2 relative">
+								<h1>{title}</h1>
+								<button
+									on:click={dialog.close}
+									class="bg-core-grey-500 absolute top-2 right-2 hover:bg-core-grey-800"
+								>
+									<span class="sr-only">Close</span>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="w-6 h-6"
+									>
+										<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+									</svg>
+								</button>
+							</div>
+
+							{#if description}
+								<div class="px-2">{description}</div>
+							{/if}
+
+							{#if hasChildren && description}
+								<div class="border-dotted border-t border-core-grey-300 pt-4 mx-2 text-sm">
+									<slot />
+								</div>
+							{:else if hasChildren}
+								<div class="border-dotted border-core-grey-300 mx-2 text-sm">
+									<slot />
+								</div>
+							{/if}
+						</div>
+					</Transition>
+				</div>
 			</div>
-
-			{#if description}
-				<DialogDescription class="px-2">{description}</DialogDescription>
-			{/if}
-
-			{#if hasChildren && description}
-				<div class="border-dotted border-t border-core-grey-300 pt-4 mx-2 text-sm">
-					<slot />
-				</div>
-			{:else if hasChildren}
-				<div class="border-dotted border-core-grey-300 mx-2 text-sm">
-					<slot />
-				</div>
-			{/if}
-		</div>
+		</Transition>
 	</div>
-</Dialog>
+</div>
