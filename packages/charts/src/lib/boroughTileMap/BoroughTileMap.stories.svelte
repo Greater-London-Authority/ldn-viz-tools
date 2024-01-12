54689d68c6497c13adf834f60988151f67665eb1
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-01-12 11:11:16 +0000

    show D3 blue/red color-maps in example

--- packages/charts/src/lib/boroughTileMap/BoroughTileMap.stories.svelte
+++ packages/charts/src/lib/boroughTileMap/BoroughTileMap.stories.svelte
@@ -18,32 +18,48 @@
 
 
 <Story name="Symbol size and color">
+    <p>
+        Available color schemes are listed <a
+            href="https://github.com/kristw/encodable/blob/master/packages/encodable-color/src/scheme/presets/d3Schemes.ts">here</a>.
+        You can see them <a href="https://vega.github.io/vega/docs/schemes">here</a>.
+    </p>
     <div class="w-[800px] h-[800px]">
         <BoroughTileMap {data} gssCodeField="GSS_CODE"
-                       spec={{
+                        spec={{
                             symbolArea: {field: "ValueA"},
+
                             symbolColor: {
                                 field: "ValueA",
                                 type: "quantitative",
                                 scale: {
-                                  zero: false,
-                                  interpolate: 'hsl',
-                                  range: ['#FAE96F', '#C96E12'],
+                                  scheme: "d3.blues"
                                 }
                             },
-                            rectColor: {
-                                field: "ValueB",
-                                type: "quantitative",
-                                scale: {
-                                  zero: false,
-                                  interpolate: 'hsl',
-                                  range: ['red', "blue"],
-                                }
 
                             }
-                           }}
-
+                           }
         />
 
     </div>
 </Story>
+
+<Story name="Symbol size and background color">
+    <div class="w-[800px] h-[800px]">
+        <BoroughTileMap {data} gssCodeField="GSS_CODE"
+                        spec={{
+                            symbolArea: {field: "ValueA"},
+                            symbolColor: {value: "grey"},
+
+
+                            rectColor: {
+                                field: "ValueB",
+                                type: "quantitative",
+                                scale: {
+                                  scheme: "d3.reds"
+                                }
+                            }
+                           }}
+        />
+    </div>
+</Story>
+
