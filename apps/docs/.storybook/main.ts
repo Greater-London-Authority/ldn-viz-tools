78bdc8a160ec5d4c1947f621e92fdc828595aced
Chris Knight <chris.knight@london.gov.uk>
2024-01-12 11:52:45 +0000

    update storybook

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,45 +1,39 @@
 import type { StorybookConfig } from '@storybook/sveltekit';
-import remarkGfm from 'remark-gfm';
+
+import { dirname, join } from 'path';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+	return dirname(require.resolve(join(value, 'package.json')));
+}
 const config: StorybookConfig = {
-  stories: [
-    '../src/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)',
-    '../../../packages/ui/src/**/*.mdx',
-    '../../../packages/ui/src/**/*.stories.@(js|jsx|ts|tsx|svelte)',
-    '../../../packages/charts/src/**/*.mdx',
-    '../../../packages/charts/src/**/*.stories.@(js|jsx|ts|tsx|svelte)',
-    '../../../packages/maps/src/**/*.mdx',
-    '../../../packages/maps/src/**/*.stories.@(js|jsx|ts|tsx|svelte)'
-  ],
-  addons: [
-    '@storybook/addon-links',
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        docs: false
-      }
-    },
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        mdxPluginOptions: {
-          mdxCompileOptions: {
-            remarkPlugins: [remarkGfm]
-          }
-        }
-      }
-    },
-    '@storybook/addon-a11y',
-    '@storybook/addon-interactions',
-    '@storybook/addon-svelte-csf',
-    'storybook-dark-mode'
-  ],
-  framework: {
-    name: '@storybook/sveltekit',
-    options: {}
-  },
-  docs: {
-    autodocs: 'tag'
-  }
+	stories: [
+		'../src/**/*.mdx',
+		'../src/**/*.stories.@(js|jsx|ts|tsx|svelte)',
+		'../../../packages/ui/src/**/*.mdx',
+		'../../../packages/ui/src/**/*.stories.@(js|jsx|ts|tsx|svelte)',
+		'../../../packages/charts/src/**/*.mdx',
+		'../../../packages/charts/src/**/*.stories.@(js|jsx|ts|tsx|svelte)',
+		'../../../packages/maps/src/**/*.mdx',
+		'../../../packages/maps/src/**/*.stories.@(js|jsx|ts|tsx|svelte)'
+	],
+	addons: [
+		'@storybook/addon-svelte-csf',
+		'@storybook/addon-essentials',
+		'@storybook/addon-links',
+		'@storybook/addon-interactions',
+		'@storybook/addon-a11y',
+		'storybook-dark-mode'
+	],
+	framework: {
+		name: '@storybook/sveltekit',
+		options: {}
+	},
+	docs: {
+		autodocs: 'tag'
+	}
 };
 export default config;
--- apps/docs/.storybook/preview.ts
+++ apps/docs/.storybook/preview.ts
@@ -2,21 +2,21 @@ import type { Preview } from '@storybook/svelte';
 import '../src/app.postcss';
 
 const preview: Preview = {
-  parameters: {
-    backgrounds: { disable: true },
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/
-      }
-    },
-    darkMode: {
-      classTarget: 'html',
-      stylePreview: true,
-      appBg: 'black'
-    }
-  }
+	parameters: {
+		backgrounds: { disable: true },
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		},
+		darkMode: {
+			classTarget: 'html',
+			stylePreview: true,
+			appContentBg: 'black'
+		}
+	}
 };
 
 export default preview;
