91898eaed2178004c30eb968ea7e4ff82936a877
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-01-19 12:26:18 +0000

    re-enable export of modal

--- packages/ui/src/lib/index.js
+++ packages/ui/src/lib/index.js
@@ -14,7 +14,7 @@ export { default as LogoByCIU } from './logos/LogoByCIU.svelte';
 export { default as LogoCIU } from './logos/LogoCIU.svelte';
 export { default as LogoLOTI } from './logos/LogoLOTI.svelte';
 export { default as LogoMayor } from './logos/LogoMayor.svelte';
-// export { default as Modal } from './modal/Modal.svelte';
+export { default as Modal } from './modal/Modal.svelte';
 export { default as PlacardButton } from './placardButton/PlacardButton.svelte';
 export { default as RadioButton } from './radioButton/RadioButton.svelte';
 export { default as RadioButtonGroup } from './radioButton/RadioButtonGroup.svelte';
