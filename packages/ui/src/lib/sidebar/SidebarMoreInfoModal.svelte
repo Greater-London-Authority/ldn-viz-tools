4482ffe2380a1d06a89608da30c6dabf96327086
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-01-22 10:54:23 +0000

    switch icon library

--- packages/ui/src/lib/sidebar/SidebarMoreInfoModal.svelte
+++ packages/ui/src/lib/sidebar/SidebarMoreInfoModal.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import { InformationCircleIcon } from '@rgossiaux/svelte-heroicons/solid';
+	import { InformationCircle } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+
 	import Modal from '../modal/Modal.svelte';
 
 	export let title = '';
@@ -15,7 +17,13 @@
 	class="w-20 mt-1 text-xs text-core-grey-200 flex gap-1 cursor-pointer align-top float-right"
 >
 	<span class="ml-auto">more info</span>
-	<InformationCircleIcon class="h-4 w-4 fill-current" on:click={openModal} />
+	<Icon
+		src={InformationCircle}
+		theme="solid"
+		class="h-4 w-4 fill-current"
+		aria-hidden="true"
+		on:click={openModal}
+	/>
 </button>
 
 <Modal bind:isOpen={modalOpen} {title}>
