2976fd6d2af0bc5b5006999374fdcbcf1170004d
ChrisKnightLDN <chris.knight@london.gov.uk>
2024-03-27 14:02:51 +0000

    add context wrappers for sidebar placement to docs

--- apps/docs/src/lib/index.ts
+++ apps/docs/src/lib/index.ts
@@ -1,2 +1,4 @@
 // Reexport your entry components here
 export { default as RelativeWrapper } from './decorators/RelativeWrapper.svelte';
+export { default as SidebarLeftContext } from './decorators/SidebarLeftContext.svelte';
+export { default as SidebarTopContext } from './decorators/SidebarTopContext.svelte';
