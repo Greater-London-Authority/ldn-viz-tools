4192681d6854cc7952944e231e6a51631d7c2346
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-04-08 11:08:34 +0100

    Implement generic sidebar component (#216)
    ---------
    
    Co-authored-by: Paul Williams <Paul.Williams@london.gov.uk>
    Co-authored-by: ChrisKnightLDN <chris.knight@london.gov.uk>
    Co-authored-by: Chris Knight <111759008+ChrisKnightLDN@users.noreply.github.com>

---
+++ .changeset/hhfuse-wew-wqwe.md
@@ -0,0 +1,8 @@
+---
+'@ldn-viz/ui': major
+---
+
+ADDED - `AppShell` and `Sidebar` components. 
+CHANGED - increased z-index of `Modal`.
+CHANGED - `TabList` component now accpts an `orientation` prop.
+REMOVED - `TabbedSidebar` component (and `TabbedSidebarTabLabel`, `TabbedSidebarTabList`, `TabbedSidebarWrapper`)
--- packages/ui/src/lib/tabs/TabList.svelte
+++ packages/ui/src/lib/tabs/TabList.svelte
@@ -1,17 +1,31 @@
 <script lang="ts">
 	import { setContext } from 'svelte';
 	import { writable, type Writable } from 'svelte/store';
+	import { classNames } from '../utils/classNames';
 
 	export let selectedValue: string | undefined = undefined;
+	export let orientation: 'vertical' | 'horizontal' = 'horizontal';
+
 	const val: Writable<string | undefined> = writable(selectedValue);
 	val.subscribe((newVal) => (selectedValue = newVal));
-	setContext('selectedValue', {
-		selectedValue: val
+	setContext('tabContext', {
+		selectedValue: val,
+		orientation
 	});
+
+	const darkThemeClasses = 'dark:bg-core-grey-800 dark:text-white';
+	const lightThemeClasses = 'bg-white text-core-grey-700';
+
+	const themeClasses = [darkThemeClasses, lightThemeClasses];
+
+	const orientationClasses = {
+		vertical: 'flex-col w-20 space-y-0.5 pb-0.5',
+		horizontal: 'flex border-b-4 border-b-core-blue-600 w-full pt-5 pb-0 space-x-0.5 items-end'
+	};
+
+	$: tabListClasses = classNames(...themeClasses, orientationClasses[orientation], $$props.class);
 </script>
 
-<div
-	class="flex bg-white dark:bg-core-grey-800 border-b-4 border-b-core-blue-600 w-full pt-5 px-4 pb-0 space-x-0.5 items-end"
->
+<div class={tabListClasses} role="tablist">
 	<slot />
 </div>
