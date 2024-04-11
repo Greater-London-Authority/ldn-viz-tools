804c384b301cebf25eb869cb818d6d91c782dbdc
ChrisKnightLDN <chris.knight@london.gov.uk>
2024-04-11 15:07:12 +0100

    fix sidebar modal after Modal component update

--- packages/ui/src/lib/sidebar/elements/sidebarHint/SidebarHintModal.svelte
+++ packages/ui/src/lib/sidebar/elements/sidebarHint/SidebarHintModal.svelte
@@ -2,6 +2,7 @@
 <script lang="ts">
 	import { InformationCircle } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
+	import { writable } from 'svelte/store';
 	import Button from '../../../button/Button.svelte';
 	import Modal from '../../../modal/Modal.svelte';
 
@@ -9,14 +10,13 @@
 	export let modalTitle: string = 'About';
 	export let modalDescription: string = '';
 
-	// let isOpen = writable(false);
-	let isOpen = false;
+	let isOpen = writable(false);
 </script>
 
 <!-- Force button styles to match sidebar tooltip etc -->
 <Button
 	variant="text"
-	on:click={() => (isOpen = true)}
+	on:click={() => ($isOpen = true)}
 	class="!p-0 !text-core-grey-400 dark:!text-core-grey-300"
 >
 	{hintLabel}
