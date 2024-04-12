a26ee08cbc5fe6a753629edb9cefd70173c00aeb
ChrisKnightLDN <chris.knight@london.gov.uk>
2024-04-12 14:04:14 +0100

    sidebar modal can accept width

--- packages/ui/src/lib/sidebar/elements/sidebarHint/SidebarHint.stories.svelte
+++ packages/ui/src/lib/sidebar/elements/sidebarHint/SidebarHint.stories.svelte
@@ -25,6 +25,16 @@
 	</SidebarHint>
 </Story>
 
+<Story name="Modal width" source>
+	<SidebarHint hintType="modal" modalWidth="5xl">
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+		voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	</SidebarHint>
+</Story>
+
 <Story name="Tooltip" source>
 	<SidebarHint hintType="tooltip">
 		'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
--- packages/ui/src/lib/sidebar/elements/sidebarHint/SidebarHint.svelte
+++ packages/ui/src/lib/sidebar/elements/sidebarHint/SidebarHint.svelte
@@ -7,6 +7,20 @@
 
 	export let modalTitle: string | undefined = undefined;
 	export let modalDescription: string | undefined = undefined;
+	export let modalWidth:
+		| 'sm'
+		| 'md'
+		| 'lg'
+		| 'xs'
+		| 'xl'
+		| '2xl'
+		| '3xl'
+		| '4xl'
+		| '5xl'
+		| '6xl'
+		| '7xl'
+		| 'full'
+		| undefined = undefined;
 </script>
 
 {#if hintType === 'tooltip'}
@@ -18,7 +32,7 @@
 		<slot />
 	</Tooltip>
 {:else if hintType === 'modal'}
-	<SidebarHintModal {hintLabel} {modalTitle} {modalDescription}>
+	<SidebarHintModal {hintLabel} {modalTitle} {modalDescription} {modalWidth}>
 		<slot />
 	</SidebarHintModal>
 {/if}
