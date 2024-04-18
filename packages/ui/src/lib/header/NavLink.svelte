1134899375f42905fa1f075a81078d71c339f71a
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-04-18 16:54:23 +0100

    Fix NavLink highlighting
    If target for a link is "", only highlighted when on the homepage, rath than on every page.

--- packages/ui/src/lib/header/NavLink.svelte
+++ packages/ui/src/lib/header/NavLink.svelte
@@ -6,7 +6,9 @@
 
 	let classes: string;
 	$: {
-		if (($page.url?.pathname || '').endsWith(target)) {
+                if (!target && $page.url?.pathname === "/") { 
+			classes = 'flex h-[50px] bg-core-blue-500 items-center px-4 py-2 items-center';
+		} else if (!!target && ($page.url?.pathname || '').endsWith(target)) {
 			classes = 'flex h-[50px] bg-core-blue-500 items-center px-4 py-2 items-center';
 		} else {
 			classes = 'flex h-[50px] bg-core-grey-700 items-center px-4 py-2 items-center';
