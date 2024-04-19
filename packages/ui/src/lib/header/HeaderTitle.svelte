5905192a4da9f8e9c3af0a2b62edbb79f66cd637
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-04-19 15:44:47 +0000

    lint

--- packages/ui/src/lib/header/HeaderTitle.svelte
+++ packages/ui/src/lib/header/HeaderTitle.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	export let base = ""; // typcally imported from '$app/paths' then pased as prop
+	export let base = ''; // typcally imported from '$app/paths' then pased as prop
 </script>
 
 <a href={base || '/'}><span class="mr-4 text-lg"><slot /></span></a>
--- packages/ui/src/lib/header/NavLink.svelte
+++ packages/ui/src/lib/header/NavLink.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-	import type { Readable } from "svelte/store";
+	import type { Readable } from 'svelte/store';
 
-	// really a Page is 
+	// really a Page is
 	// import type { Page } from "@sveltejs/kit";
 	// and the page store is Readable<Page<Record<string, string>, string | null>>
 	// but we use Readable<{url: {pathname: string}} as an approximation to avoid a dependency on SvelteKit
 
-	export let base = ""; // typcally imported from '$app/paths' then pased as prop
-	export let page: Readable<{url?: {pathname: string}}>; // typically imported from '$app/stores then passed as prop
+	export let base = ''; // typcally imported from '$app/paths' then pased as prop
+	export let page: Readable<{ url?: { pathname: string } }>; // typically imported from '$app/stores then passed as prop
 
 	export let target = '';
 
 	let classes: string;
 	const highlightedClasses = 'flex h-[50px] bg-core-blue-500 items-center px-4 py-2 items-center';
-	const nonHighlightedClasses = 'flex h-[50px] bg-core-grey-700 items-center px-4 py-2 items-center';
+	const nonHighlightedClasses =
+		'flex h-[50px] bg-core-grey-700 items-center px-4 py-2 items-center';
 	$: {
-		if (!page){
+		if (!page) {
 			// no page store provided - we don't know whether or not we're on the page this links points at
 			classes = nonHighlightedClasses;
 		} else if (!target && $page.url?.pathname === '/') {
