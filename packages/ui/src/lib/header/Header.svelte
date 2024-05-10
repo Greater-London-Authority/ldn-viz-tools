dd552def3e0d54501aca39ba8f779853cbb4d6da
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-05-10 15:23:22 +0100

    [docs] add more docs for Header components (#325)

--- packages/ui/src/lib/header/Header.svelte
+++ packages/ui/src/lib/header/Header.svelte
@@ -1,5 +1,15 @@
+<script context="module" lang="ts">
+	/**
+	 * The `<Header>` component appears at the top of a page.
+	 * It typically includes the app name, links to other pages (if the app has multiple pages), and a login/logout control (if the app requires authentication).
+	 *
+	 * @component
+	 */
+</script>
+
 <nav
 	class="w-screen flex items-center px-4 py-[.5rem] bg-core-grey-900 text-white text-left h-[50px] border-l-[5px] border-core-blue-500"
 >
+	<!-- Contents of the header (typically a combination of `<HeaderTitle>`, `<HeaderRight>`, `<NavLinks>`, and `<HeaderItem>` components) -->
 	<slot />
 </nav>
--- packages/ui/src/lib/header/HeaderRight.svelte
+++ packages/ui/src/lib/header/HeaderRight.svelte
@@ -1,3 +1,8 @@
+<!--
+	A container for content to be positioned at the right-hand side of a `<Header>`.
+-->
+
 <div class="flex flex-grow items-center justify-end">
+	<!-- Contents of the header (typically a combination of `<NavLinks>` and `<HeaderItem>`) -->
 	<slot />
 </div>
