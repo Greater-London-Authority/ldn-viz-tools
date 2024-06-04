42ed72549a1b5d473bb55a440f4386a85fe7cfb6
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-06-04 09:57:33 +0000

    initial commit of Popover component

---
+++ .changeset/nsfn-fsfs-sdsd.md
@@ -0,0 +1,5 @@
+---
+'@ldn-viz/ui': minor
+---
+
+ADDED: added `Popover` component as an alternative to `Modal` and `Tooltip`
---
+++ packages/ui/src/lib/popover/Popover.stories.svelte
@@ -0,0 +1,49 @@
+<script context="module">
+	import Button from '../button/Button.svelte';
+	import Popover from './Popover.svelte';
+
+	export const meta = {
+		title: 'Ui/Popover',
+		component: Popover
+	};
+</script>
+
+<script lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+	<Popover>
+		<svelte:fragment slot="title">Metric note</svelte:fragment>
+
+		The contents of the popover...
+	</Popover>
+</Template>
+
+<Story name="Default" source />
+
+<Story name="No title" source>
+	<Popover>
+		<svelte:fragment>Some text explaining this metric...</svelte:fragment>
+	</Popover>
+</Story>
+
+<Story name="Custom hint text" source>
+	<Popover hintLabel="Click for more information!">
+		<svelte:fragment slot="title">Metric note</svelte:fragment>
+
+		<svelte:fragment>Some text explaining this metric...</svelte:fragment>
+	</Popover>
+</Story>
+
+<Story name="Custom trigger" source>
+	<Popover hintLabel="Click for more information!">
+		<svelte:fragment slot="hint"
+			><Button>Click here to see popover contents</Button></svelte:fragment
+		>
+
+		<svelte:fragment slot="title">Metric note</svelte:fragment>
+
+		<svelte:fragment>Some text explaining this metric...</svelte:fragment>
+	</Popover>
+</Story>
