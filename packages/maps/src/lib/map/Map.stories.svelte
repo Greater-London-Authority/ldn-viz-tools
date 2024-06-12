be54bea4f932ab63bb21a8a311eed50d8eda3797
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-06-12 13:26:22 +0000

    restore some explanatory text that was removed from stories

--- packages/maps/src/lib/map/Map.stories.svelte
+++ packages/maps/src/lib/map/Map.stories.svelte
@@ -1,14 +1,14 @@
 <script>
-	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
 
-	import * as os_light_vts from '../themes/os_light_vts.json';
-	import * as greyStyle from '../themes/os_greyscale.json';
-	import * as darkGreyMutedStyle from '../themes/os_dark_grey_muted_buildings.json';
 	import * as darkStyle from '../themes/os_dark.json';
+	import * as darkGreyMutedStyle from '../themes/os_dark_grey_muted_buildings.json';
+	import * as greyStyle from '../themes/os_greyscale.json';
+	import * as os_light_vts from '../themes/os_light_vts.json';
 
+	import loadTestLayers from '../loadTestLayers';
 	import Map, { appendOSKeyToUrl } from './Map.svelte';
 	import PropertiesStory from './PropertiesStory.svelte';
-	import loadTestLayers from '../loadTestLayers';
 
 	const OS_KEY = 'vmRzM4mAA1Ag0hkjGh1fhA2hNLEM6PYP';
 </script>
@@ -73,6 +73,10 @@
 	</div>
 </Template>
 
+<!--
+This is our default light basemap.
+It uses the Ordnance Survey's [OS_VTS_3857_Light.json](https://github.com/OrdnanceSurvey/OS-Vector-Tile-API-Stylesheets) stylesheet.
+-->
 <Story name="Light OS Basemap">
 	<div class="w-[100dvw] h-[100dvh]">
 		<Map
@@ -85,6 +89,10 @@
 	</div>
 </Story>
 
+<!--
+This is the greyscale basemap used on the Cool Spaces map. 
+It is very similar to the Ordnance Survey's [OS_VTS_3857_Greyscale.json](https://github.com/OrdnanceSurvey/OS-Vector-Tile-API-Stylesheets) stylesheet, with a few tweaks.
+ -->
 <Story name="Greyscale OS Basemap">
 	<div class="w-[100dvw] h-[100dvh]">
 		<Map
@@ -97,6 +105,7 @@
 	</div>
 </Story>
 
+<!-- This was created by Mike Brondbjerg based on the Ordnance Survey's greyscale theme. -->
 <Story name="Dark Grey, muted buildings">
 	<div class="w-[100dvw] h-[100dvh]">
 		<Map
@@ -109,6 +118,10 @@
 	</div>
 </Story>
 
+<!--
+This was created by the Ordnance Survey, inspired by Mike Brondbjerg's dark gray theme with muted buildings.
+It uses the Ordnance Survey's [OS_VTS_3857_Dark.json](https://github.com/OrdnanceSurvey/OS-Vector-Tile-API-Stylesheets) sylesheet.
+-->
 <Story name="Dark OS Basemap">
 	<div class="w-[100dvw] h-[100dvh]">
 		<Map
@@ -121,6 +134,7 @@
 	</div>
 </Story>
 
+<!-- This story registers event handlers to identify the layers underneath the point where the user clicks. -->
 <Story name="Properties">
 	<PropertiesStory />
 </Story>
