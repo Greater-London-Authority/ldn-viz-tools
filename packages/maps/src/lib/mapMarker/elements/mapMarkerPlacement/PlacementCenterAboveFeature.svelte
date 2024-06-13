84e949d7bb122146c42f362f8eef195a54e8926d
Paul Williams <Paul.Williams@london.gov.uk>
2024-06-13 16:13:25 +0100

    Use maplibre popup for tooltips and popups

--- packages/maps/src/lib/mapMarker/elements/mapMarkerPlacement/PlacementCenterAboveFeature.svelte
+++ packages/maps/src/lib/mapMarker/elements/mapMarkerPlacement/PlacementCenterAboveFeature.svelte
@@ -4,12 +4,10 @@
 
 	const feature = getContext('mapMarkerFeature');
 	const maplibrePopup = getContext('mapMarkerMaplibrePopup');
-	const maplibreMarker = getContext('mapMarkerMaplibreMarker');
 
 	if (feature.geometry) {
 		const point = pointOnFeature(feature);
-		maplibrePopup?.setLngLat(point.geometry.coordinates);
-		maplibreMarker?.setLngLat(point.geometry.coordinates);
+		maplibrePopup.setLngLat(point.geometry.coordinates);
 	}
 </script>
 
