1983802e0ce89faf44ce4d7b45d6c38f0cae77ca
ChrisKnightLDN <chris.knight@london.gov.uk>
2024-06-25 11:14:41 +0100

    update theme string replace in references

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,12 +1,12 @@
 // const StyleDictionary = require('style-dictionary');
-const StyleDictionary = require("style-dictionary-utils");
+const StyleDictionary = require('style-dictionary-utils');
 // This function traverses and replaces oldStr with newStr in the object
 const traverseAndReplace = (obj, oldStr, newStr) => {
-  if (typeof obj === "string") {
+  if (typeof obj === 'string') {
     return obj.replace(oldStr, newStr);
   } else if (Array.isArray(obj)) {
     return obj.map((item) => traverseAndReplace(item, oldStr, newStr));
-  } else if (typeof obj === "object" && obj !== null) {
+  } else if (typeof obj === 'object' && obj !== null) {
     const result = {};
     for (const key in obj) {
       if (obj.hasOwnProperty(key)) {
@@ -21,7 +21,7 @@ const traverseAndReplace = (obj, oldStr, newStr) => {
 
 // This function replaces strings in the mode within the semantic key of the object
 const replaceStringInMode = (obj, mode, oldStr, newStr) => {
-  if (typeof obj !== "object" || obj === null) {
+  if (typeof obj !== 'object' || obj === null) {
     return obj;
   }
 
@@ -42,7 +42,7 @@ const replaceStringInMode = (obj, mode, oldStr, newStr) => {
 
 // This function renames keys in the object based on the keyMap
 const renameKeysDeep = (obj, keyMap) => {
-  if (typeof obj !== "object" || obj === null) {
+  if (typeof obj !== 'object' || obj === null) {
     return obj;
   }
 
@@ -68,34 +68,34 @@ StyleDictionary.registerParser({
 
       // Replace the references to modes
       const lightParsed = replaceStringInMode(
-        jsonObj["variables"],
-        "light-mode",
-        "semantic.dark-mode", // Reference to incorrect mode prior to replacement
-        "theme.light"
+        jsonObj['variables'],
+        'light-mode',
+        'semantic.dark-mode', // Reference to incorrect mode prior to replacement
+        'theme.light'
       );
 
       const darkParsed = replaceStringInMode(
         lightParsed,
-        "dark-mode",
-        "semantic.dark-mode",
-        "theme.dark"
+        'dark-mode',
+        'semantic.dark-mode',
+        'theme.dark'
       );
 
       const parsedJsonObj = renameKeysDeep(darkParsed, {
-        semantic: "theme",
-        "light-mode": "light",
-        "dark-mode": "dark",
+        semantic: 'theme',
+        'light-mode': 'light',
+        'dark-mode': 'dark'
       });
 
       return parsedJsonObj;
     } catch (error) {
       console.error(error);
     }
-  },
+  }
 });
 
-console.log("Build started...");
-console.log("\n==============================================");
+console.log('Build started...');
+console.log('\n==============================================');
 
 /*
  THESE ARE CONTITIONS TO FILTER ON AND A HELPER FUNCTION TO CREATE MATCHING FILTERS
@@ -104,35 +104,35 @@ console.log("\n==============================================");
  The objects defined here are allowed through - everything else is filtered out. Acts as a yes gate
  The not: array allows for partial filtering of object keys. Acts as a no gate
 */
-const conditionsCssLightTheme = [{ category: "theme", type: "light" }];
+const conditionsCssLightTheme = [{ category: 'theme', type: 'light' }];
 
-const conditionsCssDarkTheme = [{ category: "theme", type: "dark" }];
+const conditionsCssDarkTheme = [{ category: 'theme', type: 'dark' }];
 
 const conditionsCssGlobal = [
   // token.attributes.category === 'global' || token.attributes.category === 'spacing'
-  { category: "global", subitem: { not: ["seed"] } },
-  { category: "spacing" },
+  { category: 'global', subitem: { not: ['seed'] } },
+  { category: 'spacing' }
 ];
 
 const conditionsTw = [
-  { category: "theme", type: "light" },
-  { category: "global", subitem: { not: ["light", "dark", "seed"] } },
-  { category: "spacing" },
+  { category: 'theme', type: 'light' },
+  { category: 'global', subitem: { not: ['light', 'dark', 'seed'] } },
+  { category: 'spacing' }
 ];
 
 const conditionsJs = [
-  { category: "theme" },
+  { category: 'theme' },
   {
-    category: "global",
-    type: { not: ["spacing"] },
-    item: { not: ["mode"] },
-    subitem: { not: ["seed"] },
-  },
+    category: 'global',
+    type: { not: ['spacing'] },
+    item: { not: ['mode'] },
+    subitem: { not: ['seed'] }
+  }
 ];
 
 const matchesCondition = (token, condition) => {
   return Object.entries(condition).every(([key, value]) => {
-    if (typeof value === "object" && value.not) {
+    if (typeof value === 'object' && value.not) {
       return !value.not.includes(token.attributes[key]);
     }
     return token.attributes[key] === value;
@@ -143,46 +143,45 @@ const tokenMatchesAnyCondition = (token, conditions) => {
   return conditions.some((condition) => matchesCondition(token, condition));
 };
 
-const createMatcher = (conditions) => (token) =>
-  tokenMatchesAnyCondition(token, conditions);
+const createMatcher = (conditions) => (token) => tokenMatchesAnyCondition(token, conditions);
 
 // REGISTER THE CUSTOM FILTERS USING OUR MATCHING CONDITIONS
 
 StyleDictionary.registerFilter({
-  name: "cssGlobalFilter",
-  matcher: (token) => createMatcher(conditionsCssGlobal)(token),
+  name: 'cssGlobalFilter',
+  matcher: (token) => createMatcher(conditionsCssGlobal)(token)
 });
 
 StyleDictionary.registerFilter({
-  name: "cssLightThemeFilter",
-  matcher: (token) => createMatcher(conditionsCssLightTheme)(token),
+  name: 'cssLightThemeFilter',
+  matcher: (token) => createMatcher(conditionsCssLightTheme)(token)
 });
 
 StyleDictionary.registerFilter({
-  name: "cssDarkThemeFilter",
-  matcher: (token) => createMatcher(conditionsCssDarkTheme)(token),
+  name: 'cssDarkThemeFilter',
+  matcher: (token) => createMatcher(conditionsCssDarkTheme)(token)
 });
 
 StyleDictionary.registerFilter({
-  name: "twFilter",
-  matcher: (token) => createMatcher(conditionsTw)(token),
+  name: 'twFilter',
+  matcher: (token) => createMatcher(conditionsTw)(token)
 });
 
 StyleDictionary.registerFilter({
-  name: "jsFilter",
-  matcher: (token) => createMatcher(conditionsJs)(token),
+  name: 'jsFilter',
+  matcher: (token) => createMatcher(conditionsJs)(token)
 });
 
 /**
  * Custom format that handle reference in css variables
  */
-const transformString = (str, replace = "theme-", regex = /.*-color/) => {
+const transformString = (str, replace = 'theme', regex = /.*-color/) => {
   // Use a regular expression to replace everything before regex with replace
   return str.replace(regex, replace);
 };
 
 StyleDictionary.registerFormat({
-  name: "css/variables",
+  name: 'css/variables',
   formatter({ dictionary }) {
     return `${this.options.selector} {
           ${dictionary.allTokens
@@ -190,16 +189,14 @@ StyleDictionary.registerFormat({
               let originalName = token.name;
 
               let themedName =
-                token.path[0] === "global"
+                token.path[0] === 'global'
                   ? originalName
-                  : transformString(originalName, "theme-", /.*-color-/);
+                  : transformString(originalName, 'theme-', /.*-color-/);
 
               if (dictionary.usesReference(token.original.value)) {
-                const reference = dictionary.getReferences(
-                  token.original.value
-                );
+                const reference = dictionary.getReferences(token.original.value);
                 const referenceName =
-                  reference[0].path[0] === "global"
+                  reference[0].path[0] === 'global'
                     ? reference[0].name
                     : transformString(reference[0].name);
                 return `  --${themedName}: var(--${referenceName}, ${token.value});`;
@@ -207,9 +204,9 @@ StyleDictionary.registerFormat({
 
               return `  --${themedName}: ${token.value};`;
             })
-            .join("\n")}
+            .join('\n')}
         }`;
-  },
+  }
 });
 
 /**
@@ -217,41 +214,35 @@ StyleDictionary.registerFormat({
  */
 
 StyleDictionary.registerFormat({
-  name: "tw/css-variables",
+  name: 'tw/css-variables',
   formatter({ dictionary }) {
     return (
-      "module.exports = " +
+      'module.exports = ' +
       `{\n${dictionary.allTokens
         .map((token) => {
           let originalName = token.name;
 
           let themedName =
-            token.path[0] === "global"
-              ? transformString(originalName, "global-color")
-              : transformString(originalName, "theme-", /.*-color-/);
+            token.path[0] === 'global'
+              ? transformString(originalName, 'global-color')
+              : transformString(originalName, 'theme-', /.*-color-/);
 
-          let transformedName = transformString(
-            originalName,
-            "color-",
-            /.*-color-/
-          );
+          let transformedName = transformString(originalName, 'color-', /.*-color-/);
 
           return `  "${transformedName}": "var(--${themedName}, ${token.value})"`;
         })
-        .join(",\n")}\n}`
+        .join(',\n')}\n}`
     );
-  },
+  }
 });
 
 // APPLY THE CONFIGURATION
 // IMPORTANT: the registration of custom transforms
 // needs to be done _before_ applying the configuration
-const StyleDictionaryExtended = StyleDictionary.extend(
-  __dirname + "/config.json"
-);
+const StyleDictionaryExtended = StyleDictionary.extend(__dirname + '/config.json');
 
 // FINALLY, BUILD ALL THE PLATFORMS
 StyleDictionaryExtended.buildAllPlatforms();
 
-console.log("\n==============================================");
-console.log("\nBuild completed!");
+console.log('\n==============================================');
+console.log('\nBuild completed!');
