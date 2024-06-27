29787253babffd4f780a8bb2facb5c77801ff91b
ChrisKnightLDN <chris.knight@london.gov.uk>
2024-06-27 09:42:59 +0100

    update switch

--- packages/ui/src/lib/switch/Switch.svelte
+++ packages/ui/src/lib/switch/Switch.svelte
@@ -45,20 +45,24 @@
 </script>
 
 <div class="flex items-center">
-	<label class="pr-4 leading-none text-black" for="airplane-mode" id="airplane-mode-label">
+	<label
+		class="pr-4 leading-none text-color-input-label"
+		for="airplane-mode"
+		id="airplane-mode-label"
+	>
 		{label}
 	</label>
 	<button
 		{...$root}
 		use:root
-		class="relative h-6 cursor-default rounded-full bg-core-grey-200 transition-colors data-[state=checked]:bg-core-blue-600 w-11 p-0.5"
+		class="relative h-6 cursor-default rounded-full bg-color-input-background-off transition-colors data-[state=checked]:bg-color-input-background-on w-11 p-0.5"
 		id="airplane-mode"
 		aria-labelledby="airplane-mode-label"
 	>
 		<span
 			class={classNames(
 				'thumb w-5 h-5 block rounded-full transition',
-				disabled ? 'bg-core-grey-100' : 'bg-white'
+				disabled ? 'bg-color-palette-grey-100' : 'bg-color-input-background'
 			)}
 			style:transform={$checked ? `translate(20px, 0px)` : ''}
 		/>
