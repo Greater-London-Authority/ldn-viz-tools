c351d365ae4d8f62712a26060f25793e0e6cfbb9
James Scott-Brown <james.scott-brown@london.gov.uk>
2024-07-17 14:39:50 +0100

    lint

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -16,7 +16,7 @@ export { default as NavLinks } from './header/NavLinks.svelte';
 export { default as ImageDownloadButton } from './imageDownloadButton/ImageDownloadButton.svelte';
 export { default as Input } from './input/Input.svelte';
 export { default as InputWrapper } from './input/InputWrapper.svelte';
-export { default as LayerControl } from './layerControl/LayerControl.svelte'
+export { default as LayerControl } from './layerControl/LayerControl.svelte';
 export { default as LogoByCIU } from './logos/LogoByCIU.svelte';
 export { default as LogoCIU } from './logos/LogoCIU.svelte';
 export { default as LogoLOTI } from './logos/LogoLOTI.svelte';
