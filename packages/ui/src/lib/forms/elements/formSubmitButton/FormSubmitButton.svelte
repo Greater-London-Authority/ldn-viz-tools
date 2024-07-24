bc1c4ab83228deefcbbf4c417b26ef25278ba3e2
Paul Williams <paul.williams@london.gov.uk>
2024-07-24 16:29:14 +0100

    Add docs & add custom input story

--- packages/ui/src/lib/forms/elements/formSubmitButton/FormSubmitButton.svelte
+++ packages/ui/src/lib/forms/elements/formSubmitButton/FormSubmitButton.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
+	/**
+	 * The `<FormSubmitButton>` component is the standard way to add a submit
+	 * button to a `<Form>`. The component can only be used as slotted content
+	 * in parent components that set a `formActiveButtonStore` in context such
+	 * as `<Form>`.
+	 * @component
+	 */
+
 	import { getContext } from 'svelte';
 	import Button from '../../../button/Button.svelte';
 	import AsyncButton from '../../AsyncButton.svelte';
@@ -6,8 +14,25 @@
 
 	const activeButtonStore: FormActiveButtonStore = getContext('formActiveButtonStore');
 
+	/**
+	 * Called when the button is clicked.
+	 */
 	export let onSubmit: FormButtonhandler;
+
+	/**
+	 * If `true`, switches to a spinner while the `onSubmit` function is being
+	 * executed.
+	 */
 	export let async = false;
+
+	/**
+	 * Is set to `true` when the `onSubmit` function is being executed so
+	 * intended use is to bind on it to reactively know when a submission is
+	 * in progess.
+	 *
+	 * Working will return to `false` once the function, possible including a
+	 * retuned promise, has finished executing.
+	 */
 	export let working = false;
 
 	const onClick: FormButtonhandler = async (event) => {
@@ -36,6 +61,7 @@
 		bind:working
 		{...$$restProps}
 	>
+		<!-- Text and icons slotted into the button, defaults to "Submit". -->
 		<slot>Submit</slot>
 	</AsyncButton>
 {:else}
