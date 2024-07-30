4da0c674fc31862eb6a08378f3225754293b17b1
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-07-30 10:45:21 +0100

    allow SidebarSection component to accept a SidebarSectionTitle compon… (#412)
    * allow SidebarSection component to accept a SidebarSectionTitle component in title prop
    
    * tighten typography and spacing in sidebar section
    
    ---------
    
    Co-authored-by: Chris Knight <chris.knight@london.gov.uk>

---
+++ .changeset/sfsdf-fsdfsdf-fsdfsdf.md
@@ -0,0 +1,5 @@
+---
+'@ldn-viz/ui': minor
+---
+
+CHANGED: the `<SidebarSection>` component can now accept a `<SidebarSectionTitle>` component inside the `title` slot.
--- packages/ui/src/lib/sidebar/elements/sidebarSection/SidebarSection.stories.svelte
+++ packages/ui/src/lib/sidebar/elements/sidebarSection/SidebarSection.stories.svelte
@@ -11,6 +11,7 @@
 	import { Story, Template } from '@storybook/addon-svelte-csf';
 	import SidebarHint from '../sidebarHint/SidebarHint.svelte';
 	import SidebarGroupTitle from './sidebarGroupTitle/SidebarGroupTitle.svelte';
+	import SidebarSectionTitle from './sidebarSectionTitle/SidebarSectionTitle.svelte';
 </script>
 
 <Template let:args>
@@ -24,6 +25,39 @@
 <Story name="With Titled Group" source>
 	<SidebarSection title="Section Title">
 		Section Content
+		<div>
+			<SidebarGroupTitle>
+				Pay Attention to this group
+				<SidebarHint slot="hint" hintType="modal" hintLabel="why">
+					Beacuse it's Awesome!
+				</SidebarHint>
+			</SidebarGroupTitle>
+			Grouped content
+		</div>
+	</SidebarSection>
+</Story>
+
+<!-- You can add a subtitle or hint text by providing a `<SidebarSection>` component within the `title` slot. -->
+<Story name="With SidebarSectionTitle component" source>
+	<SidebarSection title="Section Title">
+		<svelte:fragment slot="title">
+			<SidebarSectionTitle>
+				<svelte:fragment>The title of this section</svelte:fragment>
+
+				<svelte:fragment slot="subTitle">The sub-title of ths section.</svelte:fragment>
+
+				<SidebarHint slot="hint" hintLabel="About">
+					This popover should provide some additional explanation about this section.
+				</SidebarHint>
+			</SidebarSectionTitle>
+		</svelte:fragment>
+
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+		voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
 		<div>
 			<SidebarGroupTitle>
 				Pay Attention to this group
