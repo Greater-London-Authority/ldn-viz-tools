e8133170e1f77b7a56fa518168831b9a4b2801c8
Chris Knight <chris.knight@london.gov.uk>
2024-07-31 13:38:26 +0100

    supress unused css rules (temp?)

--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -12,6 +12,13 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+	},
+
+	onwarn: (warning, handler) => {
+		const { code, frame } = warning;
+		if (code === 'css-unused-selector') return;
+
+		handler(warning);
 	}
 };
 
