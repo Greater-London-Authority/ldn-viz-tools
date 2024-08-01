7633e405e8ee21f7cbc6693192473f1d40f89331
Paul Williams <paul.williams@london.gov.uk>
2024-08-01 10:44:23 +0100

    Fix lint issue

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,6 +1,6 @@
 // Reexport your entry components here
 export { default as Button } from './button/Button.svelte';
-export { default as AsyncButton } from './button/AsyncButton.svelte'
+export { default as AsyncButton } from './button/AsyncButton.svelte';
 
 export { default as Checkbox } from './checkBox/Checkbox.svelte';
 export { default as CheckboxGroup } from './checkBox/CheckboxGroup.svelte';
