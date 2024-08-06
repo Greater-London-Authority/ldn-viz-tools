21b41c600c1c5441cc19bc6b021985d96dabde99
Chris Knight <111759008+ChrisKnightLDN@users.noreply.github.com>
2024-08-06 10:34:00 +0100

    Fix input (#489)
    * fix input disabled color ternery
    
    * Update Input.svelte

--- packages/ui/src/lib/input/Input.svelte
+++ packages/ui/src/lib/input/Input.svelte
@@ -146,7 +146,7 @@
 	$: inputClasses = classNames(
 		'm-0',
 		error ? 'border-color-input-border-error' : '',
-		$$restProps.disabled ? 'cursor-not-allowed ' : '',
+		disabled ? 'cursor-not-allowed text-color-input-label-disabled' : '',
 		type === 'range' ? 'form-range' : 'form-input'
 	);
 </script>
