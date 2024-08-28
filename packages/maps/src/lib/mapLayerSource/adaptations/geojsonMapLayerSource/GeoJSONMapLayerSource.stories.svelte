ae67f11998cb4250874fa64210fb88338d1ff154
Paul Williams <paul.williams@london.gov.uk>
2024-08-28 15:21:05 +0100

    Add two way dataStore to GeoJSONMapLayerSOurce (#490)

--- packages/maps/src/lib/mapLayerSource/adaptations/geojsonMapLayerSource/GeoJSONMapLayerSource.stories.svelte
+++ packages/maps/src/lib/mapLayerSource/adaptations/geojsonMapLayerSource/GeoJSONMapLayerSource.stories.svelte
@@ -26,12 +26,21 @@
 					}
 				}
 			},
-			data: {
+			initialData: {
 				type: 'object',
 				control: 'none',
 				table: {
 					type: {
 						summary: 'GeoJSON'
+					}
+				}
+			},
+			dataStore: {
+				control: 'none',
+				table: {
+					type: {
+						summary: 'Writable Svelte store',
+						detail: 'writable<GeoJSON>'
 					}
 				}
 			},
@@ -42,15 +51,6 @@
 					type: {
 						summary: 'function',
 						detail: '(geojson: GeoJSON) => GeoJSON'
-					}
-				}
-			},
-			geojsonStore: {
-				control: 'none',
-				table: {
-					type: {
-						summary: 'Svelte store',
-						detail: 'writable<GeoJSON>'
 					}
 				}
 			},
@@ -92,6 +92,9 @@
 <script lang="ts">
 	import { Story, Template } from '@storybook/addon-svelte-csf';
 
+	import colors from '@ldn-viz/themes/colors.json';
+	import { Button } from '@ldn-viz/ui';
+
 	import Map, { appendOSKeyToUrl } from '../../../map/Map.svelte';
 	import MapLayerView from '../../../mapLayerView/MapLayerView.svelte';
 	import testData from '../../../testData.json';
@@ -104,6 +107,22 @@
 
 	const OS_KEY = 'vmRzM4mAA1Ag0hkjGh1fhA2hNLEM6PYP';
 	const sourceId = 'gla/ldn-viz-tools/test-data';
+
+	let dataStore;
+
+	const getTestGeoJSONByGeometryType = (type) => {
+		const newData = structuredClone(testData);
+
+		newData.features = newData.features.filter((f) => {
+			return f.geometry.type === type;
+		});
+
+		return newData;
+	};
+
+	const geojsonPoints = getTestGeoJSONByGeometryType('Point');
+	const geojsonLineStrings = getTestGeoJSONByGeometryType('LineString');
+	const geojsonPolygons = getTestGeoJSONByGeometryType('Polygon');
 </script>
 
 <Template let:args>
@@ -117,7 +136,66 @@
 				transformRequest: appendOSKeyToUrl(OS_KEY)
 			}}
 		>
-			<GeoJSONMapLayerSource id={sourceId} data={testData}>
+			<GeoJSONMapLayerSource id={sourceId} initialData={testData}>
+				<MapLayerView
+					id="{sourceId}/polygon"
+					spec={{
+						type: 'fill',
+						filter: ['==', '$type', 'Polygon'],
+						paint: {
+							'fill-color': colors.core.green['500'],
+							'fill-outline-color': colors.core.green['800'],
+							'fill-opacity': 0.6
+						}
+					}}
+				/>
+				<MapLayerView
+					id="{sourceId}/line"
+					spec={{
+						type: 'line',
+						filter: ['==', '$type', 'LineString'],
+						paint: {
+							'line-color': colors.core.darkPink['400'],
+							'line-width': 4,
+							'line-opacity': 0.8
+						},
+						layout: {
+							'line-join': 'round',
+							'line-cap': 'round'
+						}
+					}}
+				/>
+				<MapLayerView
+					id="{sourceId}/point"
+					spec={{
+						type: 'circle',
+						filter: ['==', '$type', 'Point'],
+						paint: {
+							'circle-color': colors.core.blue['700'],
+							'circle-radius': 6,
+							'circle-stroke-width': 1,
+							'circle-stroke-color': '#000'
+						}
+					}}
+				/>
+			</GeoJSONMapLayerSource>
+		</Map>
+	</div>
+</Story>
+
+<Story name="Updating via dataStore">
+	<div class="relative w-[100dvw] h-[100dvh]">
+		<div class="absolute top-4 left-4 z-40 flex gap-2">
+			<Button on:click={() => dataStore.set(geojsonPoints)}>Points</Button>
+			<Button on:click={() => dataStore.set(geojsonLineStrings)}>Lines</Button>
+			<Button on:click={() => dataStore.set(geojsonPolygons)}>Polygons</Button>
+		</div>
+		<Map
+			options={{
+				transformRequest: appendOSKeyToUrl(OS_KEY)
+			}}
+		>
+			<GeoJSONMapLayerSource id={sourceId} initialData={geojsonPoints} bind:dataStore>
 				<MapLayerView
 					id="{sourceId}/polygon"
 					spec={{
