62db7ab63e3a1c6a10ca70a0e46d4e8b83fb59de
James Scott-Brown <james.scott-brown@london.gov.uk>
2024-08-28 14:42:47 +0100

    revert changes to exportBtns

--- packages/charts/src/lib/chartContainer/ExportBtns.svelte
+++ packages/charts/src/lib/chartContainer/ExportBtns.svelte
@@ -5,24 +5,36 @@
 
 	export let chartToCapture: HTMLDivElement;
 	export let data: any;
-
-	/**
-	 * An optional object defining a mapping from the names of attributes in the `data` prop to the names of columns in the generated file.
-	 */
-	export let columnMapping: undefined | { [oldName: string]: string } = undefined;
 </script>
 
 <div
-	class="flex flex-col sm:flex-row shrink-0 sm:ml-auto sm:self-end capture-ignore gap-2"
+	class="flex flex-col sm:flex-row shrink-0 sm:ml-auto sm:self-end capture-ignore"
 	data-html2canvas-ignore
 >
 	<DataDownloadButton
 		{data}
-		{columnMapping}
-		filename="download"
-		formats={["CSV", "JSON"]}
+		filename="download.csv"
+		format="CSV"
+		variant="text"
+		emphasis="secondary"
+		size="sm"
 	>
-		Download <Icon
+		Download as CSV <Icon
+			src={ArrowDownTray}
+			theme="mini"
+			class="w-5 h-5 ml-2"
+			aria-hidden="true"
+		/>
+	</DataDownloadButton>
+	<DataDownloadButton
+		{data}
+		filename="download.json"
+		format="JSON"
+		variant="text"
+		emphasis="secondary"
+		size="sm"
+	>
+		Download as JSON<Icon
 			src={ArrowDownTray}
 			theme="mini"
 			class="w-5 h-5 ml-2"
@@ -31,8 +43,11 @@
 	</DataDownloadButton>
 
 	<ImageDownloadButton
-		formats={["PNG"]}
+		format="PNG"
 		htmlNode={chartToCapture}
+		variant="text"
+		emphasis="secondary"
+		size="sm"
 	>
 		Save as image<Icon src={Camera} theme="mini" class="w-5 h-5 ml-2" aria-hidden="true" />
 	</ImageDownloadButton>
