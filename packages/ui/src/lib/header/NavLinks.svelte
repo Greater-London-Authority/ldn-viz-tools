7ffd24306f6993bc94c0d5548f5c8cb8db1226fd
Chris Knight <chris.knight@london.gov.uk>
2024-09-06 10:23:51 +0100

    Header and modal header themed as dark by default

--- packages/ui/src/lib/header/NavLinks.svelte
+++ packages/ui/src/lib/header/NavLinks.svelte
@@ -3,7 +3,7 @@ A container for `<NavLink>` components.
 @component
 -->
 
-<div class="flex items-center">
+<nav class="flex items-center">
 	<!-- one or more `<NavLink>` components -->
 	<slot />
-</div>
+</nav>
