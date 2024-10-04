48cad69144cde68f64f199e1ae8331d494868a2c
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-10-04 10:46:33 +0000

    update tab highlighting when selected tab externally changed

---
+++ .changeset/tab-list-highligting.md
@@ -0,0 +1,4 @@
+---
+'@ldn-viz/ui': minor
+---
+CHANGED: update highlighting of tab icons inside a `TabList` or `SidebarTabList` when the selected item is changed externally 
--- packages/ui/src/lib/tabs/TabList.svelte
+++ packages/ui/src/lib/tabs/TabList.svelte
@@ -27,6 +27,13 @@
 		orientation
 	});
 
+	const respondToExternalChange = (newVal: string | undefined) => {
+		if ($val !== newVal) {
+			$val = newVal;
+		}
+	};
+	$: respondToExternalChange(selectedValue);
+
 	const orientationClasses = {
 		vertical: 'flex-col w-20 space-y-0.5 pb-0.5',
 		horizontal: 'flex border-b-4 border-b-color-ui-primary w-full pt-5 pb-0 space-x-0.5 items-end'
