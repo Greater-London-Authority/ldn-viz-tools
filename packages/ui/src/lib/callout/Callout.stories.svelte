9c08e963baa3ad6e4d98e7c681d6cf9f9075a43e
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2024-10-21 14:06:05 +0000

    add Callout component (copied form hsds-hub)

---
+++ packages/ui/src/lib/callout/Callout.stories.svelte
@@ -0,0 +1,52 @@
+<script context="module">
+	import Callout from './Callout.svelte';
+
+	export const meta = {
+		title: 'Ui/Callout',
+		component: Callout,
+		argTypes: {
+			status: {
+				options: ['note', 'caution', 'warning'],
+				control: { type: 'select' }
+			},
+		},
+	};
+</script>
+
+<script lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import Button from "../button/Button.svelte";
+</script>
+
+<Template let:args>
+	<Callout {...args}>
+		<svelte:fragment slot="title">The title!</svelte:fragment>
+
+		<svelte:fragment slot="body">The body!</svelte:fragment>
+	</Callout>
+</Template>
+
+<Story
+	name="Default"
+	source
+/>
+
+
+<Story name="With 'more'" source>
+	<Callout status="warning">
+		<span slot="title">Something has gone wrong</span>
+
+		<div slot="body">
+			<p>
+					Click on the button to return home. If the error persistes please <a
+						href="mailto:some-email-address@example.com"
+						class="underline hover:no-underline">Report it here</a
+					>
+			</p>
+		</div>
+
+		<div slot="more">
+			<Button href={`/`}>Take me home</Button>
+		</div>
+	</Callout>
+</Story>
---
+++ packages/ui/src/lib/callout/Callout.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { classNames } from '../utils/classNames';
+
+	export let status: 'note' | 'caution' | 'warning' = 'note';
+
+	const statusClasses = {
+		note: 'bg-core-blue-50 border-core-blue-500',
+		caution: 'bg-core-orange-50 border-core-orange-500 ',
+		warning: 'bg-core-red-50 border-core-red-500'
+	};
+
+	let calloutClasses: string;
+
+	$: calloutClasses = classNames(
+		'border-l-[5px] text-core-grey-700 px-4 pt-4 mt-4 ',
+		statusClasses[status]
+	);
+</script>
+
+<aside class={calloutClasses}>
+
+	<!-- The title - displayed as large bold text. -->
+	{#if $$slots.title}
+		<h3 class="font-bold leading-tight text-xl mb-4">
+			<slot name="title" />
+		</h3>
+	{/if}
+
+	<!-- The main body of the callout, displayed below the title. -->
+	{#if $$slots.body}
+		<div class="pb-4">
+			<slot name="body" />
+		</div>
+	{/if}
+
+	<!-- Any additional content to display below the body. -->
+	{#if $$slots.more}
+		<div class="pb-4">
+			<slot name="more" />
+		</div>
+	{/if}
+</aside>
