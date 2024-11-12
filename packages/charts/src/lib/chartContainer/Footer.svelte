3f4cff2cafc852b3738ced037a4ea14d4ffad3f4
Chris Knight <chris.knight@london.gov.uk>
2024-11-12 11:47:27 +0000

    use flexbox to determine export button position

--- packages/charts/src/lib/chartContainer/Footer.svelte
+++ packages/charts/src/lib/chartContainer/Footer.svelte
@@ -4,9 +4,9 @@
 	export let note = '';
 </script>
 
-<div class="w-full flex flex-col sm:flex-row justify-between mt-1">
+<div class="w-full flex flex-wrap justify-between mt-1 space-y-2">
 	{#if byline || source || note}
-		<ul class="flex flex-col space-y-0.5 text-color-text-secondary text-xs max-w-xl mr-4">
+		<ul class="flex flex-col space-y-0.5 text-color-text-secondary text-xs min-w-80 max-w-xl mr-4">
 			{#if byline}<li>{byline}</li>{/if}
 			{#if source}<li><span class="font-bold mr-1">Source:</span>{source}</li>{/if}
 			{#if note}<li><span class="font-bold mr-1">Note:</span>{note}</li>{/if}
