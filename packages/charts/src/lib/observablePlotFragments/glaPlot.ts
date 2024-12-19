9da94b7b639d2ea2fbfb9ec991120d199cc4115f
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2024-12-19 12:43:50 +0000

    created a glaPlot function to handle defaultPlotStyles and take in custom marks

---
+++ packages/charts/src/lib/observablePlotFragments/glaPlot.ts
@@ -0,0 +1,56 @@
+import themeTokens from '@ldn-viz/themes/styles/js/theme-tokens';
+import type { Mark } from '@observablehq/plot';
+import { fontStack, getDefaultPlotStyles, type ThemeMode } from './observablePlotFragments';
+
+interface PlotOptions {
+	size?: {};
+	color?: {};
+	xScale?: {};
+	yScale?: {};
+}
+
+// function that will take a marks prop and return a spec to be plotted
+export const glaPlot = (
+	mode: ThemeMode,
+	marks: Mark,
+	options: PlotOptions,
+	isFaceted: boolean = false
+) => {
+	const {
+		defaultStyle,
+		defaultSize,
+		defaultSizeFacet,
+		defaultColor,
+		defaultXScale,
+		defaultYScale
+	} = getDefaultPlotStyles(mode);
+
+	const spec = {
+		style: {
+			...defaultStyle
+		},
+		...defaultSize,
+		...options.size,
+		color: {
+			...defaultColor,
+			...options.color
+		},
+		x: {
+			defaultXScale,
+			...options.xScale
+		},
+		y: {
+			...defaultYScale,
+			...options.yScale
+		},
+		marks: { ...marks }
+	};
+
+	const facetedSpec = {
+		...spec,
+		...defaultSizeFacet,
+		...options.size
+	};
+
+	return isFaceted ? facetedSpec : spec;
+};
