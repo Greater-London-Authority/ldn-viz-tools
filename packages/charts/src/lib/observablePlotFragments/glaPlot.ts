ea23e42ab68b2bfc8bbf82c3738bb0dc9dfebce6
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2024-12-20 15:58:35 +0000

    importing types and removing unused types

--- packages/charts/src/lib/observablePlotFragments/glaPlot.ts
+++ packages/charts/src/lib/observablePlotFragments/glaPlot.ts
@@ -1,31 +1,27 @@
 import type {
-	Area,
-	Arrow,
-	AutoSpec,
-	BarX,
-	BarY,
-	Cell,
-	CompoundMark,
-	Contour,
-	Density,
-	Difference,
-	Dot,
-	Frame,
-	Geo,
-	Hexgrid,
-	Image,
-	Line,
-	Link,
-	Raster,
-	Rect,
-	RenderableMark,
-	RuleX,
-	RuleY,
-	Text,
-	TickX,
-	TickY,
-	Tip,
-	Vector
+	AreaOptions,
+	AreaXOptions,
+	AreaYOptions,
+	ArrowOptions,
+	AutoOptions,
+	AxisXOptions,
+	AxisYOptions,
+	BarXOptions,
+	BarYOptions,
+	Data,
+	DotOptions,
+	DotXOptions,
+	DotYOptions,
+	GridXOptions,
+	GridYOptions,
+	LineOptions,
+	LineXOptions,
+	LineYOptions,
+	PointerOptions,
+	RuleXOptions,
+	RuleYOptions,
+	TextOptions,
+	TipOptions
 } from '@observablehq/plot';
 import * as ObservablePlot from '@observablehq/plot';
 import { getDefaultPlotStyles, type ThemeMode } from './observablePlotFragments';
@@ -36,35 +32,33 @@ interface PlotOptions {
 	xScale?: {};
 	yScale?: {};
 }
-
-type Marks =
-	| Area
-	| Arrow
-	| AutoSpec
-	// | CompoundMark
-	| RuleX
-	| RuleY
-	| BarX
-	| BarY
-	| Cell
-	| Contour
-	| RenderableMark
-	| Density
-	| Difference
-	| Dot
-	| Frame
-	| Geo
-	| Hexgrid
-	| Image
-	| Line
-	| Link
-	| Raster
-	| Rect
-	| Text
-	| TickX
-	| TickY
-	| Tip
-	| Vector;
+// type Marks =
+// 	| Area
+// 	| Arrow
+// 	// | CompoundMark
+// 	| RuleX
+// 	| RuleY
+// 	| BarX
+// 	| BarY
+// 	| Cell
+// 	| Contour
+// 	| RenderableMark
+// 	| Density
+// 	| Difference
+// 	| Dot
+// 	| Frame
+// 	| Geo
+// 	| Hexgrid
+// 	| Image
+// 	| Line
+// 	| Link
+// 	| Raster
+// 	| Rect
+// 	| Text
+// 	| TickX
+// 	| TickY
+// 	| Tip
+// 	| Vector;
 
 // Function that handles default styles and takes data, mode and marks props.
 // It also optionally takes an options object which can handle overriding default styling
@@ -133,28 +127,68 @@ const {
 
 // Object contains a custom function for each mark, that wraps the existing mark but provides default styling and props
 export const Plot = {
-	gridX: (options: any) => ObservablePlot.gridX({ ...defaultGridX, ...options }),
-	gridY: (options: any) => ObservablePlot.gridY({ ...defaultGridY, ...options }),
-	axisX: (options: any) => ObservablePlot.axisX({ ...defaultXAxis, ...options }),
-	axisY: (options: any) => ObservablePlot.axisY({ ...defaultYAxis, ...options }),
-	line: (options: any) => ObservablePlot.line({ ...defaultLine, ...options }),
-	lineX: (options: any) => ObservablePlot.lineX({ ...defaultLine, ...options }),
-	lineY: (options: any) => ObservablePlot.lineY({ ...defaultLine, ...options }),
-	dashedLine: (options: any) => ObservablePlot.line({ ...defaultDashedLine, ...options }),
-	dashedLineX: (options: any) => ObservablePlot.lineX({ ...defaultDashedLine, ...options }),
-	dashedLineY: (options: any) => ObservablePlot.lineY({ ...defaultDashedLine, ...options }),
-	dot: (options: any) => ObservablePlot.dot({ ...defaultDot, ...options }),
-	dotX: (options: any) => ObservablePlot.dotX({ ...defaultDot, ...options }),
-	dotY: (options: any) => ObservablePlot.dotY({ ...defaultDot, ...options }),
-	point: (options: any) => ObservablePlot.dot({ ...defaultDot, ...options }),
-	pointX: (options: any) => ObservablePlot.dotX({ ...defaultDot, ...options }),
-	pointY: (options: any) => ObservablePlot.dotY({ ...defaultDot, ...options }),
-	area: (options: any) => ObservablePlot.area({ ...defaultArea, ...options }),
-	areaX: (options: any) => ObservablePlot.areaX({ ...defaultArea, ...options }),
-	areaY: (options: any) => ObservablePlot.areaY({ ...defaultArea, ...options }),
-	ruleY: (options: any) => ObservablePlot.ruleY({ ...defaultRule, ...options }),
-	ruleX: (options: any) => ObservablePlot.ruleX({ ...defaultRule, ...options }),
-	tip: (options: any) => ObservablePlot.tip({ ...defaultTip, ...options }),
-	annotationTip: (options: any) => ObservablePlot.tip({ ...defaultAnnotationTip, ...options }),
-	annotationText: (options: any) => ObservablePlot.text({ ...defaultAnnotationText, ...options })
+	gridX: (data?: Data, options?: GridXOptions) =>
+		data
+			? ObservablePlot.gridX(data, { ...defaultGridX, ...options })
+			: ObservablePlot.gridX({ ...defaultGridX, ...options }),
+	gridY: (data?: Data, options?: GridYOptions) =>
+		!data
+			? ObservablePlot.gridY({ ...defaultGridY, ...options })
+			: ObservablePlot.gridY(data, { ...defaultGridY, ...options }),
+	axisX: (data?: Data, options?: AxisXOptions) =>
+		data
+			? ObservablePlot.axisX(data, { ...defaultXAxis, ...options })
+			: ObservablePlot.axisX({ ...defaultXAxis, ...options }),
+	axisY: (data?: Data, options?: AxisYOptions) =>
+		data
+			? ObservablePlot.axisY(data, { ...defaultYAxis, ...options })
+			: ObservablePlot.axisY(data, { ...defaultYAxis, ...options }),
+	line: (data?: Data, options?: LineOptions) =>
+		ObservablePlot.line(data, { ...defaultLine, ...options }),
+	lineX: (data?: Data, options?: LineXOptions) =>
+		ObservablePlot.lineX(data, { ...defaultLine, ...options }),
+	lineY: (data?: Data, options?: LineYOptions) =>
+		ObservablePlot.lineY(data, { ...defaultLine, ...options }),
+	dashedLine: (data?: Data, options?: LineOptions) =>
+		ObservablePlot.line(data, { ...defaultDashedLine, ...options }),
+	dashedLineX: (data?: Data, options?: LineXOptions) =>
+		ObservablePlot.lineX(data, { ...defaultDashedLine, ...options }),
+	dashedLineY: (data?: Data, options?: LineYOptions) =>
+		ObservablePlot.lineY(data, { ...defaultDashedLine, ...options }),
+	dot: (data?: Data, options?: DotOptions) =>
+		ObservablePlot.dot(data, { ...defaultDot, ...options }),
+	dotX: (data?: Data, options?: DotXOptions) =>
+		ObservablePlot.dotX(data, { ...defaultDot, ...options }),
+	dotY: (data?: Data, options?: DotYOptions) =>
+		ObservablePlot.dotY(data, { ...defaultDot, ...options }),
+	point: (data?: Data, options?: DotOptions) =>
+		ObservablePlot.dot(data, { ...defaultDot, ...options }),
+	pointX: (data?: Data, options?: DotXOptions) =>
+		ObservablePlot.dotX(data, { ...defaultDot, ...options }),
+	pointY: (data?: Data, options?: DotYOptions) =>
+		ObservablePlot.dotY(data, { ...defaultDot, ...options }),
+	area: (data?: Data, options?: AreaOptions) =>
+		ObservablePlot.area(data, { ...defaultArea, ...options }),
+	areaX: (data?: Data, options?: AreaXOptions) =>
+		ObservablePlot.areaX(data, { ...defaultArea, ...options }),
+	areaY: (data?: Data, options?: AreaYOptions) =>
+		ObservablePlot.areaY(data, { ...defaultArea, ...options }),
+	ruleY: (data?: Data, options?: RuleXOptions) =>
+		ObservablePlot.ruleY(data, { ...defaultRule, ...options }),
+	ruleX: (data?: Data, options?: RuleYOptions) =>
+		ObservablePlot.ruleX(data, { ...defaultRule, ...options }),
+	tip: (data?: Data, options?: TipOptions) =>
+		ObservablePlot.tip(data, { ...defaultTip, ...options }),
+	annotationTip: (data?: Data, options?: TipOptions) =>
+		ObservablePlot.tip(data, { ...defaultAnnotationTip, ...options }),
+	annotationText: (data?: Data, options?: TextOptions) =>
+		ObservablePlot.text(data, { ...defaultAnnotationText, ...options }),
+	arrow: (data?: Data, options?: ArrowOptions) => ObservablePlot.arrow(data, { ...options }),
+	auto: (data?: Data, options?: AutoOptions) => ObservablePlot.auto(data, { ...options }),
+	autoSpec: (data?: Data, options?: AutoOptions) => ObservablePlot.autoSpec(data, { ...options }),
+	barX: (data?: Data, options?: BarXOptions) => ObservablePlot.barX(data, { ...options }),
+	barY: (data?: Data, options?: BarYOptions) => ObservablePlot.barY(data, { ...options }),
+	pointer: (options?: PointerOptions) => ObservablePlot.pointer({ ...options }),
+	pointerX: (options?: PointerOptions) => ObservablePlot.pointerX({ ...options }),
+	pointerY: (options?: PointerOptions) => ObservablePlot.pointerY({ ...options })
 };
