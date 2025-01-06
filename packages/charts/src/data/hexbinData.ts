d75abc91cb462f10f8d515d14f663b74289e46c3
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2025-01-06 12:09:53 +0000

    moved data out of demoData into separate files and now loading hexbinData from API instead of saving raw data to save memory

---
+++ packages/charts/src/data/hexbinData.ts
@@ -0,0 +1,38 @@
+import { rewind } from '@turf/rewind';
+
+const buildEsriQueryUrl = (esriLayerUrl: string, endpointId: number, esriParams = {}) => {
+	const queryParams = {
+		where: '1=1',
+		outFields: '*',
+		f: 'pjson'
+	};
+
+	Object.assign(queryParams, esriParams);
+	const queryString = new URLSearchParams(queryParams);
+
+	return `${esriLayerUrl}${endpointId}/query?${queryString}`;
+};
+
+const loadGeoData = async () => {
+	const geojsonURL =
+		'https://gis2.london.gov.uk/server/rest/services/apps/Busyness_context/MapServer/';
+
+	const geoQueryParams = {
+		where: `objectid IS NOT NULL`,
+		outFields: ['highstreet_id', 'highstreet_name'].toString(),
+		geometryPrecision: 4,
+		orderByFields: 'highstreet_id',
+		f: 'geojson'
+	};
+
+	const geoQueryUrl = buildEsriQueryUrl(geojsonURL, 3, geoQueryParams);
+	const res = await fetch(geoQueryUrl);
+	return res.json();
+};
+
+const data = await loadGeoData();
+
+const rewindData = (data: GeoJSON.FeatureCollection) =>
+	data.features.map((feature) => rewind(feature, { reverse: true }));
+
+export const hexbinData = rewindData(data);
