7143213a979168d283b60da36f71a91ed0248d09
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2025-01-08 13:18:22 +0000

    handling Plot.plot if defaults are applied or not

--- packages/charts/src/lib/observablePlot/ObservablePlotInner.svelte
+++ packages/charts/src/lib/observablePlot/ObservablePlotInner.svelte
@@ -89,7 +89,8 @@
 </script>
 
 <script lang="ts">
-	import * as Plot from '@observablehq/plot';
+	import * as ObservablePlot from '@observablehq/plot';
+	import * as Plot from '../observablePlotFragments/glaPlot';
 
 	import { afterUpdate, onMount, setContext } from 'svelte';
 	import { derived, writable } from 'svelte/store';
@@ -118,8 +119,18 @@
 	/** A y-offset between data points and tooltips (pixels). */
 	export let tooltipOffset = -16;
 
+	/**
+	 * If `false`, then use the Plot.plot function provided by  Observable Plot (rather than the wrapper provided by `@ldn-viz`),
+	 * so that default chart-level styling is not applied.
+	 */
+	export let applyDefaults = true;
+
 	const renderPlot = (node: HTMLDivElement) => {
-		node.appendChild(Plot.plot(spec));
+		if (applyDefaults) {
+			node.appendChild(Plot.plot(spec));
+		} else {
+			node.appendChild(ObservablePlot.plot(spec));
+		}
 	};
 
 	let width: number;
