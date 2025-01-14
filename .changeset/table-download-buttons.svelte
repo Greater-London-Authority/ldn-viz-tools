b3799c35a34c2669da5599c0a3a268a44adb91f5
James Scott-Brown <james.scott-brown@london.gov.uk>
2025-01-14 12:14:19 +0000

    hide SVG option by default in table image export button

---
+++ .changeset/table-download-buttons.svelte
@@ -0,0 +1,5 @@
+---
+'@ldn-viz/tables': patch
+---
+
+CHANGED: default to hide SVG option in image download button, as this ill not give the exported result of downloading an image of the entire table
