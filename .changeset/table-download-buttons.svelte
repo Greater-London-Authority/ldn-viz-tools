67038f4b027c4b430828d010d6333ab18b7624e6
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2025-01-14 12:21:20 +0000

    Update table-download-buttons.svelte

--- .changeset/table-download-buttons.svelte
+++ .changeset/table-download-buttons.svelte
@@ -2,4 +2,4 @@
 '@ldn-viz/tables': patch
 ---
 
-CHANGED: default to hide SVG option in image download button, as this ill not give the exported result of downloading an image of the entire table
+CHANGED: default to hide SVG option in image download button, as this will not give the exported result of downloading an image of the entire table
