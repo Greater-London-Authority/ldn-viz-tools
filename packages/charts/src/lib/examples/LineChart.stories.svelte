496fbaea01a42598db47eb2282d48e801c89f807
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2025-01-15 17:50:45 +0000

    adding aria attributes to chart for accessibility

--- packages/charts/src/lib/examples/LineChart.stories.svelte
+++ packages/charts/src/lib/examples/LineChart.stories.svelte
@@ -18,17 +18,26 @@
 	});
 
 	$: defaultMarks = [
-		Plot.gridX({ interval: '2 years' }),
-		Plot.gridY(),
-		Plot.ruleY([0]),
-		Plot.line(chartData, { x: 'year', y: 'Domestic - Total' }),
-		Plot.axisX({ interval: '2 years' }),
-		Plot.axisY({ label: 'ktCO₂e' })
+		Plot.gridX({ interval: '2 years', ariaHidden: 'true' }),
+		Plot.gridY({ ariaHidden: 'true' }),
+		Plot.ruleY([0], { ariaHidden: 'true' }),
+		Plot.line(chartData, {
+			x: 'year',
+			y: 'Domestic - Total',
+			ariaHidden: 'true'
+		}),
+		Plot.axisX({ interval: '2 years', ariaHidden: 'true' }),
+		Plot.axisY({
+			label: 'ktCO₂e',
+			ariaHidden: 'true'
+		})
 	];
 
 	$: defaultSpec = {
 		x: { insetLeft: 80, insetRight: 20, type: 'utc' },
-		marks: defaultMarks
+		marks: defaultMarks,
+		ariaLabel:
+			'Domestic Greenhouse Gas Emissions dropped from over 16,000 kilotons of carbon dioxide emissions in 2005, to below 10,000 kilotons in 2022'
 	};
 
 	$: specWithTooltip = {
