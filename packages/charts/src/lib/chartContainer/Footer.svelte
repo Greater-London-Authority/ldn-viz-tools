3a7befea8a22cc7a1391bde9db64d3973564420e
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2025-01-22 15:00:05 +0000

    added modal to Footer with text button overrides so it displays inline with other text elements

--- packages/charts/src/lib/chartContainer/Footer.svelte
+++ packages/charts/src/lib/chartContainer/Footer.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+	import { Button, Modal } from '@ldn-viz/ui';
+	import { writable } from 'svelte/store';
+
 	export let byline = '';
 	export let source = '';
 	export let note = '';
 	export let chartDescription = '';
+
+	/**
+	 * Controls whether the Modal is open or closed
+	 */
+	let isOpen = writable(false);
 </script>
 
 <div class="w-full flex flex-wrap justify-between mt-1 items-end">
@@ -11,6 +19,16 @@
 			{#if byline}<li>{byline}</li>{/if}
 			{#if source}<li><span class="font-bold mr-1">Source:</span>{source}</li>{/if}
 			{#if note}<li><span class="font-bold mr-1">Note:</span>{note}</li>{/if}
+			{#if chartDescription}
+				<li>
+					<Button
+						variant="text"
+						on:click={() => ($isOpen = true)}
+						class="font-bold space-y-0.5 text-xs m-0 py-0 px-0">View description</Button
+					>
+				</li>
+				<Modal bind:isOpen title="Description" description={chartDescription}></Modal>
+			{/if}
 		</ul>
 	{/if}
 	{#if $$slots.exportBtns}
