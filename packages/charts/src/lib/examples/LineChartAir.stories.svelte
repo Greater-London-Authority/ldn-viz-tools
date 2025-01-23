54c81cb00f4b53f484fb288c291fed097944f691
mikeldn <mike.brondbjerg@london.gov.uk>
2025-01-23 11:09:03 +0000

    mb chart examples wip

--- packages/charts/src/lib/examples/LineChartAir.stories.svelte
+++ packages/charts/src/lib/examples/LineChartAir.stories.svelte
@@ -6,6 +6,23 @@
 	};
 </script>
 
+<!-- TODO:
+[x] Thicker line (for contrast) (done in fragments)
+[ ] Long data, not wide (wide data example elsewhere?
+[ ] Make data 'meaningless' to avoid distraction. So 'Variable 1, Variable 2, etc'
+[ ] Make date formats and ranges real, as transformations and formatting are important
+[ ] Change spec to use lomg data and set color domains (rather than wide data named fields.render. or both?)
+[ ] London vs Rest of UK (primary vs grey)
+[ ] Multi line
+[ ] Many multi Line (as grey context) 
+[ ] Points on lines (with label?)
+[ ] X & Y threshold line (and annotation)
+[ ] Range highlight 
+[ ] X axis label, Y axis label
+[ ] area chart, as simple addition to line? (not stacked)
+
+-->
+
 <script lang="ts">
 	import { Story, Template } from '@storybook/addon-svelte-csf';
 	import airQualityLondonMonthly2024 from '../../data/airQualityLondonMonthly2024.json';
