95f2b683094a950a9490d34b1fbd1b57e8705338
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2025-01-23 10:17:43 +0000

    added title to Footer list

--- packages/charts/src/lib/chartContainer/Footer.svelte
+++ packages/charts/src/lib/chartContainer/Footer.svelte
@@ -15,7 +15,10 @@
 
 <div class="w-full flex flex-wrap justify-between mt-1 items-end">
 	{#if byline || source || note || chartDescription}
-		<ul class="flex flex-col space-y-0.5 text-color-text-secondary text-xs min-w-40 max-w-xl mr-4">
+		<ul
+			title="Chart footnotes and description"
+			class="flex flex-col space-y-0.5 text-color-text-secondary text-xs min-w-40 max-w-xl mr-4"
+		>
 			{#if byline}<li>{byline}</li>{/if}
 			{#if source}<li><span class="font-bold mr-1">Source:</span>{source}</li>{/if}
 			{#if note}<li><span class="font-bold mr-1">Note:</span>{note}</li>{/if}
