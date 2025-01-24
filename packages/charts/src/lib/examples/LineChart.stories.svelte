1a5aab4bf8a60e0137c10df9cfafeb6473b5d566
mikeldn <mike.brondbjerg@london.gov.uk>
2025-01-24 16:12:26 +0000

    good single and mutli line chart examples

--- packages/charts/src/lib/examples/LineChart.stories.svelte
+++ packages/charts/src/lib/examples/LineChart.stories.svelte
@@ -3,7 +3,7 @@
 
 	/** This is an example `LineChart` chart using default plot styles.
 	 *
-	 * By default, charts are hidden from screen readers to improve accessibility. Instead, use descriptive `title`, `subTitle` and surrounding text description so all users understand what the chart shows.
+	 * By default, charts (and their inner details) are hidden from screen readers to improve the accessibility experience. Instead, it's crtitical we use a descriptive `title`, `subTitle`, `alt`, `chartDescription` and surrounding document text, so all users understand what the chart shows and gain the same insight. We should also link to the data where possible.
 	 */
 
 	export const meta = {
@@ -12,123 +12,184 @@
 </script>
 
 <!-- TODO:
-[x] Thicker line (for contrast) (done in fragments)
-[ ] Long data, not wide (wide data example elsewhere?
-[ ] Make data 'meaningless' to avoid distraction. So 'Variable 1, Variable 2, etc'
-[ ] Make date formats and ranges real, as transformations and formatting are important
-[ ] Change spec to use lomg data and set color domains (rather than wide data named fields.render. or both?)
-[ ] London vs Rest of UK (primary vs grey)
-[ ] Multi line
-[ ] Many multi Line (as grey context) 
+[x] Thicker line (for contrast)?
+[x] Long data, not wide (wide data example elsewhere?
+[x] Make data 'meaningless' to avoid distraction. So 'Variable 1, Variable 2, etc'
+[x] Make date formats and ranges real, as transformations and formatting are important
+[x] Change spec to use lomg data and set color domains (rather than wide data named fields.render. or both?)
+[ ] London vs Rest of UK (primary vs grey) (A <> B)
+[x] Multi line
+[ ] Many Multi line, focus vs context 
 [ ] Points on lines (with label?)
 [ ] X & Y threshold line (and annotation)
 [ ] Range highlight 
-[ ] X axis label, Y axis label
+[x] X axis label, Y axis label
 [ ] area chart, as simple addition to line? (not stacked)
-
+[ ] non zero baseline (small change)
 -->
 
 <script lang="ts">
 	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import { format } from 'd3';
 	import demoMonthlyTimeseriesLong from '../../data/demoMonthlyTimeseriesLong.json';
-	import ghgLondonTotalByYear from '../../data/ghgLondonTotalByYear.json';
 	import { plotTheme } from '../observablePlotFragments/observablePlotFragments';
 	import { Plot } from '../observablePlotFragments/plot';
 
-	$: chartData = ghgLondonTotalByYear.map((d) => {
-		return { ...d, year: new Date(d.Year, 0) };
-	});
+	const formatLow = format(',.0f'); // for lower than 10000, format commas and not dp
+	const formatHigh = format(',.4~s'); // for 10000 and above, format commas and SI numbering (M & K)
 
-	const getRandomArbitrary = function (min: number, max: number) {
-		return Math.random() * (max - min) + min;
-	};
-
-	$: chartDataMulti = demoMonthlyTimeseriesLong;
-	// .map((d) => {
-	// 	return { ...d, Value: d.Value * getRandomArbitrary(1, 1.025) * 1000 };
-	// });
-
-	/**
-	 * Spec for default example
-	 */
-
-	$: defaultSpec = {
+	// Spec and data for single line example (default)
+	$: singleLineData = demoMonthlyTimeseriesLong.filter((d) => d.Variable == 'Variable A');
+	$: singleLineSpec = {
 		x: { insetLeft: 80, insetRight: 20, type: 'utc' },
 		marks: [
 			Plot.gridX({ interval: '2 years' }),
 			Plot.gridY(),
+			Plot.axisX({ label: 'Year', interval: '1 year' }),
+			Plot.axisY({ label: '', tickFormat: (d) => '£' + formatHigh(d) }),
 			Plot.ruleY([0]),
-			Plot.line(chartData, { x: 'year', y: 'Domestic - Total' }),
-			Plot.axisX({ interval: '2 years' }),
-			Plot.axisY({ label: 'ktCO₂e' }),
+			Plot.line(singleLineData, {
+				x: 'Month',
+				y: 'Value',
+				z: 'Variable',
+				stroke: plotTheme().color.data.primary,
+				tip: true
+			})
+		]
+	};
+
+	// Spec and data for multi-line example
+	$: multiLineData = demoMonthlyTimeseriesLong;
+	$: multiLineSpec = {
+		x: { insetLeft: 80, insetRight: 20, type: 'utc' },
+		color: {
+			legend: true,
+			type: 'ordinal',
+			range: [
+				plotTheme().color.data.primary,
+				plotTheme().color.data.secondary,
+				plotTheme().color.data.tertiary
+			]
+		},
+		marks: [
+			Plot.gridX({ interval: '2 years' }),
+			Plot.gridY(),
+			Plot.axisX({ label: 'Year', interval: '1 year' }),
+			Plot.axisY({ label: '', tickFormat: (d) => '£' + formatHigh(d) }),
+			Plot.ruleY([0]),
+			Plot.line(multiLineData, {
+				x: 'Month',
+				y: 'Value',
+				z: 'Variable',
+				stroke: 'Variable'
+			}),
+
+			Plot.ruleX(
+				multiLineData,
+				Plot.pointerX({ x: 'Month', stroke: plotTheme().color.chart.label })
+			),
+			Plot.point(
+				multiLineData,
+				Plot.pointer({ x: 'Month', y: 'Value', z: 'Variable', stroke: 'Variable' })
+			),
 			Plot.tip(
-				chartData,
-				Plot.pointerX({
-					x: 'year',
-					y: 'Domestic - Total',
-					channels: { Year: 'year', Value: 'Value' },
+				multiLineData,
+				Plot.pointer({
+					x: 'Month',
+					y: 'Value',
+					z: 'Variable',
+					channels: {
+						Variable: 'Variable',
+						Month: { value: 'Month', label: 'Date' },
+						Value: { value: 'Value', label: 'GBP' }
+					},
 					format: {
+						Variable: true,
+						Month: true,
+						Value: (d) => '£' + formatHigh(d),
 						x: false,
-						Year: (d) => d.getFullYear()
+						y: false
 					}
 				})
 			)
 		]
 	};
+</script>
 
-	/**
-	 * Spec for multi-line example
-	 */
+<Template let:args>
+	<ObservablePlot {...args} />
+</Template>
 
-	// $: multiLineSpec = {
-	// 	x: { insetLeft: 80, insetRight: 20, type: 'utc' },
-	// 	marks: [
-	// 		Plot.gridX({ interval: '2 years' }),
-	// 		Plot.gridY(),
-	// 		Plot.ruleY([0]),
-	// 		Plot.line(chartData, {
-	// 			x: 'year',
-	// 			y: 'Domestic - Electricity',
-	// 			stroke: plotTheme().color.data.primary
-	// 		}),
-	// 		Plot.line(chartData, {
-	// 			x: 'year',
-	// 			y: 'Domestic - Gas',
-	// 			stroke: plotTheme().color.data.secondary
-	// 		}),
-	// 		Plot.axisX({ interval: '2 years' }),
-	// 		Plot.axisY({ label: 'ktCO₂e' }),
-	// 		Plot.ruleX(
-	// 			chartData,
-	// 			Plot.pointerX({ x: 'year', py: 'Domestic - Gas', stroke: plotTheme().color.chart.label })
-	// 		),
-	// 		Plot.tip(
-	// 			chartData,
-	// 			Plot.pointerX({
-	// 				x: 'year',
-	// 				y: 'Domestic - Electricity',
-	// 				channels: { Value: 'Value' },
-	// 				format: {
-	// 					x: false,
-	// 					Year: (d) => d.getFullYear()
-	// 				}
-	// 			})
-	// 		),
-	// 		Plot.tip(
-	// 			chartData,
-	// 			Plot.pointerX({
-	// 				x: 'year',
-	// 				y: 'Domestic - Gas',
-	// 				channels: { Value: 'Value' },
-	// 				format: {
-	// 					x: false,
-	// 					Year: (d) => d.getFullYear()
-	// 				}
-	// 			})
-	// 		)
-	// 	]
-	// };
+<!--
+```html
+<script>
+	import { Plot, plotTheme } from '@ldn-viz/charts';
+	import demoMonthlyTimeseriesLong from '../../data/demoMonthlyTimeseriesLong.json'; // import or load your data
+	import { format } from 'd3'; // for formatting numbers in the chart	
 
+	const formatLow = format(',.0f'); // for lower than 10000, format commas and not dp
+	const formatHigh = format(',.4~s'); // for 10000 and above, format commas and SI numbering (M & K)
+
+	$: singleLineData = demoMonthlyTimeseriesLong.filter((d) => d.Variable == 'Variable A');
+	$: singleLineSpec = {
+		x: { insetLeft: 80, insetRight: 20, type: 'utc' },
+		marks: [
+			Plot.gridX({ interval: '2 years' }),
+			Plot.gridY(),
+			Plot.axisX({ label: 'Year', interval: '1 year' }),
+			Plot.axisY({ label: '', tickFormat: (d) => '£' + formatHigh(d) }),
+			Plot.ruleY([0]),
+			Plot.line(singleLineData, {
+				x: 'Month',
+				y: 'Value',
+				z: 'Variable',
+				stroke: plotTheme().color.data.primary,
+				tip: true
+			})
+		]
+	};
+	
+</script>
+
+<ObservablePlot
+		spec={singleLineSpec}
+		data={singleLineData}
+		title={"In London, Variable A's value has fallen steadily since 2017"}
+		subTitle={"London monthly estimated variable value (GBP), January 2015 to March 2024"}
+		alt={"Line chart of London's variable A values"}
+		byline={"GLA City Intelligence"}
+		source={"LDN Viz Tools Demo Data"}
+		note={"Data for demonstration only"}
+		chartDescription={"The line chart shows monthly time series data for Variable A, measured in GBP (Pounds Sterling). The x axis ranges in months from January 2015 to March 2024. The y axis ranges from £0 to £60,000. Variable A's has fallen steadily since around 2017. Variable A's highest value was £61,816 in Febraury 2015, it's lowest value was £11,667 in July 2023, (a change of around -£50,149) and it's mean average value was £30,758"}
+	/>
+```
+-->
+
+<Story name="Default" source>
+	<ObservablePlot
+		spec={singleLineSpec}
+		data={singleLineData}
+		title={"In London, Variable A's value has fallen steadily since 2017"}
+		subTitle={'London monthly estimated variable value (GBP), January 2015 to March 2024'}
+		alt={"Line chart of London's variable A values"}
+		byline={'GLA City Intelligence'}
+		source={'LDN Viz Tools Demo Data'}
+		note={'Data for demonstration only'}
+		chartDescription={"The line chart shows monthly time series data for Variable A, measured in GBP (Pounds Sterling). The x axis ranges in months from January 2015 to March 2024. The y axis ranges from £0 to £60,000. Variable A's has fallen steadily since around 2017. Variable A's highest value was £61,816 in Febraury 2015, it's lowest value was £11,667 in July 2023, (a change of around -£50,149) and it's mean average value was £30,758"}
+	/>
+</Story>
+
+<!--
+```html
+<script>
+	import { Plot, plotTheme } from '@ldn-viz/charts';
+	import demoMonthlyTimeseriesLong from '../../data/demoMonthlyTimeseriesLong.json'; // import or load your data
+	import { format } from 'd3'; // for formatting numbers in the chart	
+
+	const formatLow = format(',.0f'); // for lower than 10000, format commas and not dp
+	const formatHigh = format(',.4~s'); // for 10000 and above, format commas and SI numbering (M & K)
+
+	$: multiLineData = demoMonthlyTimeseriesLong; // using demo data directly, without parsing
 	$: multiLineSpec = {
 		x: { insetLeft: 80, insetRight: 20, type: 'utc' },
 		color: {
@@ -144,9 +205,9 @@
 			Plot.gridX({ interval: '2 years' }),
 			Plot.gridY(),
 			Plot.axisX({ interval: '1 year' }),
-			Plot.axisY({ label: '£ GBP' }),
+			Plot.axisY({ label: '', tickFormat: (d) => '£' + formatHigh(d) }),
 			Plot.ruleY([0]),
-			Plot.line(chartDataMulti, {
+			Plot.line(multiLineData, {
 				x: 'Month',
 				y: 'Value',
 				z: 'Variable',
@@ -154,191 +215,61 @@
 			}),
 
 			Plot.ruleX(
-				chartDataMulti,
+				multiLineData,
 				Plot.pointerX({ x: 'Month', stroke: plotTheme().color.chart.label })
 			),
 			Plot.point(
-				chartDataMulti,
+				multiLineData,
 				Plot.pointer({ x: 'Month', y: 'Value', z: 'Variable', stroke: 'Variable' })
 			),
 			Plot.tip(
-				chartDataMulti,
+				multiLineData,
 				Plot.pointer({
 					x: 'Month',
 					y: 'Value',
-					z: 'Varaible',
-					channels: { Variable: 'Variable', Date: 'Month', 'GBP (£)': 'Value' },
+					z: 'Variable',
+					channels: {
+						Variable: 'Variable',
+						Month: { value: 'Month', label: 'Date' },
+						Value: { value: 'Value', label: 'GBP' }
+					},
 					format: {
+						Variable: true,
+						Month: true,
+						Value: (d) => '£' + formatHigh(d),
 						x: false,
 						y: false
 					}
 				})
 			)
-			// Plot.tip(
-			// 	chartData,
-			// 	Plot.pointerX({
-			// 		x: 'year',
-			// 		y: 'Domestic - Gas',
-			// 		channels: { Value: 'Value' },
-			// 		format: {
-			// 			x: false,
-			// 			Year: (d) => d.getFullYear()
-			// 		}
-			// 	})
-			// )
-		]
-	};
-</script>
-
-<Template let:args>
-	<ObservablePlot {...args} />
-</Template>
-
-<!--
-```html
-<script>
-	import { Plot, ObservablePlot } from '@ldn-viz/charts';
-
-	// Import data
-	import ghgLondonTotalByYear from '../../data/ghgLondonTotalByYear.json';
-
-	// Parse dates
-	$: chartData = ghgLondonTotalByYear.map((d) => {
-		return { ...d, year: new Date(d.Year, 0) };
-	});
-
-	$: spec = {
-		x: { insetLeft: 80, insetRight: 20, type: 'utc' },
-		marks: [
-			Plot.gridX({ interval: '2 years' }),
-			Plot.gridY(),
-			Plot.ruleY([0]),
-			Plot.line(chartData, { x: 'year', y: 'Domestic - Total' }),
-			Plot.axisX({ interval: '2 years' }),
-			Plot.axisY({ label: 'ktCO₂e' }),
-			Plot.tip(
-				chartData,
-				Plot.pointerX({
-					x: 'year',
-					y: 'Domestic - Total',
-					channels: { Year: 'year', Value: 'Value' },
-					format: {
-						x: false,
-						Year: (d) => d.getFullYear()
-					}
-				})
-			)
 		]
 	};
 </script>
 
 <ObservablePlot
-	spec={spec}
-	title={'Domestic Greenhouse Gas Emissions'}
-	subTitle={'Total Domestic Greenhouse Gas Emissions in London have fallen between 2005 and 2022'}
-	data={ chartData }
+	spec={multiLineSpec}
+	data={multiLineData}
+	title={'In London, all variable values have fallen steadily since 2017, with Variable A experiencing the most siginificant fall'}
+	subTitle={'London monthly estimated variable values (GBP), January 2015 to March 2024'}
+	alt={'Line chart of London variable values'}
+	byline={'GLA City Intelligence'}
+	source={'LDN Viz Tools Demo Data'}
+	note={'Data for demonstration only'}
+	chartDescription={"The line chart shows monthly time series data for Variable A, B and C, measured in GBP (Pounds Sterling). The x axis ranges in months from January 2015 to March 2024. The y axis ranges from £0 to £60,000. All variable values have fallen steadily since around 2017, but Variable A has fallen the most. Variable A's highest value was £61,816 in Febraury 2015, it's lowest value was £11,667 in July 2023, (a change of around -£50,149) and it's mean average value was £30,758. Variable B and C follow a similar fall, with a mean average of £27,545 and £23,681 respectively."}
 />
 ```
 -->
 
-<Story name="Default" source>
-	<ObservablePlot
-		spec={defaultSpec}
-		title={'Domestic Greenhouse Gas Emissions'}
-		subTitle={'Total Domestic Greenhouse Gas Emissions in London have fallen between 2005 and 2022'}
-		data={chartData}
-	/>
-</Story>
-
-<!--
-```html
-<script>
-	import { Plot, ObservablePlot } from '@ldn-viz/charts';
-
-	// Import data
-	import ghgLondonTotalByYear from '../../data/ghgLondonTotalByYear.json';
-
-	// Parse dates
-	$: chartData = ghgLondonTotalByYear.map((d) => {
-		return { ...d, year: new Date(d.Year, 0) };
-	});
-
-	$: spec = {
-		x: { insetLeft: 80, insetRight: 20, type: 'utc' },
-		marks: [
-			Plot.gridX({ interval: '2 years' }),
-			Plot.gridY(),
-			Plot.ruleY([0]),
-			Plot.line(chartData, {
-				x: 'year',
-				y: 'Domestic - Electricity',
-				stroke: plotTheme().color.data.primary,
-				tip: 'x'
-			}),
-			Plot.line(chartData, {
-				x: 'year',
-				y: 'Domestic - Gas',
-				stroke: plotTheme().color.data.secondary,
-				tip: 'x'
-			}),
-			Plot.axisX({ interval: '2 years' }),
-			Plot.axisY({ label: 'ktCO₂e' }),
-			Plot.tip(
-				chartData,
-				Plot.pointerX({
-					x: 'year',
-					y: 'Domestic - Electricity',
-					channels: { Year: 'year', Value: 'Value' },
-					format: {
-						x: false,
-						Year: (d) => d.getFullYear()
-					}
-				})
-			),
-			Plot.tip(
-				chartData,
-				Plot.pointerX({
-					x: 'year',
-					y: 'Domestic - Gas',
-					channels: { Year: 'year', Value: 'Value' },
-					format: {
-						x: false,
-						Year: (d) => d.getFullYear()
-					}
-				})
-			)
-			// Plot.ruleX(
-			// 	chartData,
-			// 	Plot.pointerX({ x: 'year', py: 'Domestic - Gas', stroke: plotTheme().color.chart.label })
-			// )
-		]
-	};
-</script>
-
-<ObservablePlot
-	spec={spec}
-	data={chartData}
-	title="Domestic Greenhouse Gas Emissions in London have fallen steadily since 2005"
-	subTitle="London annual domestic greenhouse gas emissions split by gas and electric energy, measured in kilotonnes of carbon dioxide equivalent (ktCO₂e), from 2005 to 2022"
-	alt="Line chart of London annual domestic greenhouse gas emissions"
-	byline="GLA City Intelligence"
-	source="LDN Viz Tools Demo Data"
-	note="Data for demonstration purposes only"
-	chartDescription="The chart shows domestic greenhouse gas emissions in London fell by almost 8,000 kilotonnes of carbon dioxide equivalent from 2005 to 2022. There are two lines, one shows domestic gas emissions and the second shows domestic electricity emissions. In 2005, for gas, there were 9,748 kilotonnes of carbon dioxide equivalent which dropped to 6,343 kilotonnes in 2022. In 2005, for electricity, there were 6,986 kilotonnes of carbon dioxide equivalent which dropped to 2,632 kilotonnes in 2022."
-	/>
-```
--->
-
-<Story name="Multiple lines" source>
+<Story name="Multiple lines (inc custom tool tips)" source>
 	<ObservablePlot
 		spec={multiLineSpec}
-		data={chartDataMulti}
-		title="In London, all variable values have fallen steadily since 2017, with Variable A experiencing the most siginificant fall. "
-		subTitle="London monthly estimated variable values (GBP), January 2015 to March 2024"
-		alt="Line chart of London variable values"
-		byline="GLA City Intelligence"
-		source="LDN Viz Tools Demo Data"
-		note="Data for demonstration purposes only"
-		chartDescription="The line chart shows monthly time series data for Variable A, B and C, measured in GBP (Pounds Sterling), from January 2015 to March 2024.  "
+		data={multiLineData}
+		title={'In London, all variable values have fallen steadily since 2017, with Variable A experiencing the most siginificant fall'}
+		subTitle={'London monthly estimated variable values (GBP), January 2015 to March 2024'}
+		alt={'Line chart of London variable values'}
+		byline={'GLA City Intelligence'}
+		source={'LDN Viz Tools Demo Data'}
+		note={'Data for demonstration only'}
+		chartDescription={"The line chart shows monthly time series data for Variable A, B and C, measured in GBP (Pounds Sterling). The x axis ranges in months from January 2015 to March 2024. The y axis ranges from £0 to £60,000. All variable values have fallen steadily since around 2017, but Variable A has fallen the most. Variable A's highest value was £61,816 in Febraury 2015, it's lowest value was £11,667 in July 2023, (a change of around -£50,149) and it's mean average value was £30,758. Variable B and C follow a similar fall, with a mean average of £27,545 and £23,681 respectively."}
 	/>
 </Story>
