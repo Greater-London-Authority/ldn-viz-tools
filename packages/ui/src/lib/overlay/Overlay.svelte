515e99eea9d5b9dcc9a240bc4cb65d545a5784bb
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2025-02-04 13:05:10 +0000

    created Overlay component to handle tooltips, popovers and modals given similar functionality

---
+++ packages/ui/src/lib/overlay/Overlay.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	/**
+	 * The `<sidebarHint>` component provides additional explanatory or help text when a user interacts with a trigger.
+	 * @component
+	 */
+
+	import Modal from '../modal/Modal.svelte';
+	import Popover from '../popover/Popover.svelte';
+	import Tooltip from '../tooltip/Tooltip.svelte';
+	import Trigger from './Trigger.svelte';
+
+	/**
+	 * Form in which the help text should be displayed.
+	 */
+	export let overlayType: 'tooltip' | 'popover' | 'modal' = 'tooltip';
+
+	/**
+	 * Text to be displayed next to icon in trigger
+	 */
+	export let hintLabel = 'More Info';
+
+	/**
+	 * Title of modal (if `overlayType` is `'modal'`)
+	 */
+	export let modalTitle: string | undefined = undefined;
+
+	/**
+	 * Description of modal (if `overlayType` is `'modal'`)
+	 */
+	export let modalDescription: string | undefined = undefined;
+
+	/**
+	 * Width of modal (if `overlayType` is `'modal'`)
+	 */
+	export let modalWidth:
+		| 'sm'
+		| 'md'
+		| 'lg'
+		| 'xs'
+		| 'xl'
+		| '2xl'
+		| '3xl'
+		| '4xl'
+		| '5xl'
+		| '6xl'
+		| '7xl'
+		| 'full'
+		| undefined = undefined;
+</script>
+
+{#if overlayType === 'tooltip'}
+	<Tooltip>
+		<slot slot="trigger" name="trigger">
+			<!-- TODO: set the props of trigger correctly-->
+			<Trigger {hintLabel} />
+		</slot>
+
+		<!-- The help message. -->
+		<slot />
+	</Tooltip>
+{:else if overlayType === 'popover'}
+	<Popover {hintLabel}>
+		<!-- The help message. -->
+		<slot />
+	</Popover>
+{:else if overlayType === 'modal'}
+	<Modal title={modalTitle || ''} description={modalDescription} width={modalWidth} showTrigger>
+		<svelte:fragment slot="hintText">
+			{hintLabel}
+		</svelte:fragment>
+
+		<!-- The help message. -->
+		<slot />
+	</Modal>
+{/if}
