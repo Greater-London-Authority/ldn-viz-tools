3863712c97fdd1c248ff450ddbfb4ef4f2c40868
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2025-02-04 13:00:25 +0000

    Refactored Tooltip to use Melt builder and triggerFuncs context, so trigger no longer lives inside it

--- packages/ui/src/lib/tooltip/Tooltip.svelte
+++ packages/ui/src/lib/tooltip/Tooltip.svelte
@@ -1,99 +1,65 @@
 <script lang="ts">
 	/**
-	 * The `<Tooltip>` component renders a tooltip target, and displays a message in a tooltip when a user mouses-over it.
+	 * The `<Tooltip>` component renders a tooltip target, and displays a message in a tooltip when a user focuses it (mouseover and keyboard).
 	 *
 	 * **Alternatives**: for longer messages, or messages that must persist until being dismissed, consider using a [Modal](./?path=/docs/ui-modal--documentation)
 	 * or [Popover](./?path=/docs/ui-popover--documentation).
 	 * @component
 	 */
 
-	import { arrow } from 'svelte-floating-ui';
-	import { flip, offset, shift } from 'svelte-floating-ui/dom';
-	import { writable, type Writable } from 'svelte/store';
-	import { floatingContent } from './tooltip';
-
-	import { InformationCircle } from '@steeze-ui/heroicons';
-	import { Icon } from '@steeze-ui/svelte-icon';
-
-	import Button from '../button/Button.svelte';
-	import { floatingRef } from '../tooltip/tooltip.js';
+	import { createTooltip } from '@melt-ui/svelte';
+	import { setContext } from 'svelte';
+	import { fade } from 'svelte/transition';
 
 	/**
-	 * text that appears in the tooltip target, next to the icon
+	 * Options for position of tooltip
 	 */
-	export let hintLabel = 'more info';
+	type PlacementOptions =
+		| 'top'
+		| 'top-start'
+		| 'top-end'
+		| 'right'
+		| 'right-start'
+		| 'right-end'
+		| 'bottom'
+		| 'bottom-start'
+		| 'bottom-end'
+		| 'left'
+		| 'left-start'
+		| 'left-end';
 
+	// TODO: Check whether placement updates responsively
 	/**
-	 * text size for the tooltip target
+	 * Determines the placement of the tooltip, relative to Trigger
 	 */
-	export let hintSize: 'sm' | 'md' | 'lg' | undefined = undefined;
+	export let placement: PlacementOptions = 'top';
 
-	let showTooltip = false;
+	const {
+		elements: { trigger, content, arrow },
+		states: { open }
+	} = createTooltip({
+		positioning: { placement },
+		openDelay: 0,
+		closeDelay: 0,
+		closeOnPointerDown: false
+	});
 
-	let element: HTMLSpanElement;
-
-	const arrowRef: Writable<HTMLElement> = writable();
-	let dynamicOptions = {};
-	$: if (showTooltip) {
-		dynamicOptions = {
-			middleware: [offset(10), flip(), shift(), arrow({ element: arrowRef })],
-			onComputed({ placement, middlewareData }: { placement: string; middlewareData: any }) {
-				const { x, y } = middlewareData.arrow || {};
-				const staticSide = {
-					top: 'bottom',
-					right: 'left',
-					bottom: 'top',
-					left: 'right'
-				}[placement.split('-')[0]];
-
-				// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-				$arrowRef &&
-					Object.assign($arrowRef.style, {
-						left: x != null ? `${x}px` : '',
-						top: y != null ? `${y}px` : '',
-						[staticSide!.toString()]: '-8px'
-					});
-			}
-		};
-	}
+	setContext('triggerFuncs', { customAction: trigger, actionProps: $trigger });
 </script>
 
-<Button variant="text" size={hintSize} class="!p-0" emphasis="secondary">
-	<span
-		use:floatingRef
-		bind:this={element}
-		on:mouseenter={() => {
-			showTooltip = true;
-			floatingRef(element);
-		}}
-		on:mouseleave|stopPropagation={() => (showTooltip = false)}
-		role="tooltip"
-		class="inline-flex items-center"
-	>
-		{#if $$slots.hint}
-			<!-- if present, replaces the default `hintLabel` and icon  -->
-			<slot name="hint" />
-		{:else}
-			{hintLabel}
+<!-- The trigger that opens the tooltip, usually `Trigger` button but allows customisation -->
+<slot name="trigger" />
 
-			<Icon
-				src={InformationCircle}
-				theme="mini"
-				class="w-[18px] h-[18px] ml-0.5"
-				aria-hidden="true"
-			/>
-		{/if}
-	</span>
-</Button>
-
-{#if showTooltip}
+{#if $open}
 	<div
+		{...$content}
+		use:content
+		transition:fade={{ duration: 100 }}
 		class="absolute max-w-[200px] text-sm p-2 bg-color-container-level-1 shadow z-50"
-		use:floatingContent={dynamicOptions}
 	>
-		<!-- the text that will be displayed in the tooltip -->
+		<!-- The text that will be displayed in the tooltip -->
 		<slot />
 
-		<div class="absolute bg-color-container-level-1 rotate-45 w-4 h-4" bind:this={$arrowRef} />
+		<div {...$arrow} use:arrow />
 	</div>
 {/if}
