1413d529ec60a718167f12bd951161a01b858505
James Scott-Brown <james.scott-brown@london.gov.uk>
2025-02-07 14:45:40 +0000

    add compoenents for OAuth authentication

---
+++ packages/ui/src/lib/auth/HandleRedirectFromAuth.stories.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	import HandleRedirectFromAuth from './HandleRedirectFromAuth.svelte';
+
+	export const meta = {
+		title: 'Ui/Auth/HandleRedirectFromAuth',
+		component: HandleRedirectFromAuth
+	};
+</script>
+
+<script lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args></Template>
+
+<!-- This story is empty, as we don't actually want to redirect from Storybook. -->
+<Story name="Default" source />
