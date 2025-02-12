d98c3dabddfda8113a7a6e94679bb9547ca17bee
Chris Knight <111759008+ChrisKnightLDN@users.noreply.github.com>
2025-02-12 10:09:15 +0000

    add Popover story with clickable element (#855)

--- packages/ui/src/lib/popover/Popover.stories.svelte
+++ packages/ui/src/lib/popover/Popover.stories.svelte
@@ -9,7 +9,10 @@
 
 <script lang="ts">
 	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import Button from '../button/Button.svelte';
 	import Trigger from '../overlay/Trigger.svelte';
+
+	let count = 0;
 </script>
 
 <Template let:args>
@@ -48,5 +51,15 @@
 		<svelte:fragment slot="title">Metric note</svelte:fragment>
 
 		<svelte:fragment>Some text explaining this metric...</svelte:fragment>
+	</Popover>
+</Story>
+
+<Story name="Contains clickable element" source>
+	<Popover>
+		<Trigger slot="trigger" />
+		<svelte:fragment slot="title">Click the button Below</svelte:fragment>
+		<svelte:fragment>
+			<Button on:click={() => count++}>You can click me {count}</Button>
+		</svelte:fragment>
 	</Popover>
 </Story>
