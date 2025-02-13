072f3bb0ab80a291a1406cca848f1fd7c5b79df2
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2025-02-13 16:37:30 +0000

    delete unused template in story

--- packages/ui/src/lib/auth/HandleRedirectFromAuth.stories.svelte
+++ packages/ui/src/lib/auth/HandleRedirectFromAuth.stories.svelte
@@ -11,7 +11,5 @@
 	import { Story, Template } from '@storybook/addon-svelte-csf';
 </script>
 
-<Template let:args></Template>
-
 <!-- This story is empty, as we don't actually want to redirect from Storybook. -->
 <Story name="Default" source />
