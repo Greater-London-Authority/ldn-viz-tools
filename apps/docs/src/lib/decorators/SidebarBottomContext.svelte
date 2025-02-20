2aa7dcce482ee169444594e4ca49af04d5ee48e9
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2025-02-20 15:41:55 +0000

    created SidebarBottomContext for use in stories

---
+++ apps/docs/src/lib/decorators/SidebarBottomContext.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	const sidebarPlacementStore = writable('bottom');
+	setContext('sidebarPlacement', sidebarPlacementStore);
+</script>
+
+<div>
+	<slot />
+</div>
--- apps/docs/src/lib/index.ts
+++ apps/docs/src/lib/index.ts
@@ -1,4 +1,5 @@
 // Reexport your entry components here
 export { default as RelativeWrapper } from './decorators/RelativeWrapper.svelte';
+export { default as SidebarBottomContext } from './decorators/SidebarBottomContext.svelte';
 export { default as SidebarLeftContext } from './decorators/SidebarLeftContext.svelte';
 export { default as SidebarTopContext } from './decorators/SidebarTopContext.svelte';
