71959c6179384c1310802e196e61a91b72ea2f0e
Chris Knight <chris.knight@london.gov.uk>
2025-05-29 16:42:24 +0100

    workaround require bug in node v. 24

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,6 +1,8 @@
 import type { StorybookConfig } from '@storybook/sveltekit';
-
 import { dirname, join } from 'path';
+
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -9,6 +11,7 @@ import { dirname, join } from 'path';
 function getAbsolutePath(value: string): any {
 	return dirname(require.resolve(join(value, 'package.json')));
 }
+
 const config: StorybookConfig = {
 	stories: [
 		'../src/**/*.mdx',
