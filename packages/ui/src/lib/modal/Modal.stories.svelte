35344aa91d847787491fd137c60d8542d2a738ea
Chris Knight <chris.knight@london.gov.uk>
2025-06-04 09:37:06 +0100

    initial dev of modal

---
+++ packages/ui/src/lib/modal/Modal.stories.svelte
@@ -0,0 +1,79 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Modal from './Modal.svelte';
+	import Button from '../button/Button.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Ui/Components/Modal',
+		component: Modal,
+		tags: ['autodocs']
+		// argTypes: {
+		// 	isOpen: { options: ['true', 'false'], control: { type: 'radio' } },
+		// 	title: { type: 'string', control: 'text' },
+		// 	description: { type: 'string', control: 'text' },
+		// 	width: {
+		// 		options: ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl', 'full'],
+		// 		control: { type: 'select' }
+		// 	}
+		// },
+		// args: {
+		// 	title: 'Expects a title',
+		// 	description: 'Expects a summarising description of modal for accessibility'
+		// }
+	});
+
+	let isOpen = $state(false);
+</script>
+
+<Story name="Default">
+	{#snippet template(args)}
+		<Modal {...args} buttonText="Open Dialog">
+			{#snippet title()}
+				Account settings
+			{/snippet}
+
+			{#snippet description()}
+				Manage your account settings and preferences.
+			{/snippet}
+
+			<!-- Additional dialog content here... -->
+		</Modal>
+	{/snippet}
+</Story>
+
+<Story name="External Custom Trigger ">
+	{#snippet template(args)}
+		<Button variant="brand" onclick={() => (isOpen = true)}>Open Dialog</Button>
+		<Modal {...args} bind:open={isOpen}>
+			{#snippet title()}
+				Account settings
+			{/snippet}
+
+			{#snippet description()}
+				Manage your account settings and preferences.
+			{/snippet}
+
+			<!-- Additional dialog content here... -->
+		</Modal>
+	{/snippet}
+</Story>
+
+<Story name="Internal Custom Trigger">
+	{#snippet template(args)}
+		<Modal {...args}>
+			{#snippet trigger()}
+				<Button variant="brand">Open Dialog</Button>
+			{/snippet}
+
+			{#snippet title()}
+				Account settings
+			{/snippet}
+
+			{#snippet description()}
+				Manage your account settings and preferences.
+			{/snippet}
+
+			<!-- Additional dialog content here... -->
+		</Modal>
+	{/snippet}
+</Story>
---
+++ packages/ui/src/lib/modal/Modal.svelte
@@ -0,0 +1,136 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { Dialog, type DialogTriggerProps, type WithoutChild } from 'bits-ui';
+	import { classNames } from '../utils/classNames.js';
+	import Button from '../button/Button.svelte';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { XMark } from '@steeze-ui/heroicons';
+
+	type Props = Dialog.RootProps & {
+		buttonText: string;
+		title: Snippet;
+		description: Snippet;
+		trigger: Snippet<[DialogTriggerProps]>;
+
+		width:
+			| 'xs'
+			| 'sm'
+			| 'md'
+			| 'lg'
+			| 'xl'
+			| '2xl'
+			| '3xl'
+			| '4xl'
+			| '5xl'
+			| '6xl'
+			| '7xl'
+			| 'full';
+
+		headerTheme: 'light' | 'dark';
+
+		contentProps?: WithoutChild<Dialog.ContentProps>;
+		// ...other component props if you wish to pass them
+	};
+
+	let {
+		open = $bindable(false),
+		children,
+		buttonText,
+		contentProps,
+		/**
+		 * Title that appears at the top of the modal
+		 */
+		title,
+
+		/**
+		 * Description that appears below the title
+		 */
+		description,
+
+		trigger,
+
+		/**
+		 * width of the modal
+		 */
+		width = 'md',
+
+		/**
+		 * Colour scheme to apply to the header, either `light` or `dark`. The modal will respect global theme settings.
+		 */
+		headerTheme = 'dark',
+
+		...restProps
+	}: Props = $props();
+
+	const widthClasses = {
+		xs: 'max-w-xs',
+		sm: 'max-w-sm',
+		md: 'max-w-md',
+		lg: 'max-w-lg',
+		xl: 'max-w-xl',
+		'2xl': 'max-w-2xl',
+		'3xl': 'max-w-3xl',
+		'4xl': 'max-w-4xl',
+		'5xl': 'max-w-5xl',
+		'6xl': 'max-w-6xl',
+		'7xl': 'max-w-7xl',
+		full: 'max-w-full'
+	};
+
+	const modalClass = classNames(
+		'inline-block w-full max-h-full flex flex-col overflow-hidden text-left align-middle transition-all transform bg-color-container-level-0 shadow-xl pointer-events-auto',
+		widthClasses[width]
+	);
+</script>
+
+<Dialog.Root bind:open {...restProps}>
+	<!-- {#if trigger}
+		{@render trigger()}
+	{:else}
+		<Dialog.Trigger>
+			{#snippet child({ props })}
+
+				<Button {...props}>{buttonText}</Button>
+			{/snippet}
+		</Dialog.Trigger>
+	{/if} -->
+
+	<Dialog.Trigger>
+		{#snippet child({ props })}
+			<!-- {console.log(props)} -->
+			<Button {...props}>{buttonText}</Button>
+		{/snippet}
+	</Dialog.Trigger>
+
+	<Dialog.Portal>
+		<Dialog.Overlay class="fixed inset-0 z-40 bg-black/60" />
+		<div class="pointer-events-none fixed inset-8 z-50 flex items-center justify-center">
+			<Dialog.Content {...contentProps} class={modalClass}>
+				<div
+					class={`bg-color-container-level-1 text-color-text-primary border-color-static-brand relative flex items-center justify-between border-l-[5px] p-3 pr-4 ${headerTheme}`}
+				>
+					<Dialog.Title class="font-medium">
+						{@render title()}
+					</Dialog.Title>
+					<Dialog.Close>
+						{#snippet child({ props })}
+							<Button {...props} variant="square" emphasis="secondary" class="h-8 w-8 self-center">
+								<span class="sr-only">Close</span>
+								<Icon src={XMark} theme="solid" class="h-6 w-6" aria-hidden="true" />
+							</Button>
+						{/snippet}
+					</Dialog.Close>
+				</div>
+
+				<div class="overflow-y-auto">
+					<div class="px-4 py-6">
+						<Dialog.Description>
+							{@render description()}
+						</Dialog.Description>
+						{@render children?.()}
+					</div>
+				</div>
+			</Dialog.Content>
+		</div>
+	</Dialog.Portal>
+</Dialog.Root>
