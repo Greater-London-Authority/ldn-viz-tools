4853ee7a856714fe547973c29d839c1f7e9bb78a
Chris Knight <chris.knight@london.gov.uk>
2025-06-10 08:58:05 +0100

    create web project

---
+++ apps/web/README.md
@@ -0,0 +1,3 @@
+# @ldn-viz
+
+This contains documentation for the `@ldn-viz/maps`,`@ldn-viz/charts`, `@ldn-viz/ui` packages.
---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.postcss';
+
+	let { children } = $props();
+</script>
+
+<div class="container mx-auto">
+	{@render children()}
+</div>
