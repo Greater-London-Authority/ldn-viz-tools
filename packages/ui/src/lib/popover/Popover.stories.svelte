ef442730ccb8a4c0e3caa176e0f96d14fdc7820b
Chris Knight <chris.knight@london.gov.uk>
2025-06-11 13:52:10 +0100

    initial popover implementation

---
+++ packages/ui/src/lib/popover/Popover.stories.svelte
@@ -0,0 +1,19 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Popover from './Popover.svelte';
+	import Button from '../button/Button.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Ui/Components/Overlays/Popover',
+		component: Popover,
+		tags: ['autodocs']
+	});
+
+	let isOpen = $state(false);
+</script>
+
+<Story name="Default">
+	{#snippet template(args)}
+		<Popover {...args}>The contents of the popover...</Popover>
+	{/snippet}
+</Story>
---
+++ packages/ui/src/lib/popover/Popover.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { Popover } from 'bits-ui';
+	import Button from '../button/Button.svelte';
+
+	import { XMark } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+
+	type Props = Popover.RootProps & {
+		buttonText?: string;
+	};
+
+	let { buttonText, children }: Props = $props();
+</script>
+
+<Popover.Root>
+	<Popover.Trigger>
+		{#snippet child({ props })}
+			<!-- {console.log(props)} -->
+			<Button {...props}>{buttonText}</Button>
+		{/snippet}
+	</Popover.Trigger>
+	<Popover.Content
+		class="bg-color-container-level-0 border-color-ui-border-secondary z-50 w-60 border p-4 shadow-lg"
+	>
+		{@render children?.()}
+		<Popover.Close>
+			{#snippet child({ props })}
+				<Button
+					{...props}
+					variant="text"
+					emphasis="secondary"
+					size="sm"
+					class="absolute top-1 right-1"
+				>
+					<span class="sr-only">Close</span>
+					<Icon src={XMark} theme="mini" class="h-5 w-5" aria-hidden="true" />
+				</Button>
+			{/snippet}
+		</Popover.Close>
+		<Popover.Arrow class="text-color-ui-border-secondary" />
+	</Popover.Content>
+</Popover.Root>
