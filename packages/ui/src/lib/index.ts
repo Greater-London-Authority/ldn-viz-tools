2fd50efe95cb44d08138cdfb1597a2b06cfc188a
Chris Knight <chris.knight@london.gov.uk>
2025-06-11 12:30:43 +0100

    add components to export

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,8 +1,14 @@
 // Reexport your entry components here
 export { default as Button } from './button/Button.svelte';
+export { default as Callout } from './callout/Callout.svelte';
+export { default as CopyButton } from './copyButton/CopyButton.svelte';
 export { default as Flag } from './flag/Flag.svelte';
 
+export { default as Theme } from './theme/Theme.svelte';
 export { theme } from './theme/themeState.svelte.js';
 export { default as ThemeSwitcher } from './theme/ThemeSwitcher.svelte';
 
 export { prefersDarkMode, prefersReducedMotion } from './userPreference/userPreference.js';
+
+export { classNames } from './utils/classNames.js';
+export { randomId } from './utils/randomId.js';
