71577d3cacf0b31d2f7c0a34c9bf86e53585bbdd
Chris Knight <111759008+ChrisKnightLDN@users.noreply.github.com>
2025-06-13 11:13:39 +0100

    Update packages/ui/src/lib/overlay/Overlay.svelte
    Co-authored-by: James Scott-Brown <James.Scott-Brown@london.gov.uk>

--- packages/ui/src/lib/overlay/Overlay.svelte
+++ packages/ui/src/lib/overlay/Overlay.svelte
@@ -4,7 +4,7 @@
 	 * The `<Overlay>` component provides additional explanatory or help text when a user interacts with a trigger.
 	 * You can choose whether this is a modal, popover or tooltip, depending on your needs.
 	 *
-	 * The trigger can either be the built in `<Trigger>` component or a custom trigger passed into the relevant slot.
+	 * The trigger can either be the built in `<Trigger>` component or a custom trigger provided as a snippet.
 	 * @component
 	 */
 
