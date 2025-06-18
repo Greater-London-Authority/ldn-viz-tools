612c451d618ef6229eda684ddf64086b532012ae
James Scott-Brown <james.scott-brown@london.gov.uk>
2025-06-18 16:03:50 +0100

    add a showHeaderBottomRule option

--- packages/tables/src/lib/table/TableHeader.svelte
+++ packages/tables/src/lib/table/TableHeader.svelte
@@ -29,7 +29,7 @@
 	export let tableWidth;
 
 	$: topRuleClass = tableSpec.showHeaderTopRule === false ? '' : 'border-t';
-	$: bottomRuleClass = tableSpec.colGroups ? '' : 'border-b';
+	$: bottomRuleClass = (tableSpec.showHeaderBottomRule === false || tableSpec.colGroups) ? '' : 'border-b';
 </script>
 
 <div
@@ -55,7 +55,7 @@
 		<AxisRow {table} />
 	{/if}
 
-	{#if tableSpec.colGroups}
+	{#if tableSpec.colGroups && tableSpec.showHeaderBottomRule !== false}
 		<ColumnGroupHeadingRuleRow {table} />
 	{/if}
 </div>
