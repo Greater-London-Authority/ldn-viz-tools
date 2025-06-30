431b34b64b31c5f27413ab917b4bea07c6d92469
Chris Knight <chris.knight@london.gov.uk>
2025-06-30 15:10:42 +0100

    export component

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -5,4 +5,6 @@ export { default as Flag } from './flag/Flag.svelte';
 export { theme } from './theme/themeState.svelte.js';
 export { default as ThemeSwitcher } from './theme/ThemeSwitcher.svelte';
 
+export { default as Switch } from './switch/Switch.svelte';
+
 export { prefersDarkMode, prefersReducedMotion } from './userPreference/userPreference.js';
