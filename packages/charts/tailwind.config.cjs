8a6bafa96c0f1ef1f15df8e39e97575527de7b05
Chris Knight <chris.knight@london.gov.uk>
2025-07-08 14:15:33 +0100

    update tailwind config to ignore non tailwind repo modules

--- packages/charts/tailwind.config.cjs
+++ packages/charts/tailwind.config.cjs
@@ -1,6 +1,6 @@
 const config = {
 	presets: [require('@ldn-viz/themes/ldn-theme')],
-	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/@ldn-viz/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/@ldn-viz/**/*.{html,js,svelte,ts}', '!./node_modules/@ldn-viz/*-config/**/*.{html,js,svelte,ts}'],
 
 	theme: {}
 };
