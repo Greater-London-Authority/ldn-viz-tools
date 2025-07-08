ab886b2e54b0851de13db0a05fe132cdccae256f
Chris Knight <chris.knight@london.gov.uk>
2025-07-08 14:25:29 +0100

    format tailwind config files

--- packages/charts/tailwind.config.cjs
+++ packages/charts/tailwind.config.cjs
@@ -1,6 +1,10 @@
 const config = {
 	presets: [require('@ldn-viz/themes/ldn-theme')],
-	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/@ldn-viz/**/*.{html,js,svelte,ts}', '!./node_modules/@ldn-viz/*-config/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/@ldn-viz/**/*.{html,js,svelte,ts}',
+		'!./node_modules/@ldn-viz/*-config/**/*.{html,js,svelte,ts}'
+	],
 
 	theme: {}
 };
