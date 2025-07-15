e40ba33779ed5fe009203aef720776281055b891
James Scott-Brown <james.scott-brown@london.gov.uk>
2025-07-15 15:58:19 +0100

    config change to avoid error from exported enum

--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({ script: true}),
 	kit: { adapter: adapter() }
 };
 
