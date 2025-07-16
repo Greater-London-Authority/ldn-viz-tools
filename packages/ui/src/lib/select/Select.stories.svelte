420f05fb7bd34c68441c02ec3c09e0e2dcc8a483
James Scott-Brown <james.scott-brown@london.gov.uk>
2025-07-16 13:13:55 +0100

    start migrating select component

---
+++ packages/ui/src/lib/select/Select.stories.svelte
@@ -0,0 +1,192 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Select from './Select.svelte';
+
+	const options: Option[] = [
+		{ label: 'One', value: 1 },
+		{ label: 'Two', value: 2 },
+		{ label: 'Three', value: 3 }
+	];
+
+	const { Story } = defineMeta({
+		title: 'Ui/Components/Select',
+		component: Select,
+		tags: ['autodocs'],
+		args: {
+			label: 'Label',
+			options
+		}
+	});
+
+	type Option = { label: string; value: number };
+	let value: Option | undefined = $state();
+
+	// let justValue: number | null = $state();
+	// let justValueMulti: number[] | null = $state();
+	let error = $state('');
+</script>
+
+<Story name="Basic">
+	{#snippet template(args)}
+		<div class="w-96">
+			<Select {...args} {options} id="labelled-input" />
+		</div>
+	{/snippet}
+</Story>
+
+<!--
+<Template>
+	{#snippet children({ args })}
+		<Select {...args} />
+	{/snippet}
+</Template>
+
+<Story name="Default" args={{ options: options }} source />
+-->
+
+<!--
+<Story name="Basic">
+	<div class="w-96">
+		<Select {options} id="labelled-input" />
+	</div>
+</Story>
+
+<Story name="With Label">
+	<div class="w-96">
+		<Select {options} label="Label" id="labelled-input" />
+	</div>
+</Story>
+
+<Story name="Optional">
+	<div class="w-96">
+		<Select {options} label="Label" id="labelled-input" optional />
+	</div>
+</Story>
+
+<Story name="Custom Placeholder">
+	<div class="w-96">
+		<Select {options} label="Label" id="labelled-input" placeholder="Custom Placeholder text" />
+	</div>
+</Story>
+
+<Story name="With Context Hint">
+	<div class="w-96">
+		<Select {options} label="Label" id="labelled-input">
+			{#snippet hint()}
+				<Overlay hintLabel="optional hint label">Contextual help text</Overlay>
+			{/snippet}
+		</Select>
+	</div>
+</Story>
+
+<Story name="Description">
+	<div class="w-96">
+		<Select {options} label="Label" id="labelled-input" description="descriptive text" />
+	</div>
+</Story>
+
+<Story name="Description alignment">
+	<div class="w-96">
+		<Select
+			{options}
+			label="Label"
+			id="labelled-input"
+			placeholder="Placeholder text"
+			description="descriptive text"
+			descriptionAlignment="right"
+			optional
+		/>
+	</div>
+</Story>
+
+<Story name="Multiple selects">
+	<div class="w-96">
+		<Select
+			{options}
+			label="Label"
+			id="labelled-input"
+			placeholder="Placeholder text"
+			description="descriptive text"
+			optional
+			multiple
+			bind:value
+			on:change={() => console.log('Selection changed!')}
+		/>
+
+		<span class="text-color-text-secondary mt-4 block">
+			Value is: {JSON.stringify(value)}
+		</span>
+	</div>
+</Story>
+
+<Story name="Error">
+	<div class="w-96">
+		<Select
+			{options}
+			label="Label"
+			id="labelled-input"
+			placeholder="Placeholder text"
+			description="descriptive text"
+			optional
+			error="something has gone wrong here"
+		/>
+	</div>
+</Story>
+
+<Story name="Disabled">
+	<div class="w-96">
+		<Select
+			{options}
+			label="Label"
+			id="labelled-input"
+			placeholder="Placeholder text"
+			description="descriptive text"
+			disabled
+			optional
+		/>
+	</div>
+</Story>
+
+<Story name="Binding to justValue">
+	<div class="flex w-[500px] flex-col gap-2">
+		<p>
+			You can bind directly to <code>justValue</code>, rather than <code>value</code> (which is an
+			object including the <code>label</code> as well as <code>value</code>)
+		</p>
+
+		<div>Current value: <span class="font-bold">{justValue}</span></div>
+
+		<Button on:click={() => (justValue = 2)}>Reset to 2</Button>
+		<Button on:click={() => (justValue = null)}>Clear</Button>
+
+		<Select {options} bind:justValue id="labelled-input" />
+	</div>
+</Story>
+
+<Story name="Binding to justValue - multi">
+	<div class="flex w-[500px] flex-col gap-2">
+		<p>
+			You can bind directly to <code>justValue</code>, rather than <code>value</code> (which is an
+			object including the <code>label</code> as well as <code>value</code>)
+		</p>
+
+		<div>Current value: <span class="font-bold">{justValueMulti}</span></div>
+
+		<Button on:click={() => (justValueMulti = [1, 3])}>Reset to 1 and 3</Button>
+		<Button on:click={() => (justValueMulti = null)}>Clear</Button>
+
+		<Select {options} bind:justValue={justValueMulti} id="labelled-input" multiple />
+	</div>
+</Story>
+
+<Story name="Setting and clearing error message">
+	<div class="flex w-[500px] flex-col gap-2">
+		<Button on:click={() => (error = 'OH NO')}>Set error</Button>
+		<Button on:click={() => (error = '')}>Clear error</Button>
+
+		<span><code>error is:</code> {error}</span>
+
+		<Select {options} id="labelled-input" {error} />
+	</div>
+</Story>
+-->
---
+++ packages/ui/src/lib/select/Select.svelte
@@ -0,0 +1,189 @@
+<script lang="ts">
+	import type { ComponentProps } from 'svelte';
+
+	/**
+	 * The `Select` component allows users to select an option form a drop-down list of alternatives.
+	 * Our select element is a wrapper around ['Svelte Select'](https://github.com/rob-balfre/svelte-select).
+	 * Many of the props exposed by this component are provided by `svelte-select`, so you may find it helpful to consult its documentation and [list of examples](https://svelte-select-examples.vercel.app/examples).
+	 *
+	 * Notably, this wrapper implements a `justValues` prop that can be bound to, and the `InputWrapper` chrome (label, description, tooltip, error, etc.)
+	 * @component
+	 */
+
+	import Svelecte from 'svelecte';
+
+	import InputWrapper from '../input/InputWrapper.svelte';
+	import type { InputProps } from '../input/types';
+	import Overlay from '../overlay/Overlay.svelte';
+	import Trigger from '../overlay/Trigger.svelte';
+
+	type SvelectComponentPropsType = ComponentProps<typeof Svelecte>;
+
+	interface Props extends InputProps, SvelectComponentPropsType {}
+
+	// TODO: chek events forwared
+
+	let {
+		value = $bindable(undefined),
+
+		//svelect stuff
+		options,
+
+		name,
+		inputId,
+		required,
+		//disabled,
+		optionResolver,
+		valueAsObject,
+		parentValue,
+		valueField,
+		labelField,
+		groupLabelField,
+		groupItemsField,
+		disabledField,
+		//placeholder,
+		searchable,
+		clearable,
+		renderer,
+		disableHighlight,
+		highlightFirstItem,
+		selectOnTab,
+		resetOnBlur,
+		resetOnSelect,
+		closeAfterSelect,
+		deselectMode,
+		strictMode,
+		multiple,
+		max,
+		collapseSelection,
+		keepSelectionInList,
+		creatable,
+		creatablePrefix,
+		allowEditing,
+		keepCreated,
+		delimiter,
+		createFilter,
+		createHandler,
+		fetch,
+		fetchProps,
+		fetchCallback,
+		fetchResetOnBlur,
+		fetchDebounceTime,
+		minQuery,
+		lazyDropdown,
+		positionResolver,
+		virtualList,
+		vlItemSize,
+		searchProps,
+		class: classes,
+		i18n,
+		dndzone,
+		anchor_element,
+		controlClass,
+		dropdownClass,
+		optionClass,
+
+		// input wrapper stuff
+		id,
+		disabled,
+		error,
+
+		// overlays
+		hint,
+		hintLabel,
+		customOverlay,
+
+		...restProps
+	}: Props = $props();
+
+	const hint_render = $derived(hint);
+
+	/*
+	let propsForInputWrapper = $derived.by( () => {
+
+		InputProps 
+		return restProps.createFilter()
+
+	} )
+
+	*/
+
+	type f = keyof InputProps;
+
+	console.log();
+</script>
+
+<InputWrapper {...restProps} {id} {disabled} {error}>
+	{#if hint}
+		<Overlay>
+			{#snippet trigger(props)}
+				<Trigger {...props} size="xs" {hintLabel} />
+			{/snippet}
+			{hint}
+		</Overlay>
+	{/if}
+
+	{#if customOverlay}
+		{@render customOverlay()}
+	{/if}
+
+	<div>
+		<Svelecte
+			bind:value
+			{options}
+			{name}
+			{inputId}
+			{required}
+			{optionResolver}
+			{valueAsObject}
+			{parentValue}
+			{valueField}
+			{labelField}
+			{groupLabelField}
+			{groupItemsField}
+			{disabledField}
+			{searchable}
+			{clearable}
+			{renderer}
+			{disableHighlight}
+			{highlightFirstItem}
+			{selectOnTab}
+			{resetOnBlur}
+			{resetOnSelect}
+			{closeAfterSelect}
+			{deselectMode}
+			{strictMode}
+			{multiple}
+			{max}
+			{collapseSelection}
+			{keepSelectionInList}
+			{creatable}
+			{creatablePrefix}
+			{allowEditing}
+			{keepCreated}
+			{delimiter}
+			{createFilter}
+			{createHandler}
+			{fetch}
+			{fetchProps}
+			{fetchCallback}
+			{fetchResetOnBlur}
+			{fetchDebounceTime}
+			{minQuery}
+			{lazyDropdown}
+			{positionResolver}
+			{virtualList}
+			{vlItemSize}
+			{searchProps}
+			class={classes}
+			{i18n}
+			{dndzone}
+			{anchor_element}
+			{controlClass}
+			{dropdownClass}
+			{optionClass}
+		/>
+	</div>
+</InputWrapper>
+
+{#if true}{/if}
