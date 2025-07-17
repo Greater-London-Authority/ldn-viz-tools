68c39c5eddd4bb077f98e4ec9ef9d0aed2902ccd
James Scott-Brown <james.scott-brown@london.gov.uk>
2025-07-17 17:19:01 +0100

    re-enable remaining exports

--- packages/charts/src/lib/index.ts
+++ packages/charts/src/lib/index.ts
@@ -4,11 +4,10 @@ export { default as Footer } from './chartContainer/Footer.svelte';
 export { default as SubTitle } from './chartContainer/SubTitle.svelte';
 export { default as Title } from './chartContainer/Title.svelte';
 
-// TODO
-// export { default as ObservablePlot } from './observablePlot/ObservablePlot.svelte';
+export { default as ObservablePlot } from './observablePlot/ObservablePlot.svelte';
 
-// export { default as ObservablePlotInner } from './observablePlot/ObservablePlotInner.svelte';
-// export * from './observablePlot/ObservablePlotInner.svelte';
+export * from './observablePlot/ObservablePlotInner.svelte';
+export { default as ObservablePlotInner } from './observablePlot/ObservablePlotInner.svelte';
 
 export * from './observablePlotFragments/observablePlotFragments';
 export * from './observablePlotFragments/plot.js';
