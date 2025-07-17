965cd90979727195ab43107eaa13e863e8fa709b
Chris Knight <111759008+ChrisKnightLDN@users.noreply.github.com>
2025-07-17 15:04:50 +0100

    add async button + loading indicator and spinner (#1011)
    ---------
    
    Co-authored-by: James Scott-Brown <James.Scott-Brown@london.gov.uk>

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,4 +1,5 @@
 // Reexport your entry components here
+export { default as AsyncButton } from './button/AsyncButton.svelte';
 export { default as Button } from './button/Button.svelte';
 export { default as Callout } from './callout/Callout.svelte';
 export { default as Checkbox } from './checkBox/Checkbox.svelte';
@@ -6,15 +7,23 @@ export { default as CheckboxGroup } from './checkBox/CheckboxGroup.svelte';
 export { default as CopyButton } from './copyButton/CopyButton.svelte';
 export { default as DataDownloadButton } from './dataDownloadButton/DataDownloadButton.svelte';
 export { default as Flag } from './flag/Flag.svelte';
+
+export { default as LoadingIndicator } from './loadingIndicator/LoadingIndicator.svelte';
+
 export { default as Input } from './input/Input.svelte';
 export { default as InputWrapper } from './input/InputWrapper.svelte';
+
 export { default as Modal } from './modal/Modal.svelte';
 export { default as MultipleActionButton } from './multipleActionButton/MultipleActionButton.svelte';
 export { default as NavigationMenu } from './navigationMenu/NavigationMenu.svelte';
 export { default as Overlay } from './overlay/Overlay.svelte';
 export { default as Trigger } from './overlay/Trigger.svelte';
 export { default as Popover } from './popover/Popover.svelte';
+
+export { default as Spinner } from './spinners/Spinner.svelte';
+
 export { default as Select } from './select/Select.svelte';
+
 export { default as Switch } from './switch/Switch.svelte';
 export { default as Tooltip } from './tooltip/Tooltip.svelte';
 
