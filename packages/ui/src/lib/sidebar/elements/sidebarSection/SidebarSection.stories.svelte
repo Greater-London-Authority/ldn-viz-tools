8e770cfda4eb49e4b121b9a989562801da3e41eb
Chris Knight <chris.knight@london.gov.uk>
2025-07-18 15:06:47 +0100

    sidebar section

--- packages/ui/src/lib/sidebar/elements/sidebarSection/SidebarSection.stories.svelte
+++ packages/ui/src/lib/sidebar/elements/sidebarSection/SidebarSection.stories.svelte
@@ -1,67 +1,75 @@
 <script context="module">
-	import SidebarSection from './SidebarSection.svelte';
-
-	export const meta = {
-		title: 'Ui/Components - Layout And Themes/Sidebar/elements/SidebarSection',
-		component: SidebarSection
-	};
-</script>
-
-<script>
-	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
 	import Overlay from '../../../overlay/Overlay.svelte';
 	import SidebarGroupTitle from './sidebarGroupTitle/SidebarGroupTitle.svelte';
+	import SidebarSection from './SidebarSection.svelte';
 	import SidebarSectionTitle from './sidebarSectionTitle/SidebarSectionTitle.svelte';
+
+	/**
+	 * The `<SidebarSection>` component is used to group together a 'section' of content within a `<Sidebar>`.
+	 * As well as accepting a section title as a prop (which it displays as a `<SidebarSectionTitle>`), it accepts the section content as a slot.
+	 */
+
+	const { Story } = defineMeta({
+		title: 'Ui/Components - Layout And Themes/Sidebar/elements/SidebarSection',
+		component: SidebarSection,
+		tags: ['autodocs']
+	});
 </script>
 
-<Template let:args>
-	<SidebarSection {...args} />
-</Template>
-
-<Story name="Default" source>
-	<SidebarSection title="Section Title">Section</SidebarSection>
+<Story name="Default">
+	{#snippet template()}
+		<SidebarSection title="Section Title">Section</SidebarSection>
+	{/snippet}
 </Story>
 
-<Story name="With Titled Group" source>
-	<SidebarSection title="Section Title">
-		Section Content
-		<div>
-			<SidebarGroupTitle>
-				Pay Attention to this group
-				<Overlay slot="hint" hintLabel="why">Beacuse it's Awesome!</Overlay>
-			</SidebarGroupTitle>
-			Grouped content
-		</div>
-	</SidebarSection>
+<Story name="With Titled Group">
+	{#snippet template()}
+		<SidebarSection title="Section Title">
+			Section Content
+			<div>
+				<SidebarGroupTitle>
+					Pay Attention to this group
+					{#snippet hint()}
+						<Overlay hintLabel="why">Beacuse it's Awesome!</Overlay>
+					{/snippet}
+				</SidebarGroupTitle>
+				Grouped content
+			</div>
+		</SidebarSection>
+	{/snippet}
 </Story>
 
-<!-- You can add a subtitle or hint text by providing a `<SidebarSection>` component within the `title` slot. -->
-<Story name="With SidebarSectionTitle component" source>
-	<SidebarSection title="Section Title">
-		<svelte:fragment slot="title">
-			<SidebarSectionTitle>
-				<svelte:fragment>The title of this section</svelte:fragment>
+<!-- You can add a subtitle or hint text by providing a `<SidebarSectionTitle>` as the title snippet. You can either pass a string or a snippet - not both. -->
+<Story name="With SidebarSectionTitle component">
+	{#snippet template()}
+		<SidebarSection>
+			{#snippet title()}
+				<SidebarSectionTitle>
+					The title of this section
 
-				<svelte:fragment slot="subTitle">The sub-title of ths section.</svelte:fragment>
+					{#snippet subTitle()}The sub-title of ths section.{/snippet}
 
-				<Overlay slot="hint" hintLabel="About">
-					This popover should provide some additional explanation about this section.
-				</Overlay>
-			</SidebarSectionTitle>
-		</svelte:fragment>
+					{#snippet hint()}<Overlay hintLabel="About">
+							This popover should provide some additional explanation about this section.
+						</Overlay>
+					{/snippet}
+				</SidebarSectionTitle>
+			{/snippet}
 
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-		laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-		voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-		<div>
-			<SidebarGroupTitle>
-				Pay Attention to this group
-				<Overlay slot="hint" hintLabel="why">Beacuse it's Awesome!</Overlay>
-			</SidebarGroupTitle>
-			Grouped content
-		</div>
-	</SidebarSection>
+			<div>
+				<SidebarGroupTitle>
+					Pay Attention to this group
+					{#snippet hint()}<Overlay hintLabel="why">Beacuse it's Awesome!</Overlay>{/snippet}
+				</SidebarGroupTitle>
+				Grouped content
+			</div>
+		</SidebarSection>
+	{/snippet}
 </Story>
