8ac4a79fe4b8c79aa8f994d6f963b85585e52ca8
Chris Knight <111759008+ChrisKnightLDN@users.noreply.github.com>
2025-07-18 13:21:59 +0100

    Sv5 analytics (#1025)

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -7,36 +7,31 @@ export { default as CheckboxGroup } from './checkBox/CheckboxGroup.svelte';
 export { default as CopyButton } from './copyButton/CopyButton.svelte';
 export { default as DataDownloadButton } from './dataDownloadButton/DataDownloadButton.svelte';
 export { default as Flag } from './flag/Flag.svelte';
-
 export { default as ImageDownloadButton } from './imageDownloadButton/ImageDownloadButton.svelte';
-
+export { default as Input } from './input/Input.svelte';
+export { default as InputWrapper } from './input/InputWrapper.svelte';
+export { default as LoadingIndicator } from './loadingIndicator/LoadingIndicator.svelte';
 export { default as LogoByCIU } from './logos/LogoByCIU.svelte';
 export { default as LogoCIU } from './logos/LogoCIU.svelte';
 export { default as LogoLOTI } from './logos/LogoLOTI.svelte';
 export { default as LogoMayor } from './logos/LogoMayor.svelte';
-
-export { default as LoadingIndicator } from './loadingIndicator/LoadingIndicator.svelte';
-
-export { default as Input } from './input/Input.svelte';
-export { default as InputWrapper } from './input/InputWrapper.svelte';
-
 export { default as Modal } from './modal/Modal.svelte';
 export { default as MultipleActionButton } from './multipleActionButton/MultipleActionButton.svelte';
 export { default as NavigationMenu } from './navigationMenu/NavigationMenu.svelte';
 export { default as Overlay } from './overlay/Overlay.svelte';
 export { default as Trigger } from './overlay/Trigger.svelte';
 export { default as Popover } from './popover/Popover.svelte';
-
-export { default as Spinner } from './spinners/Spinner.svelte';
-
 export { default as Select } from './select/Select.svelte';
-
+export { default as Spinner } from './spinners/Spinner.svelte';
 export { default as Switch } from './switch/Switch.svelte';
 export { default as Tooltip } from './tooltip/Tooltip.svelte';
 
 export { default as Theme } from './theme/Theme.svelte';
 export { theme } from './theme/themeState.svelte.js';
 export { default as ThemeSwitcher } from './theme/ThemeSwitcher.svelte';
+
+export { default as AnalyticsAndCookieConsent } from './analytics/AnalyticsAndCookieConsent.svelte';
+export { default as CookieControlSettings } from './analytics/CookieControlSettings.svelte';
 
 export { default as Geocoder } from './geolocation/Geocoder.svelte';
 export * from './geolocation/GeocoderAdapter';
