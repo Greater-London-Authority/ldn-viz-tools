0f270ef79af963bb2d50ffb654a3be6dcd1dfa0f
Chris Knight <chris.knight@london.gov.uk>
2025-07-18 14:47:11 +0100

    section group title

---
+++ packages/ui/src/lib/sidebar/elements/sidebarSection/sidebarGroupTitle/SidebarGroupTitle.stories.svelte
@@ -0,0 +1,44 @@
+<script context="module">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Overlay from '../../../../overlay/Overlay.svelte';
+	import SidebarGroupTitle from './SidebarGroupTitle.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Ui/Components - Layout And Themes/Sidebar/elements/SidebarSection/Group Title',
+		component: SidebarGroupTitle
+	});
+</script>
+
+<Story name="Default">
+	{#snippet template()}
+		<SidebarGroupTitle>Group Title</SidebarGroupTitle>
+	{/snippet}
+</Story>
+
+<Story name="With Subtitle">
+	{#snippet template()}
+		<SidebarGroupTitle>
+			Section Title
+			{#snippet subTitle()}
+				<p>Maecenas ut libero vel nibh maximus feugiat non sed tortor.</p>
+			{/snippet}
+		</SidebarGroupTitle>
+	{/snippet}
+</Story>
+
+<Story name="With Hint">
+	{#snippet template()}
+		<SidebarGroupTitle>
+			Group Title
+			{#snippet hint()}
+				<Overlay>
+					<p class="mb-4">Any content you want can go here</p>
+					<p>
+						Maecenas ut libero vel nibh maximus feugiat non sed tortor. Sed in lacinia dui, nec
+						venenatis sapien. Etiam venenatis felis.
+					</p>
+				</Overlay>
+			{/snippet}
+		</SidebarGroupTitle>
+	{/snippet}
+</Story>
---
+++ packages/ui/src/lib/sidebar/elements/sidebarSection/sidebarGroupTitle/SidebarGroupTitle.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	interface Props {
+		children?: import('svelte').Snippet;
+		hint?: import('svelte').Snippet;
+		subTitle?: import('svelte').Snippet;
+	}
+
+	let { children, hint, subTitle }: Props = $props();
+	/**
+	 * The `<SidebarGroupTitle>` component is used to display a subtitle to subdivide a `<SidebarSection>`.
+	 * @component
+	 */
+</script>
+
+<div class=" text-color-text-primary">
+	<div class="flex items-end justify-between">
+		<h3 class="text-sm font-semibold leading-snug">
+			<!-- The title. -->
+			{@render children?.()}
+		</h3>
+
+		{#if hint}
+			<!-- An optional `<Overlay>` component to provide additional explanation. -->
+			{@render hint?.()}
+		{/if}
+	</div>
+
+	{#if subTitle}
+		<div class="my-1 text-xs">
+			<!-- Optional longer subtitle to display below the main title. -->
+			{@render subTitle?.()}
+		</div>
+	{/if}
+</div>
