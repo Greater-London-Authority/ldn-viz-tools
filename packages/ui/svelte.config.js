048ce62ddf6ead7b71205e784e0bbd0daa770401
Chris Knight <chris.knight@london.gov.uk>
2025-07-18 11:16:28 +0100

    add analytics, supress unused css seletor warnings

--- packages/ui/svelte.config.js
+++ packages/ui/svelte.config.js
@@ -3,7 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter() },
+	onwarn: (warning, handler) => {
+		const { code } = warning;
+		if (code === 'css_unused_selector') return;
+		handler(warning);
+	}
 };
 
 export default config;
