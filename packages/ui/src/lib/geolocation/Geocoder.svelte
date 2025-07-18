2cf08970dfd953e4e82d2002b6b99f5567481882
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2025-07-18 10:30:33 +0100

    migrate the geolocation components (#1022)

---
+++ packages/ui/src/lib/geolocation/Geocoder.svelte
@@ -0,0 +1,359 @@
+<script lang="ts" module>
+	type Timeout = ReturnType<typeof setTimeout>;
+
+	const newDebouncer = (func: () => void, delay: number) => {
+		let timerId: undefined | Timeout = undefined;
+
+		const restart = () => {
+			clearTimeout(timerId);
+			timerId = setTimeout(func, delay);
+		};
+
+		const clear = () => {
+			clearTimeout(timerId);
+		};
+
+		return [restart, clear];
+	};
+</script>
+
+<script lang="ts">
+	import { MagnifyingGlass, XMark } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+
+	import type { GeocoderAdapter } from './GeocoderAdapter';
+	import type {
+		Geolocation,
+		GeolocationNamed,
+		GeolocationSearchError,
+		OnGeolocationSearchError,
+		OnGeolocationSearchResult,
+		OnSuggestionListInteraction
+	} from './types';
+
+	interface Props {
+		/**
+		 * adapter to source location suggestions from.
+		 */
+		adapter: GeocoderAdapter;
+		/**
+		 * delay in ms after a keystroke to minimise redundant API requests.
+		 */
+		delay?: number;
+		/**
+		 * called when a user clicks a location from the suggestions list.
+		 */
+		onLocationSelected: undefined | OnGeolocationSearchResult;
+		/**
+		 * called when the adapter promise rejects a search request.
+		 */
+		onSearchError: undefined | OnGeolocationSearchError;
+		/**
+		 * Called when the user clears the search box.
+		 */
+		onSearchClear?: any;
+		/**
+		 * suggestions can be bound via 'bind:suggestions' to reactively receive
+		 * changes to search results.
+		 */
+		suggestions?: GeolocationNamed[];
+		/**
+		 * The currently selected location.
+		 */
+		selected?: any;
+		/**
+		 * a space-separated list of additional classes applied to the root container.
+		 */
+		classes?: string;
+		/**
+		 * a space-separated list of additional classes applied to
+		 * the input element because laying input elements can be quite fiddly.
+		 */
+		inputClasses?: string;
+		/**
+		 * `showClearButton` forces the showing of the clear button at the end of the
+		 * input element.
+		 *
+		 * It is set to `false` each time the user clicks the button so binding on this
+		 * prop is useful.
+		 */
+		showClearButton?: boolean;
+		/**
+		 * Placeholder text to be displayed in the input element.
+		 */
+		placeholder?: string;
+		children?: import('svelte').Snippet<[any]>;
+	}
+
+	let {
+		adapter,
+		delay = 250,
+		onLocationSelected,
+		onSearchError,
+		onSearchClear = () => {},
+		suggestions = $bindable(),
+		selected = $bindable(null),
+		classes = '',
+		inputClasses = '',
+		showClearButton = $bindable(false),
+		placeholder = 'Location search',
+		children
+	}: Props = $props();
+
+	let container: HTMLElement = $state();
+	let input: HTMLInputElement = $state();
+	let query = $state('');
+	let silentQueryTextUpdate = $state(false);
+	let showSuggestionList = $state(false);
+
+	// apply fallback - can't set directly with a bindable
+	if (!suggestions) {
+		suggestions = [];
+	}
+
+	const updateSuggestionsNow = async () => {
+		if (!query || query.length < 3) {
+			suggestions = [];
+			showSuggestionList = false;
+			return;
+		}
+
+		try {
+			suggestions = await adapter
+				.search(query)
+				.then((res) => res || [])
+				.catch((err: unknown) => {
+					console.error('[Location Search] Search suggestions could not be retrieved.');
+
+					// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+					onSearchError && onSearchError(err as GeolocationSearchError);
+					return [];
+				});
+
+			showSuggestionList = !!children;
+		} catch (e: unknown) {
+			console.error(e);
+			showSuggestionList = false;
+		}
+	};
+
+	const [scheduleUpdate, cancelUpdate] = newDebouncer(updateSuggestionsNow, delay);
+
+	const closeSuggestionsList = () => {
+		cancelUpdate();
+		showSuggestionList = false;
+	};
+
+	const onSelect = (suggestion: Geolocation) => {
+		closeSuggestionsList();
+		selected = suggestion;
+
+		if (suggestion.name) {
+			query = suggestion.name;
+			silentQueryTextUpdate = true;
+		} else if (suggestion.address) {
+			query = suggestion.address;
+			silentQueryTextUpdate = true;
+		}
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+		onLocationSelected && onLocationSelected(suggestion);
+	};
+
+	const onSuggestionEvent: OnSuggestionListInteraction = (
+		event: Event,
+		suggestion: GeolocationNamed
+	) => {
+		const pressEvent = event as KeyboardEvent;
+		const clickEvent = event as PointerEvent;
+
+		if (pressEvent.key === 'Enter' || clickEvent.type === 'click') {
+			onSelect(suggestion);
+			return;
+		}
+
+		if (pressEvent.key === 'Escape') {
+			closeSuggestionsList();
+			return;
+		}
+
+		if (pressEvent.key === 'ArrowDown') {
+			focusOnNextSuggestion(suggestion);
+			return;
+		}
+
+		if (pressEvent.key === 'ArrowUp') {
+			if (isFirstSuggestion(suggestion)) {
+				focusOnInput();
+				return;
+			}
+
+			focusOnPrevSuggestion(suggestion);
+		}
+	};
+
+	const findSuggestionIndex = (suggestion?: GeolocationNamed) => {
+		if (!suggestion || !suggestions) {
+			return -1;
+		}
+
+		for (let i = 0; i < suggestions.length; i++) {
+			if (suggestions[i].id === suggestion.id) {
+				return i;
+			}
+		}
+
+		return -1;
+	};
+
+	const isFirstSuggestion = (suggestion: GeolocationNamed) => {
+		return findSuggestionIndex(suggestion) === 0;
+	};
+
+	const focusOnPrevSuggestion = (suggestion: GeolocationNamed) => {
+		const i = findSuggestionIndex(suggestion);
+		if (i >= 0) {
+			const prev = i - 1 >= 0 ? suggestions[i - 1] : undefined;
+			focusOnLocation(prev);
+		}
+	};
+
+	const focusOnNextSuggestion = (suggestion: GeolocationNamed) => {
+		const i = findSuggestionIndex(suggestion);
+		if (i >= 0) {
+			const next = i + 1 < suggestions.length ? suggestions[i + 1] : undefined;
+			focusOnLocation(next);
+		}
+	};
+
+	const focusOnLocation = (suggestion?: GeolocationNamed) => {
+		if (!suggestion) {
+			return;
+		}
+
+		const elem = document.querySelector(`[data-geocoder-suggestion-id="${suggestion.id}"]`);
+		if (elem) {
+			(elem as HTMLElement).focus();
+		}
+	};
+
+	const focusOnInput = () => {
+		input.focus();
+		setTimeout(() => {
+			const end = input.value.length;
+			input.setSelectionRange(end, end);
+		}, 0);
+	};
+
+	const hideSuggestionList = (event: Event) => {
+		const target = event.target as HTMLElement;
+		if (!target || !container || !isElementOrChildOf(target, container)) {
+			closeSuggestionsList();
+		}
+	};
+
+	const isElementOrChildOf = (element: HTMLElement, parent: HTMLElement) => {
+		const positionBitmask = element.compareDocumentPosition(parent);
+		return !!(positionBitmask & Node.DOCUMENT_POSITION_CONTAINS);
+	};
+
+	const reshowSuggestionList = (event: Event) => {
+		if (suggestions) {
+			showSuggestionList = !!children;
+		}
+
+		navigateSuggestionList(event);
+	};
+
+	const navigateSuggestionList = (event: Event) => {
+		if (!showSuggestionList || !suggestions || suggestions.length === 0) {
+			return;
+		}
+
+		const pressEvent = event as KeyboardEvent;
+
+		if (pressEvent.key === 'Enter') {
+			onSelect(suggestions[0]);
+			return;
+		}
+
+		if (pressEvent.key === 'Escape') {
+			closeSuggestionsList();
+			return;
+		}
+
+		if (pressEvent.key === 'ArrowDown') {
+			focusOnLocation(suggestions[0]);
+		}
+	};
+
+	const clearSearch = () => {
+		showClearButton = false;
+		query = '';
+		selected = null;
+		suggestions = [];
+		showSuggestionList = false;
+
+		onSearchClear();
+	};
+
+	// TODO: check this state change inside effect is ok
+	$effect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+		query;
+		if (!silentQueryTextUpdate) {
+			scheduleUpdate();
+		} else {
+			silentQueryTextUpdate = false;
+		}
+	});
+</script>
+
+<svelte:window onmousedown={hideSuggestionList} onkeyup={hideSuggestionList} />
+
+<search
+	bind:this={container}
+	class="bg-color-input-background pointer-events-auto relative flex h-10 w-full {classes}"
+>
+	<div class="absolute left-2 top-2 flex items-center justify-center">
+		<Icon src={MagnifyingGlass} class="text-color-input-icon h-6 w-6" />
+	</div>
+
+	<input
+		bind:this={input}
+		type="search"
+		{placeholder}
+		class="form-input bg-color-input-background border-color-input-border placeholder-color-input-placeholder text-color-valuetext h-full w-64 min-w-0 max-w-[100%] shrink grow border pl-10 {inputClasses}"
+		class:pr-8={showClearButton}
+		bind:value={query}
+		onfocus={reshowSuggestionList}
+		onclick={reshowSuggestionList}
+		onkeyup={reshowSuggestionList}
+	/>
+
+	{#if showClearButton || query?.length > 0}
+		<button
+			onclick={clearSearch}
+			class="y-auto bg-color-input-background absolute right-1 top-1 flex items-center justify-center"
+			title="Clear search and marker"
+		>
+			<Icon src={XMark} class="p-0.25 text-color-input-icon h-8 w-8" />
+		</button>
+	{/if}
+
+	{#if showSuggestionList && suggestions}
+		<!-- component that will render the list of suggestions (e.g, `<GeocoderSuggestionList>`)-->
+		{@render children?.({
+			attribution: adapter?.attribution ? adapter.attribution() : undefined,
+			selected,
+			suggestions,
+			onSuggestionEvent
+		})}
+	{/if}
+</search>
+
+<style>
+	[type='search']::-webkit-search-cancel-button {
+		appearance: none;
+	}
+</style>
---
+++ packages/ui/src/lib/geolocation/Geolocator.svelte
@@ -0,0 +1,193 @@
+<script lang="ts">
+	/**
+	 * The `<Geolocator>` component uses the Geolocation API to identify the
+	 * users current location.
+	 *
+	 * Note that user settings and browser implementations of the Geolocation API
+	 * mean only simple usage is cross-platform. The problem mostly affects
+	 * the querying of, and listening for changes in, permissions.
+	 *
+	 * @component
+	 */
+
+	import { Button, Modal } from '@ldn-viz/ui';
+	import { XMark } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import LoadingIndicator from '../loadingIndicator/LoadingIndicator.svelte';
+	import TargetIcon from './TargetIcon.svelte';
+	import type {
+		GeolocationCoords,
+		OnGeolocationSearchError,
+		OnGeolocationSearchResult
+	} from './types';
+
+	interface Props {
+		/**
+		 * Called when a location is found.
+		 */
+		onLocationFound: OnGeolocationSearchResult | undefined;
+		/**
+		 * Called when a location could not be found.
+		 */
+		onSearchError: OnGeolocationSearchError | undefined;
+		/**
+		 * The last found location. This will be reset to `null` each time a new search
+		 * is started.
+		 */
+		location?: GeolocationCoords[2] | null;
+		/**
+		 * Bind to be reactively informed when searching is in progress.
+		 */
+		isSearching?: boolean;
+		/**
+		 * If `true`, the search button will be replaced by a clear button.
+		 */
+		allowClearButton?: boolean;
+		/**
+		 * Bind to be reactively informed when the clear button is being shown.
+		 */
+		showClearButton?: boolean;
+	}
+
+	let {
+		onLocationFound,
+		onSearchError,
+		location = $bindable(null),
+		isSearching = $bindable(false),
+		allowClearButton = false,
+		showClearButton = $bindable(false)
+	}: Props = $props();
+
+	/**
+	 * When bound to, allows activation of the search to be started by an
+	 * external source. Behaves as if the geolocator button had been clicked.
+	 */
+	export const startSearch = () => {
+		if (!isGeolocatorAvailable() || isSearching) {
+			return;
+		}
+
+		clearSearch();
+		isSearching = true;
+
+		navigator.geolocation.getCurrentPosition(
+			apiFoundLocation, //
+			apiNotFoundLocation
+		);
+	};
+
+	let errorMessage = $state('');
+	let errorModalOpen = $derived(!!errorMessage);
+
+	const isGeolocatorAvailable = () => {
+		return 'geolocation' in navigator;
+	};
+
+	const clearSearch = () => {
+		showClearButton = false;
+		location = null;
+		errorMessage = '';
+	};
+
+	const apiFoundLocation = (result: GeolocationPosition) => {
+		// https://developer.mozilla.org/en-US/docs/Web/API/GeolocationCoordinates
+		isSearching = false;
+		showClearButton = allowClearButton;
+		location = extractCoords(result);
+
+		if (location && onLocationFound) {
+			onLocationFound({ center: location });
+		}
+	};
+
+	const apiNotFoundLocation = (err: GeolocationPositionError) => {
+		// https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPositionError
+		isSearching = false;
+
+		if (err.code === err.PERMISSION_DENIED) {
+			errorMessage =
+				'Location permission denied. To use geolocation, please ensure location feature is enabled in device settings and permission granted to the browser.';
+		} else if (err.code === err.TIMEOUT) {
+			errorMessage =
+				"Location search timed out. If you're using a mobile device, this may be due to inadequate signal.";
+		} else {
+			errorMessage =
+				'Could not find location or location permission denied. To use geolocation, please ensure location feature is enabled in device settings and permission granted to the browser.';
+		}
+
+		logError(errorMessage);
+		if (onSearchError) {
+			onSearchError(err);
+		}
+	};
+
+	const logError = (...msg: string[]) => {
+		console.error(`[Geolocator]`, ...msg);
+	};
+
+	const extractCoords = (result: GeolocationPosition): null | GeolocationCoords => {
+		const lng = result?.coords?.longitude;
+		const lat = result?.coords?.latitude;
+
+		if (!isValidCoords(lng, lat)) {
+			logError(
+				'Invalid location provided by browser', //
+				JSON.stringify(result, null, 2)
+			);
+			return null;
+		}
+
+		return [lng, lat];
+	};
+
+	const isValidCoords = (lng: number, lat: number): boolean => {
+		// 0,0 is a valid coordinate (null island)
+		// But, actually, it's not because maps are limited to Greater London area!
+		// But there might be a case for no limit so better safe than sorry.
+		return (!!lng || lng === 0) && (!!lat || lat === 0);
+	};
+</script>
+
+<div class="pointer-events-auto h-10 w-10" aria-live="polite" aria-busy={isSearching}>
+	{#if isSearching}
+		<div class="h-10 w-10 p-1">
+			<LoadingIndicator title="Searching for location..." class="h-8 w-8 stroke-[12] p-0.5" />
+		</div>
+	{:else if showClearButton}
+		<Button
+			variant="square"
+			emphasis="secondary"
+			title="Clear location"
+			role="search"
+			aria-label="Clear location"
+			on:click={clearSearch}
+			class="dark:border-color-ui-border-primary dark:border"
+		>
+			<Icon src={XMark} class="p-0.25 h-8 w-8" />
+		</Button>
+	{:else}
+		<Button
+			variant="square"
+			emphasis="secondary"
+			title={errorMessage ? errorMessage : 'Find my location'}
+			role="search"
+			aria-label={errorMessage ? errorMessage : 'Find my location'}
+			on:click={startSearch}
+			class="dark:border-color-ui-border-primary dark:border"
+		>
+			<TargetIcon title={errorMessage ? errorMessage : 'Find my location'} class="h-8 w-8 p-0.5" />
+		</Button>
+	{/if}
+</div>
+
+{#if errorMessage}
+	<Modal bind:open={errorModalOpen}>
+		{#snippet title()}
+			Unable to find location.
+		{/snippet}
+
+		{#snippet description()}
+			{errorMessage}
+		{/snippet}
+	</Modal>
+{/if}
