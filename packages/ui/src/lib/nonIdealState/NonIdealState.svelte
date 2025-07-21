7962ee605941b1ca8db1a24b0383ed5899201d0e
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2025-07-21 10:49:54 +0100

    created sv5 NonIdealState and updated LoadingIndicator (#1028)
    * created sv5 NonIdealState and updated LoadingIndicator
    
    * add description to story

---
+++ packages/ui/src/lib/nonIdealState/NonIdealState.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	/**
+	 * The `<NonIdealState>` component acts as a placeholder for when other components are not ready render, or are prevented from successfully rendering by an error.
+	 * It can include an icon or spinner, and explanatory text.
+	 *
+	 * **Alternatives**: if no explanatory text is required, and a placeholder is not needed to prevent content re-flows, then consider using the [LoadingIndicator](./?path=/docs/ui-components-loadingindicator--documentation) component.
+	 * @component
+	 */
+
+	import { ExclamationTriangle } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import type { Snippet } from 'svelte';
+	import LoadingIndicator from '../loadingIndicator/LoadingIndicator.svelte';
+
+	export interface NonIdealStateProps {
+		/**
+		 * If `true`, then a spinner is displayed in place of the icon.
+		 */
+		spinner?: boolean;
+		icon?: Snippet;
+		title?: Snippet;
+		children?: Snippet;
+	}
+
+	let { spinner = false, icon, title, children }: NonIdealStateProps = $props();
+</script>
+
+<div
+	class="text-color-text-secondary bg-color-ui-background-neutral border-color-ui-border-secondary flex h-full w-full flex-col items-center justify-center gap-2 border px-2 py-2 text-center"
+>
+	{#if spinner}
+		<LoadingIndicator class="h-12 w-12" />
+	{:else}
+		<!-- contains the icon -->
+		{#if icon}
+			{@render icon()}
+		{:else}
+			<Icon src={ExclamationTriangle} theme="solid" class="h-6 w-6" aria-hidden="true" />
+		{/if}
+	{/if}
+
+	<span class="max-w-xl text-lg font-bold">
+		<!-- contains the title text, displayed below the icon -->
+		{#if title}
+			{@render title()}
+		{:else}
+			Nothing to display
+		{/if}
+	</span>
+
+	<div class="max-w-xl">
+		<!-- inserted into a `<div>` below the description -->
+		{@render children?.()}
+	</div>
+</div>
