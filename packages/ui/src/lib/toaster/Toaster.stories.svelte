eb7bd563c5e4cd5924abc22428cf43cd0ca7694a
James Scott-Brown <james.scott-brown@london.gov.uk>
2025-07-24 13:35:58 +0100

    migrate toaster

---
+++ packages/ui/src/lib/toaster/Toaster.stories.svelte
@@ -0,0 +1,158 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	import Toaster from './Toaster.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Ui/Components/Toaster ',
+		component: Toaster,
+		tags: ['autodocs'],
+
+		argTypes: {
+			position: {
+				description:
+					'Position is used to layout the Toaster. You can specify your own classes for positioning via the classes property if you want something bespoke.',
+				options: [
+					'TopLeft',
+					'TopCenter',
+					'TopRight',
+					'CenterRight',
+					'BottomRight',
+					'BottomCenter',
+					'BottomLeft',
+					'CenterLeft',
+					'Center'
+				],
+				control: { type: 'select' }
+			},
+			classes: {
+				description:
+					'classes for applying additional classes. These are appended to the class string so they have implicit but weak priority over other styles.',
+				control: { type: 'text' }
+			},
+			$$restprops: {
+				description:
+					'Any other props are applied to the top-level `<div>` that contains the toast messages',
+				table: {
+					category: 'properties'
+				}
+			}
+		}
+	});
+</script>
+
+<script lang="ts">
+	import Button from '../button/Button.svelte';
+
+	import { newToastMessage } from './toaster';
+	import { ToastType, ToasterPosition } from './types';
+
+	const toastNotice = () => {
+		newToastMessage('A notice!').post();
+	};
+
+	const toastSuccess = () => {
+		newToastMessage('A success!', { type: ToastType.Success }).post();
+	};
+
+	const toastWarning = () => {
+		newToastMessage('A warning!', { type: ToastType.Warning }).post();
+	};
+
+	const toastError = () => {
+		newToastMessage('An error!', { type: ToastType.Error }).post();
+	};
+
+	const longLivedMessage = newToastMessage(
+		'By default, messages disappear after five seconds. You can set a' +
+			' longer timeToLive and enable closeButton if you need a message to' +
+			' stick around. In the presence of a close button users will think they' +
+			' have to manually dismiss the popup so short lived toasts should not' +
+			' have a close button.',
+		{
+			id: 'a-long-lived-message',
+			timeToLive: 30000,
+			closeButton: true
+		}
+	);
+
+	const toastClosable = () => {
+		longLivedMessage.post();
+	};
+
+	let position: keyof typeof ToasterPosition = $state('TopLeft');
+	const setToaster = (event: MouseEvent) => {
+		const target = event.target as HTMLButtonElement;
+		if (target && target.textContent) {
+			position = target.textContent as keyof typeof ToasterPosition;
+		}
+	};
+</script>
+
+<Story name="Default" source>
+	{#snippet template(args)}
+		<Button onclick={toastNotice}>Notice</Button>
+
+		<Toaster {...args} />
+	{/snippet}
+</Story>
+
+<Story name="Types">
+	{#snippet template()}
+		<Toaster position="Center" />
+		<div class="flex gap-6">
+			<Button onclick={toastNotice}>Notice</Button>
+			<Button emphasis="positive" onclick={toastSuccess}>Success</Button>
+			<Button emphasis="caution" onclick={toastWarning}>Warning</Button>
+			<Button emphasis="negative" onclick={toastError}>Error</Button>
+		</div>
+	{/snippet}
+</Story>
+
+<Story name="Position">
+	{#snippet template()}
+		<div class="relative h-[100vh] w-[100vw] p-6">
+			<Toaster {position} />
+			<div
+				class="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-wrap justify-center gap-6 [&>*]:basis-3/12"
+			>
+				<Button onclick={setToaster} variant={position === 'TopLeft' ? 'solid' : 'outline'}>
+					TopLeft
+				</Button>
+				<Button onclick={setToaster} variant={position === 'TopCenter' ? 'solid' : 'outline'}>
+					TopCenter
+				</Button>
+				<Button onclick={setToaster} variant={position === 'TopRight' ? 'solid' : 'outline'}>
+					TopRight
+				</Button>
+				<Button onclick={setToaster} variant={position === 'CenterLeft' ? 'solid' : 'outline'}>
+					CenterLeft
+				</Button>
+				<Button condition="warning" onclick={toastNotice}>Add Toast!</Button>
+				<Button onclick={setToaster} variant={position === 'CenterRight' ? 'solid' : 'outline'}>
+					CenterRight
+				</Button>
+				<Button onclick={setToaster} variant={position === 'BottomLeft' ? 'solid' : 'outline'}>
+					BottomLeft
+				</Button>
+				<Button onclick={setToaster} variant={position === 'BottomCenter' ? 'solid' : 'outline'}>
+					BottomCenter
+				</Button>
+				<Button onclick={setToaster} variant={position === 'BottomRight' ? 'solid' : 'outline'}>
+					BottomRight
+				</Button>
+			</div>
+		</div>
+	{/snippet}
+</Story>
+
+<Story name="Close Button">
+	{#snippet template()}
+		<div class="h-[100vh] w-[100vw] p-6">
+			<Toaster position="Center" />
+			<div class="flex gap-6">
+				<Button onclick={toastClosable}>Toast!</Button>
+			</div>
+		</div>
+	{/snippet}
+</Story>
---
+++ packages/ui/src/lib/toaster/Toaster.svelte
@@ -0,0 +1,81 @@
+<script lang="ts" module>
+	type ToasterPositionClass = {
+		[key: string]: string;
+	};
+
+	const positionClasses: ToasterPositionClass = {
+		TopLeft: 'top-6 left-6',
+		TopCenter: 'top-6 left-1/2 -translate-x-1/2 transform',
+		TopRight: 'top-6 right-6',
+		CenterRight: 'top-1/2 -translate-y-1/2 right-6 transform',
+		BottomRight: 'bottom-6 right-6',
+		BottomCenter: 'bottom-6 left-1/2 -translate-x-1/2 transform',
+		BottomLeft: 'bottom-6 left-6',
+		CenterLeft: 'top-1/2 -translate-y-1/2 left-6 transform',
+		Center: 'top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 transform'
+	};
+</script>
+
+<script lang="ts">
+	/**
+	 * The `<Toaster>` component acts as a container for short messages that appear temporarily ("toasts").
+	 * It can be included in the `+layout.svelte` file to avoid needing to add it to each route separately.
+	 *
+	 * On a page that includes a `<Toaster>` component, you can create a toast using the `newToastMessage()` function,
+	 * then display it by calling `.post()` on the object it returns; calling `.post()` repeatedly will refresh the toast.
+	 * You can remove a toast by calling `.remove()`.
+	 *
+	 * ```js
+	 *  // there should be an at-sign in the package name, but JSDoc chokes on it
+	 * import { newToastMessage, ToastType, ToastMessageOptions } from 'ldn-viz/ui';
+	 *
+	 * const staticToast = newToastMessage('This is a warning!', {
+	 *	// Type: ToastMessageOptions
+	 *	// An id is rarely needed but prevents HMR duplicates.
+	 *	id: 'a-warning-toast',
+	 *	type: ToastType.Warning,
+	 *	closeButton: true,
+	 * timeToLive: 10 * 1000, // in ms, so this is 10 seconds
+	 *});
+	 *
+	 * // calling .post() repeatedly on same toast object will refresh it, rather than creating duplicate toasts
+	 * staticToast.post();
+	 * staticToast.post();
+	 *
+	 *```
+	 *
+	 * @component
+	 */
+
+	import Toast from './Toast.svelte';
+	import { messages } from './toaster';
+	import type { ToasterPosition } from './types';
+
+	// position of the `Toaster`. You can specify your own classes
+
+	// classes for applying additional classes. These are appended to the class
+
+	interface Props {
+		// for positioning via the classes property if you want something bespoke.
+		position?: keyof typeof ToasterPosition;
+		// string so they have implicit but weak priority over other styles.
+		classes?: string;
+		[key: string]: any;
+	}
+
+	let { position = 'TopCenter', classes = '', ...rest }: Props = $props();
+
+	// ...$$restProps applied to the top-level `<div>` that contains the toast messages.
+
+	let posClasses = $derived(positionClasses[position] || '');
+</script>
+
+<div
+	style:display={$messages.length === 0 ? 'none' : 'block'}
+	class="fixed z-[50] flex w-fit flex-col space-y-1 {posClasses} {classes}"
+	{...rest}
+>
+	{#each $messages as message (message.id)}
+		<Toast {message} />
+	{/each}
+</div>
