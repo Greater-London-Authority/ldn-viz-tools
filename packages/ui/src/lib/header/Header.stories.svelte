65b3cb38eb7acd122a39a80fe4147c3be1db25bc
Chris Knight <111759008+ChrisKnightLDN@users.noreply.github.com>
2025-07-24 15:33:00 +0100

    Header component (#1052)

---
+++ packages/ui/src/lib/header/Header.stories.svelte
@@ -0,0 +1,81 @@
+<script context="module" lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { subMenu } from '../navigationMenu/NavigationMenu.stories.svelte';
+	import NavigationMenu from '../navigationMenu/NavigationMenu.svelte';
+	import Header from './Header.svelte';
+	import HeaderItem from './HeaderItem.svelte';
+	import HeaderRight from './HeaderRight.svelte';
+	import HeaderTitle from './HeaderTitle.svelte';
+
+	let { Story } = defineMeta({
+		title: 'Ui/Components - Layout And Themes/Header',
+		component: Header as any,
+		subcomponents: { HeaderItem, HeaderRight, HeaderTitle, NavigationMenu },
+		parameters: {
+			layout: 'fullscreen'
+		}
+	});
+</script>
+
+<Story name="Default">
+	{#snippet template(args)}
+		<Header></Header>
+	{/snippet}
+</Story>
+
+<Story name="Title only">
+	{#snippet template()}
+		<Header>
+			<HeaderTitle>App title</HeaderTitle>
+		</Header>
+	{/snippet}
+</Story>
+
+<Story name="Title and menu-right">
+	{#snippet template()}
+		<Header>
+			<HeaderTitle>App title</HeaderTitle>
+			<HeaderRight>
+				<HeaderItem>
+					<NavigationMenu ariaLabel="Main menu" items={subMenu} orientation="horizontal" />
+				</HeaderItem>
+			</HeaderRight>
+		</Header>
+	{/snippet}
+</Story>
+
+<Story name="Title, links, and auth section">
+	{#snippet template()}
+		<Header>
+			<HeaderTitle>App title</HeaderTitle>
+
+			<HeaderItem>
+				<NavigationMenu ariaLabel="Main menu" items={subMenu} orientation="horizontal" />
+			</HeaderItem>
+
+			<HeaderRight>
+				<!-- This would come from your auth menu Component in your app -->
+				<HeaderItem>
+					<span class="text-xs">Logged in as <b>Bob Smith</b></span>
+				</HeaderItem>
+
+				<HeaderItem>
+					<a class="text-xs" href="/#">Log Out</a>
+				</HeaderItem>
+			</HeaderRight>
+		</Header>
+	{/snippet}
+</Story>
+
+<Story name="Light Theme">
+	{#snippet template()}
+		<Header theme="light">
+			<HeaderTitle>App title</HeaderTitle>
+			<HeaderRight>
+				<HeaderItem>
+					<NavigationMenu ariaLabel="Main menu" items={subMenu} orientation="horizontal" />
+				</HeaderItem>
+			</HeaderRight>
+		</Header>
+	{/snippet}
+</Story>
---
+++ packages/ui/src/lib/header/HeaderTitle.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	/**
+	 * Typically imported from `$app/paths` then passed as prop.
+	 * Used to construct link to homepage.
+	 */
+
+	interface Props {
+		base?: string;
+		children?: import('svelte').Snippet;
+	}
+
+	let { base = '', children }: Props = $props();
+</script>
+
+<a href={base || '/'}>
+	<span class="mr-4 text-lg font-semibold">
+		<!-- The app title, possibly including an icon. -->
+		{@render children?.()}
+	</span>
+</a>
