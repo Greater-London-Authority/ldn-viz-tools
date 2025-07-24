eccdad87b312447f6d305bf44d7a30fc551f29a6
Chris Knight <chris.knight@london.gov.uk>
2025-07-24 15:26:05 +0100

    Header component

---
+++ packages/ui/src/lib/header/HeaderRight.svelte
@@ -0,0 +1,9 @@
+<script>
+	/** A container for content to be positioned at the right-hand side of a `<Header>`.*/
+	let { children } = $props();
+</script>
+
+<div class="flex flex-grow items-center justify-end">
+	<!-- contents -->
+	{@render children?.()}
+</div>
