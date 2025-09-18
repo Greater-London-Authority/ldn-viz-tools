32a27b652651cb92b41d5624ec2c39cbc923106c
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2025-09-18 12:32:42 +0100

    create sv5 MapLegend

---
+++ packages/maps/src/lib/mapLegend/MapLegend.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { Button } from '@ldn-viz/ui';
+	import { XMark } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import type { Snippet } from 'svelte';
+
+	/**
+	 * The `<MapLegend>` component provides a container inside which a `Legend` can be inserted for display on maps.
+	 */
+
+	interface Props {
+		/**
+		 * Boolean to display or hide legend
+		 */
+		isOpen?: boolean;
+		/**
+		 * Value for width to pass into `ColorLegend` component
+		 */
+		width: number;
+		children?: Snippet;
+	}
+
+	let { isOpen = $bindable(true), width = $bindable(), children }: Props = $props();
+
+	const handleClick = () => (isOpen = !isOpen);
+</script>
+
+{#if isOpen}
+	<div
+		class="bg-color-container-level-0 pointer-events-auto flex w-full flex-col gap-3 px-4 py-3 sm:max-w-96"
+		bind:clientWidth={width}
+	>
+		<div class="flex justify-between align-middle">
+			<p class="text-color-text-primary font-bold">Legend</p>
+			<Button
+				variant="text"
+				size="sm"
+				class="bg-color-container-level-0 text-color-text-primary absolute right-1 top-1 cursor-pointer"
+				onclick={handleClick}
+				aria-label="Close legend"
+			>
+				<Icon src={XMark} theme="mini" class="h-5 w-5" />
+			</Button>
+		</div>
+		<!-- Pass in `ColorLegend` or other components in the default slot -->
+		{@render children?.()}
+	</div>
+{:else}
+	<Button
+		size="md"
+		emphasis="secondary"
+		class="pointer-events-auto h-8"
+		onclick={handleClick}
+		aria-label="Show legend"
+	>
+		Legend
+	</Button>
+{/if}
