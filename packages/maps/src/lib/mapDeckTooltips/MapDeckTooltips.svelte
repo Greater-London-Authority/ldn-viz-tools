2e5d5d06df3ed1d0e13e591ccfd1aeae067da44a
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2025-10-06 09:03:40 +0100

    sv5: MapDeckTooltips z-index (#1141)

--- packages/maps/src/lib/mapDeckTooltips/MapDeckTooltips.svelte
+++ packages/maps/src/lib/mapDeckTooltips/MapDeckTooltips.svelte
@@ -106,8 +106,7 @@
 	<div
 		use:floatingContent={dynamicOptions}
 		class:width={'100px'}
-		style:z-index={9999}
-		class="pointer-events-none"
+		class="maplibregl-popup pointer-events-none"
 	>
 		<MapMarkerStyledContainer>
 			{#if typeof tooltipSpec === 'string'}
