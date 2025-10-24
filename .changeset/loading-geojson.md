a393d53bd8688387ba129cd0dd18da43f0879396
James Scott-Brown <james.scott-brown@london.gov.uk>
2025-10-24 07:47:15 +0100

    lint

--- .changeset/loading-geojson.md
+++ .changeset/loading-geojson.md
@@ -2,5 +2,5 @@
 '@ldn-viz/maps': patch
 ---
 
-CHANGED: be more more permissive in loading GeoJSON that wasn't created with TerraDRaw or the MapDrawing component, by defaulting tool to be polygon and replacing feature ids with a UUID4 (if they aren't already)
+CHANGED: be more permissive in loading GeoJSON that wasn't created with TerraDRaw or the MapDrawing component, by defaulting tool to be polygon and replacing feature ids with a UUID4 (if they aren't already)
 
--- packages/maps/src/lib/mapDraw/FileUpload.svelte
+++ packages/maps/src/lib/mapDraw/FileUpload.svelte
@@ -3,7 +3,7 @@
 	import type { FeatureCollection } from 'geojson';
 	import type { ChangeEventHandler } from 'svelte/elements';
 
-import uuid4 from "uuid4";
+	import uuid4 from 'uuid4';
 	export let onCancel: () => void;
 	export let onLoad: (data: FeatureCollection) => void;
 
@@ -20,14 +20,14 @@ import uuid4 from "uuid4";
 			// modify features to please TerraDrw
 			// 9see e.g. https://github.com/JamesLMilner/terra-draw/issues/177
 			for (const feature of parsedJson.features) {
-				if (!uuid4.valid(feature.id)){
+				if (!uuid4.valid(feature.id)) {
 					feature.id = uuid4();
 				}
 
-				if (!feature.properties){
+				if (!feature.properties) {
 					feature.properties = {};
 				}
-				if (!feature.properties.mode){
+				if (!feature.properties.mode) {
 					feature.properties.mode = 'polygon';
 				}
 			}
