ccb4e8ff669d81362a45c539f38e304128c69822
Chris Knight <chris.knight@london.gov.uk>
2025-11-06 11:23:30 +0000

    copy over mapdraw sv4

---
+++ packages/maps/src/lib/mapDraw/FileUpload.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+	import { Button, classNames } from '@ldn-viz/ui';
+	import type { FeatureCollection } from 'geojson';
+	import type { ChangeEventHandler } from 'svelte/elements';
+
+	import uuid4 from 'uuid4';
+	export let onCancel: () => void;
+	export let onLoad: (data: FeatureCollection) => void;
+
+	export let features;
+	export let savedFeatures;
+
+	let isDragging = false;
+
+	async function readFile(file) {
+		try {
+			const text = await file.text();
+			const parsedJson = JSON.parse(text);
+
+			// modify features to please TerraDrw
+			// see e.g. https://github.com/JamesLMilner/terra-draw/issues/177
+			for (const feature of parsedJson.features) {
+				if (!uuid4.valid(feature.id)) {
+					feature.id = uuid4();
+				}
+
+				if (!feature.properties) {
+					feature.properties = {};
+				}
+				if (!feature.properties.mode) {
+					feature.properties.mode = 'polygon';
+				}
+			}
+
+			features = parsedJson.features;
+			savedFeatures = parsedJson.features;
+
+			onLoad(parsedJson);
+		} catch (error) {
+			console.error('Error parsing JSON:', error);
+		}
+	}
+
+	function handleDrag(e: DragEvent) {
+		e.preventDefault();
+		e.stopPropagation();
+	}
+
+	function handleDragIn(e: DragEvent) {
+		e.preventDefault();
+		e.stopPropagation();
+		if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+			isDragging = true;
+		}
+	}
+
+	function handleDragOut(e: DragEvent) {
+		e.preventDefault();
+		e.stopPropagation();
+		isDragging = false;
+	}
+
+	function handleDrop(e: DragEvent) {
+		e.preventDefault();
+		e.stopPropagation();
+		isDragging = false;
+
+		const items = [...e.dataTransfer.items];
+		const file = items.find((item) => item.kind === 'file')?.getAsFile();
+
+		if (file && file.name.endsWith('.geojson')) {
+			readFile(file);
+		}
+	}
+
+	function handleFileSelect(e: ChangeEventHandler<HTMLInputElement>) {
+		const file = e.target.files[0];
+		if (file && file.name.endsWith('.geojson')) {
+			readFile(file);
+		}
+	}
+
+	let inputRef: HTMLInputElement;
+</script>
+
+<div class="bg-color-container-level-1 pointer-events-auto flex flex-col gap-2 p-4">
+	<span class="pb-2 text-lg font-bold">Upload GeoJSON</span>
+
+	<div
+		class={classNames(
+			'border-color-input-border hover:border-color-input-border-focussed relative min-h-[200px] rounded-lg border-2 border-dashed p-2',
+			isDragging ? '!bg-color-action-background-primary-muted-hover' : ''
+		)}
+		on:dragenter={handleDragIn}
+		on:dragleave={handleDragOut}
+		on:dragover={handleDrag}
+		on:drop={handleDrop}
+		on:keydown={(e) => {
+			if (e.key === 'Enter' || e.key === ' ') {
+				inputRef.click();
+			}
+		}}
+		role="button"
+		tabindex="0"
+		aria-label="GeoJSON file drop zone"
+		aria-describedby="dropZoneDescription"
+	>
+		<span id="dropZoneDescription" class="sr-only">
+			Drop a GeoJSON file here or press Enter to select a file
+		</span>
+
+		<div class="absolute inset-0 z-10 h-full w-full cursor-pointer">
+			<!-- TODO: WHY NOT OUR INPUT HERE -->
+			<input
+				bind:this={inputRef}
+				type="file"
+				accept=".geojson"
+				on:change={handleFileSelect}
+				class="absolute inset-0 z-10 h-full w-full cursor-pointer opacity-0"
+			/>
+		</div>
+
+		<div class="relative z-0 text-center">
+			<p class="text-color-text-secondary mb-4">
+				Drag & drop a GeoJSON file here or click to open file picker.
+			</p>
+		</div>
+	</div>
+
+	<Button onclick={onCancel} emphasis="secondary" class="mt-2">Cancel</Button>
+</div>
---
+++ packages/maps/src/lib/mapDraw/MapDraw.svelte
@@ -0,0 +1,248 @@
+<script lang="ts">
+	import { getContext, onDestroy } from 'svelte';
+
+	import {
+		TerraDraw,
+		TerraDrawCircleMode,
+		TerraDrawFreehandMode,
+		TerraDrawLineStringMode,
+		TerraDrawPointMode,
+		TerraDrawPolygonMode,
+		TerraDrawRectangleMode,
+		TerraDrawRenderMode,
+		TerraDrawSectorMode,
+		TerraDrawSelectMode
+	} from 'terra-draw';
+	//import type { HexColorStyling } from 'terra-draw';
+
+	import { TerraDrawMapLibreGLAdapter } from 'terra-draw-maplibre-gl-adapter';
+	import type { TerraDrawBaseAdapter } from 'terra-draw/dist/common/base.adapter';
+
+	import type { Feature } from 'geojson';
+	import { currentTheme, tokenNameToValue } from '@ldn-viz/ui';
+	import type { MapLibreStore } from '../map/types';
+	import MapDrawControls from './MapDrawControls.svelte';
+
+	const mapStore: MapLibreStore = getContext('mapStore');
+
+	/**
+	The modes/tools available for selection.
+	 **/
+	export let enabledModes = ['polygon'];
+
+	/**
+	 * The currently active mode.
+	 */
+	export let currentMode: string;
+
+	/**
+	 * GeoJSON features that have been drawn (continuously updates).
+	 */
+	export let features: Feature[]; // can't control externally yet
+
+	/**
+	 * GeoJSON features that have been drawn (updates on Save or Clear).
+	 */
+	export let savedFeatures: Feature[];
+
+	/**
+	 * Function to be called when user clicks 'Done' button.
+	 */
+	export let onDone = (_features: Feature[]) => null;
+
+	const lightThemeStyle = {
+		fillColor: tokenNameToValue('ui.primary', $currentTheme), // as HexColorStyling,
+		fillOpacity: 0.5,
+		outlineColor: tokenNameToValue('ui.primary', $currentTheme),
+		outlineWidth: 1
+	};
+
+	const modeMapping = {
+		circle: new TerraDrawCircleMode({
+			styles: lightThemeStyle
+		}),
+		freehand: new TerraDrawFreehandMode({
+			styles: lightThemeStyle
+		}),
+		linestring: new TerraDrawLineStringMode({
+			styles: lightThemeStyle
+		}),
+		point: new TerraDrawPointMode({
+			styles: lightThemeStyle
+		}),
+		polygon: new TerraDrawPolygonMode({
+			styles: lightThemeStyle
+		}),
+		rectangle: new TerraDrawRectangleMode({
+			styles: lightThemeStyle
+		}),
+		sector: new TerraDrawSectorMode({
+			styles: lightThemeStyle
+		}),
+
+		select: new TerraDrawSelectMode()
+	};
+
+	const selectMode = new TerraDrawSelectMode({
+		// Allow manual deselection of features
+		allowManualDeselection: true, // allows users to deselect by clicking on the map
+
+		// we override the default key bindings so that features are deleted with Backspace rather than Delete
+		keyEvents: {
+			deselect: 'Escape',
+			delete: 'Backspace',
+			rotate: ['Control', 'r'],
+			scale: ['Control', 's']
+		},
+
+		// Enable editing tools by Feature
+		flags: {
+			point: {
+				feature: {
+					draggable: true
+				}
+			},
+
+			polygon: {
+				feature: {
+					draggable: true,
+					coordinates: {
+						midpoints: true,
+						draggable: true,
+						deletable: true
+					}
+				}
+			},
+
+			linestring: {
+				feature: {
+					draggable: true,
+					coordinates: {
+						midpoints: true,
+						draggable: true,
+						deletable: true
+					}
+				}
+			},
+
+			freehand: {
+				feature: {
+					draggable: true,
+					coordinates: {
+						midpoints: true,
+						draggable: true,
+						deletable: true
+					}
+				}
+			},
+
+			circle: {
+				feature: {
+					draggable: true,
+					coordinates: {
+						midpoints: true,
+						draggable: true,
+						deletable: true
+					}
+				}
+			},
+
+			rectangle: {
+				feature: {
+					draggable: true,
+					coordinates: {
+						midpoints: true,
+						draggable: true,
+						deletable: true
+					}
+				}
+			},
+
+			sector: {
+				feature: {
+					draggable: true,
+					coordinates: {
+						midpoints: true,
+						draggable: true,
+						deletable: true
+					}
+				}
+			}
+		}
+	});
+
+	const renderMode = new TerraDrawRenderMode({
+		modeName: 'render',
+		styles: lightThemeStyle
+	});
+
+	let draw: TerraDraw;
+	let adapter: TerraDrawBaseAdapter;
+
+	const createTerraDraw = () => {
+		if ($mapStore) {
+			const modes = [
+				...enabledModes.map((modeName) => modeMapping[modeName]),
+				selectMode,
+				renderMode
+			];
+
+			adapter = new TerraDrawMapLibreGLAdapter({ map: $mapStore });
+
+			draw = new TerraDraw({
+				adapter,
+				modes
+			});
+
+			draw.start();
+
+			if (savedFeatures.length > 0) {
+				draw.addFeatures(savedFeatures);
+			}
+
+			// once a user has finished creating a shape, reset to select tool
+			draw.on('finish', (id: string, context: { action: string; mode: string }) => {
+				if (context.action === 'draw') {
+					currentMode = 'select';
+				}
+			});
+
+			// continuously update features
+			draw.on('change', () => {
+				features = draw.getSnapshot();
+			});
+		}
+	};
+	$: createTerraDraw($mapStore, enabledModes);
+
+	const setMode = (newMode: string) => {
+		if (!draw) return;
+
+		if (newMode) {
+			draw.setMode(newMode);
+		} else {
+			draw.setMode('select');
+		}
+	};
+
+	$: setMode(currentMode);
+
+	onDestroy(() => {
+		// if we don't tidy up, then re-creating MapDraw component will fail as its map layers will already exist
+		if (draw) {
+			draw.clear();
+		}
+		if (adapter) {
+			adapter.unregister();
+		}
+	});
+</script>
+
+<MapDrawControls
+	{enabledModes}
+	{features}
+	bind:savedFeatures
+	bind:currentMode
+	{onDone}
+	terraDraw={draw}
+/>
