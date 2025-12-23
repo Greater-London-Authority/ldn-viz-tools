d4701522841c244dbf6ea6b7169cdbb7046a6314
James Scott-Brown <james.scott-brown@london.gov.uk>
2025-12-23 10:22:34 +0000

    add stroy showing how to use preprocessOptions

--- packages/charts/src/lib/observablePlot/ObservablePlotInner.stories.svelte
+++ packages/charts/src/lib/observablePlot/ObservablePlotInner.stories.svelte
@@ -29,6 +29,7 @@
 	import * as UnstyledPlot from '@observablehq/plot';
 	import { format } from 'd3-format';
 	import { monthlyData } from '../../data/demoData';
+	import { preprocessOptions } from '../observablePlotFragments/observablePlotFragments';
 	import { Plot } from '../observablePlotFragments/plot';
 
 	const chartData = monthlyData.filter((d) => d.Variable === 'Variable A');
@@ -147,6 +148,102 @@
 			})
 		]
 	});
+	const annotations = [
+		{
+			date: new Date('2015-02-01'),
+			value: 61816,
+			label: 'Peak',
+			dx: 0,
+			dy: -20,
+			textAnchor: 'middle'
+		},
+		{
+			date: new Date('2016-12-01'),
+			value: 53560,
+			label: 'Decline begins',
+			dx: 10,
+			dy: 0,
+			textAnchor: 'start'
+		},
+		{
+			date: new Date('2019-11-01'),
+			value: 41090,
+			label: 'Sharp drop',
+			dx: -10,
+			dy: 0,
+			textAnchor: 'end'
+		},
+		{
+			date: new Date('2022-07-01'),
+			value: 15020,
+			label: 'Near bottom',
+			dx: 10,
+			dy: 10,
+			textAnchor: 'start'
+		},
+		{
+			date: new Date('2023-07-01'),
+			value: 11667,
+			label: 'Lowest point',
+			dx: 0,
+			dy: 20,
+			textAnchor: 'middle'
+		}
+	];
+
+	const specPreprocessed = $derived({
+		x: { insetLeft: 80, insetRight: 20, type: 'utc' },
+		y: { insetTop: 30, insetBottom: 20 },
+		marks: [
+			Plot.gridX({ interval: '2 years' }),
+			Plot.gridY(),
+			Plot.axisX({ label: 'Year', interval: '1 year' }),
+			Plot.axisY({ label: '', tickFormat: (d) => '£' + format(',.4~s')(d) }),
+			Plot.ruleY([0]),
+
+			Plot.line(chartData, {
+				x: 'Month',
+				y: 'Value',
+				tip: {
+					format: {
+						x: true,
+						y: (d) => '£' + format(',.4~s')(d)
+					}
+				}
+			}),
+
+			Plot.dot(annotations, {
+				x: 'date',
+				y: 'value',
+				r: 4,
+				fill: 'currentColor'
+			}),
+
+			preprocessOptions(
+				// the data
+				annotations,
+
+				// the mark options that don't need to be modified
+				{
+					type: Plot.annotationText,
+					options: {
+						x: 'date',
+						y: 'value',
+						text: 'label',
+						fontSize: 11,
+						fontWeight: 'bold'
+					},
+
+					// the mark options that we want to treat as channels, despite them not being channels
+					optionsToEval: {
+						dx: (d) => d.dx,
+						dy: (d) => d.dy,
+						textAnchor: (d) => d.textAnchor
+					}
+				}
+			)
+		]
+	});
 </script>
 
 <Story name="Default">
@@ -215,5 +312,17 @@ In this plot, the `dot` mark is used on the left and the `point` mark on the rig
 <Story name="point mark">
 	{#snippet template(args)}
 		<ObservablePlotInner {...args} spec={specPointsAndDots} data={chartData} />
+	{/snippet}
+</Story>
+
+<!--
+In Observable Plot, not everything is a channel: in other words, some things cannot take different values for different instances of the mark.
+For example, different text marks created with a single `Plot.text()` call must have the same vlaue of `dx` and `dy`.
+This is inconvenient, as we often want to adjust different text annotations by different amounts to avoid overlapping with each other or the data marks.
+We therefore provide a `preprocessOptions` function, which splits one mark into multiple marks as necessary.
+-->
+<Story name="Preprocessing to split into multple marks">
+	{#snippet template(args)}
+		<ObservablePlotInner {...args} spec={specPreprocessed} data={chartData} />
 	{/snippet}
 </Story>
