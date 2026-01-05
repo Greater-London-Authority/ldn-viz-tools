1cff0ffb177cfb74c891716eee88393030816793
James Scott-Brown <james.scott-brown@london.gov.uk>
2026-01-05 13:25:31 +0000

    make it possible to provide custom pattern definitions

--- packages/ui/src/lib/layerControl/FillTypeControl.svelte
+++ packages/ui/src/lib/layerControl/FillTypeControl.svelte
@@ -3,6 +3,8 @@
 	import Popover from '../popover/Popover.svelte';
 	import RadioButtonGroup from '../radioButton/RadioButtonGroup.svelte';
 
+	import { patterns as defaultPatterns, type PatternDefinitions } from './patterns';
+
 	import { AdjustmentsHorizontal, NoSymbol } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 
@@ -11,56 +13,21 @@
 		{ id: 'no-fill', label: 'Outline only' }
 	];
 
-	const patterns = {
-		patternURL:
-			'https://raw.githubusercontent.com/visgl/deck.gl/master/examples/layer-browser/data/pattern.png',
-
-		// N.B. this can't be determined from the pattern atlas alone, as there may be blank/unused pixels
-		// to the right or bottom of the pattern swatches. Ignoring these would cause incorrect scaling, and mis-alignment.
-		patternImageWidth: 256,
-		patternImageHeight: 256,
-
-		atlas: {
-			'hatch-1x': {
-				x: 4,
-				y: 4,
-				width: 120,
-				height: 120,
-				mask: true
-			},
-			'hatch-2x': {
-				x: 132,
-				y: 4,
-				width: 120,
-				height: 120,
-				mask: true
-			},
-			'hatch-cross': {
-				x: 4,
-				y: 132,
-				width: 120,
-				height: 120,
-				mask: true
-			},
-			dots: {
-				x: 132,
-				y: 132,
-				width: 120,
-				height: 120,
-				mask: true
-			}
-		}
-	};
-
 	const swatchWidth = 30;
 
 	interface Props {
 		label: any;
 		disabled?: boolean;
 		fillType?: string;
+		patterns?: PatternDefinitions;
 	}
 
-	let { label, disabled = false, fillType = $bindable() }: Props = $props();
+	let {
+		label,
+		disabled = false,
+		fillType = $bindable(),
+		patterns = defaultPatterns
+	}: Props = $props();
 
 	const clickPattern = (name: string) => {
 		fillType = name;
