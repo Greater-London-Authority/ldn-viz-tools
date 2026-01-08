64a8c4802b0ce90be68c8d64a6218c14abe6648e
Chris Knight <chris.knight@london.gov.uk>
2026-01-08 10:49:12 +0000

    run format

--- packages/charts/src/lib/observablePlot/ObservablePlotInner.svelte
+++ packages/charts/src/lib/observablePlot/ObservablePlotInner.svelte
@@ -263,7 +263,7 @@
 	<!-- IMPORTANT TODO: data prop and exportData prop for buttons - align usage-->
 	{#if $tooltipStore && $tooltipData}
 		<div
-			class="bg-color-container-level-0 absolute z-50 max-w-[200px] -translate-x-1/2 -translate-y-full p-2 text-sm shadow"
+			class="absolute z-50 max-w-[200px] -translate-x-1/2 -translate-y-full bg-color-container-level-0 p-2 text-sm shadow"
 			style:top={`${$tooltipStore.layerY + tooltipOffset}px`}
 			style:left={`${$tooltipStore.layerX}px`}
 		>
@@ -272,7 +272,7 @@
 			{/if}
 
 			<div
-				class="bg-color-container-level-0 absolute inset-x-1/2 h-4 w-4 -translate-x-1/2 rotate-45"
+				class="absolute inset-x-1/2 h-4 w-4 -translate-x-1/2 rotate-45 bg-color-container-level-0"
 			></div>
 		</div>
 	{/if}
