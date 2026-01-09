6fdbc2b9f192177a08df6b33517ec4ea669391f7
Chris Knight <chris.knight@london.gov.uk>
2026-01-09 14:37:13 +0000

    update nav item util function
    Co-authored-by: James Scott-Brown <jamesscottbrown@users.noreply.github.com>

--- packages/ui/src/lib/navigationMenu/utils.ts
+++ packages/ui/src/lib/navigationMenu/utils.ts
@@ -14,14 +14,29 @@ import type { NavigationMenuItemProps } from './types';
 	/>
 */
 
+const getItems = (item: NavigationMenuItemProps): NavigationMenuItemProps[] => {
+	return [item, ...(item.children || []).flatMap((it) => getItems(it))];
+};
+
 export const findActiveItem = (
 	items: NavigationMenuItemProps[],
 	page: any,
 	resolve: (url: string) => string
 ) => {
-	for (const item of items) {
-		if (page.url?.pathname.endsWith(resolve(item.href ?? '/'))) {
-			return item;
+	let bestMatch: NavigationMenuItemProps | undefined = undefined;
+
+	const flattenedItemList = (items || [])?.flatMap((it) => getItems(it));
+
+	for (const item of flattenedItemList) {
+		if (page.url?.pathname.startsWith(resolve(item.href ?? '/'))) {
+			const itemPath = item?.href ?? '';
+			const bestMatchPath = bestMatch?.href ?? '';
+
+			if (itemPath.split('/').length > bestMatchPath.split('/').length) {
+				bestMatch = item;
+			}
 		}
 	}
+
+	return bestMatch;
 };
