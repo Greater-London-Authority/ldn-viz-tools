c23d8aabd690af59d1ee3992f61a07a46e454050
Chris Knight <chris.knight@london.gov.uk>
2026-01-09 16:57:41 +0000

    stub footer

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -47,6 +47,33 @@
 	{@render children()}
 
 	<Footer showCiuLogo>
+		<div class="pb-4 text-sm lg:w-1/6">
+			<nav>
+				<h3 class="mb-1 font-semibold">Links</h3>
+				<ul class=" text-color-text-secondary">
+					<li>
+						<a href="https://data.london.gov.uk/" target="_blank" class="hover:underline">
+							London Data Store
+						</a>
+					</li>
+					<li>
+						<a href="https://dfl.london.gov.uk/library" target="_blank" class="hover:underline">
+							Data for London Libraray
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+
+		<div class="pb-4 text-sm lg:w-1/6">
+			<nav>
+				<h3 class="mb-1 font-semibold">Contact</h3>
+				<ul class="text-color-text-secondary">
+					<li>email:</li>
+				</ul>
+			</nav>
+		</div>
+
 		{#snippet footerCookieMenu()}
 			<div class="flex justify-end">
 				<ThemeSwitcher size="xs" />
