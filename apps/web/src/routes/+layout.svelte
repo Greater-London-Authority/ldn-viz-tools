68c04dfea8ed7b2e47f1991536225485328fb892
Chris Knight <chris.knight@london.gov.uk>
2026-01-12 10:47:05 +0000

    add more responsive function to menu in header

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -2,9 +2,11 @@
 	import { resolve } from '$app/paths';
 	import { page } from '$app/state';
 
+	import { afterNavigate } from '$app/navigation';
 	import { findActiveItem } from '@ldn-viz/ui';
 
 	import {
+		Button,
 		Footer,
 		Header,
 		HeaderItem,
@@ -14,34 +16,63 @@
 		Theme,
 		ThemeSwitcher
 	} from '@ldn-viz/ui';
+	import { Bars3, XMark } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { slide } from 'svelte/transition';
 	import '../app.postcss';
 
-	//TMP
 	let mainMenu = [
 		{ title: 'Design System', id: 'design-system-docs', href: '/design-system' },
 		{ title: 'Contact', id: 'contact', href: '/contact' }
 	];
 
+	let menuOpen = $state(false);
+	let breakPoint: number = $state(0);
+
 	let { children } = $props();
 
 	let activeMenuItem = $derived(findActiveItem(mainMenu, page, resolve));
+
+	afterNavigate(() => (menuOpen = false));
 </script>
 
 <Theme />
 
+<svelte:window bind:innerWidth={breakPoint} />
+
 <div class="flex min-h-dvh flex-col">
 	<Header>
-		<HeaderTitle>London City Data</HeaderTitle>
+		<HeaderTitle base={resolve('/', {})}>London City Data</HeaderTitle>
 		<HeaderRight>
-			<HeaderItem>
-				<NavigationMenu
-					items={mainMenu}
-					orientation="horizontal"
-					ariaLabel="main menu"
-					activeMenuItemId={activeMenuItem?.id}
-				/>
-			</HeaderItem>
+			{#if breakPoint >= 768}
+				<HeaderItem>
+					<NavigationMenu
+						items={mainMenu}
+						orientation="horizontal"
+						ariaLabel="main menu"
+						activeMenuItemId={activeMenuItem?.id}
+					/>
+				</HeaderItem>
+			{:else}
+				<Button
+					variant="text"
+					class=" !text-color-static-white no-underline"
+					onclick={() => (menuOpen = !menuOpen)}
+				>
+					Menu {#if !menuOpen}
+						<Icon src={Bars3} class="ml-2 h-5 w-5"></Icon>
+					{:else}
+						<Icon src={XMark} class="ml-2 h-5 w-5"></Icon>
+					{/if}
+				</Button>
+			{/if}
 		</HeaderRight>
+
+		{#if breakPoint <= 768 && menuOpen}
+			<div class="absolute inset-x-0 top-[48px] z-50 bg-color-container-level-0" transition:slide>
+				<NavigationMenu ariaLabel="Main Menu" items={mainMenu}></NavigationMenu>
+			</div>
+		{/if}
 	</Header>
 
 	{@render children()}
