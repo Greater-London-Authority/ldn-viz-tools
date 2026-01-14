3a0fb0c832c0812b47d91d6e8e21efd1b7ffd2d4
Chris Knight <chris.knight@london.gov.uk>
2026-01-14 16:43:27 +0000

    Card component stub

---
+++ packages/ui/src/lib/card/Card.stories.svelte
@@ -0,0 +1,40 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Card from './Card.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Ui/Components - Layout And Themes/Card',
+		component: Card
+	});
+</script>
+
+<Story name="Default">
+	{#snippet template(args)}
+		<Card {...args} />
+	{/snippet}
+</Story>
+
+<Story name="With Label">
+	{#snippet template(args)}
+		<Card {...args} label="Label" />
+	{/snippet}
+</Story>
+
+<Story name="With Title">
+	{#snippet template(args)}
+		<Card {...args} title="Card title" />
+	{/snippet}
+</Story>
+
+<Story name="With Content">
+	{#snippet template(args)}
+		<Card {...args} title="Card title">
+			<p class="product responsive body-md">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non dignissim odio.
+				Morbi at sollicitudin magna, viverra dapibus libero. Etiam finibus malesuada mi, in
+				tristique nulla suscipit ut. In imperdiet leo nec faucibus luctus. Fusce nec mattis velit,
+				sed vestibulum dolor. Aenean semper tempus ipsum, at pulvinar quam posuere et.
+			</p>
+		</Card>
+	{/snippet}
+</Story>
---
+++ packages/ui/src/lib/card/Card.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import Overlay from '../overlay/Overlay.svelte';
+
+	interface CardProps {
+		title?: string;
+		label?: string;
+		overlayContent?: Snippet;
+		children: Snippet;
+	}
+
+	let { title, label, overlayContent, children }: CardProps = $props();
+</script>
+
+<div
+	class="border-color-ui-border-secondary bg-color-container-level-0 relative flex h-full flex-col justify-between border p-4 shadow-sm"
+>
+	<div class="flex items-center justify-between">
+		{#if label}
+			<h3 class="product responsive label-md text-color-text-secondary mb-typography-spacing-xs">
+				{label}
+			</h3>
+		{/if}
+
+		{#if title}
+			<h3 class="product responsive title-md mb-typography-spacing-xs">{title}</h3>
+		{/if}
+
+		<div class="absolute right-1 top-1">
+			<Overlay>
+				{@render overlayContent?.()}
+			</Overlay>
+		</div>
+	</div>
+	{@render children?.()}
+</div>
