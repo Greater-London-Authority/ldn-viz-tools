38043e2f111630b96a77709325090399d50eaba3
Halimah Manan <175839409+halimahLDN@users.noreply.github.com>
2026-01-28 15:51:43 +0000

    fixed: MapControlFullscreen's isFullscreen state updates when keyboard is used to exit fullscreen

--- packages/maps/src/lib/mapControlFullscreen/MapControlFullscreen.svelte
+++ packages/maps/src/lib/mapControlFullscreen/MapControlFullscreen.svelte
@@ -64,7 +64,7 @@
 		}
 	});
 
-	const handle = (event: UIEvent) => {
+	const handleClick = (event: UIEvent) => {
 		if (!$mapStore || !mode) {
 			return;
 		}
@@ -76,18 +76,29 @@
 			$mapStore.getCanvas().focus();
 		}
 	};
+
+	// Update isFullscreen state when user presses 'Esc' or 'F11' instead of clicking button
+	const handleEscape = (_event: Event) => {
+		const updateState = () => isFullscreen.set(false);
+
+		if (!document.fullscreenElement) {
+			updateState();
+		}
+	};
 </script>
+
+<svelte:document onfullscreenchange={handleEscape} />
 
 {#if mode}
 	<div
-		class="invisible flex flex-col space-y-1 shadow dark:border dark:border-color-ui-border-primary sm:visible"
+		class="dark:border-color-ui-border-primary invisible flex flex-col space-y-1 shadow sm:visible dark:border"
 	>
 		<Button
 			variant="square"
 			emphasis="secondary"
 			title={$isFullscreen ? mode.titleIn : mode.titleOut}
 			class="pointer-events-auto"
-			onclick={handle}
+			onclick={handleClick}
 		>
 			<Icon src={$isFullscreen ? mode.iconIn : mode.iconOut} class="h-8 w-8 p-1" />
 		</Button>
