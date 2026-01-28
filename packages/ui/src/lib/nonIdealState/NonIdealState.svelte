33e7f2b87f415173c57e34b9f21e61174463e71a
Chris Knight <chris.knight@london.gov.uk>
2026-01-28 13:21:12 +0000

    non ideal state

--- packages/ui/src/lib/nonIdealState/NonIdealState.svelte
+++ packages/ui/src/lib/nonIdealState/NonIdealState.svelte
@@ -26,7 +26,7 @@
 </script>
 
 <div
-	class="border-color-border-muted bg-color-surface-neutral text-color-text-muted flex h-full w-full flex-col items-center justify-center gap-2 border px-2 py-2 text-center"
+	class="border-color-border-muted bg-color-surface-neutral-muted text-color-text-muted flex h-full w-full flex-col items-center justify-center gap-2 border px-2 py-2 text-center"
 >
 	{#if spinner}
 		<LoadingIndicator class="h-12 w-12" />
