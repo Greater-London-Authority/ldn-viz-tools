56de83f730eb9a0b2c10656b3078f6288e1e5998
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2026-02-06 15:09:43 +0000

    allow all overlay types to be externally opened (#1267)
    * allow all overlay types to be externally opened

--- packages/ui/src/lib/overlay/Overlay.stories.svelte
+++ packages/ui/src/lib/overlay/Overlay.stories.svelte
@@ -5,12 +5,16 @@
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import { Cog6Tooth } from '@steeze-ui/heroicons';
 	import Button from '../button/Button.svelte';
+	import Select from '../select/Select.svelte';
 
 	const { Story } = defineMeta({
 		title: 'Ui/Components/Overlays/Overlay',
 		component: Overlay,
 		tags: ['autodocs']
 	});
+
+	let open = $state(false);
+	let type = $state('modal');
 </script>
 
 <Story name="Default">
@@ -131,5 +135,26 @@
 			{/snippet}
 			This is a popover.
 		</Overlay>
+	{/snippet}
+</Story>
+
+<Story name="Externally controlled">
+	{#snippet template()}
+		<div class="flex w-96 flex-col">
+			<span>Overlay is currently {open ? 'Open' : 'Closed'}</span>
+			<Button onclick={() => (open = !open)}>Toggle</Button>
+
+			<Select
+				label="Overlay type"
+				bind:value={type}
+				options={[
+					{ value: 'modal', label: 'Modal' },
+					{ value: 'tooltip', label: 'Tooltip' },
+					{ value: 'popover', label: 'Popover' }
+				]}
+			/>
+		</div>
+
+		<Overlay overlayType={type} bind:open>This is an overlay.</Overlay>
 	{/snippet}
 </Story>
