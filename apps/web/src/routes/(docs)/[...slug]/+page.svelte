4c459a54618508dabc261b0f9b9238fcff9e32e8
Chris Knight <chris.knight@london.gov.uk>
2026-03-17 09:00:42 +0000

    adjust layout and add banner graphic

--- apps/web/src/routes/(docs)/[...slug]/+page.svelte
+++ apps/web/src/routes/(docs)/[...slug]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import BannerHeader from '$lib/components/bannerHeader/BannerHeader.svelte';
 	import Toc from '$lib/components/toc/Toc.svelte';
 
 	let { data } = $props();
@@ -8,15 +9,21 @@
 </script>
 
 <!-- TODO: adding container her is of course restrictive to items that may want to break out of the column -->
-<div class="container prose responsive p-0">
-	<div class="my-typography-spacing-xl">
-		<p class="mb-0.5 text-xs uppercase tracking-widest text-color-surface-accent">Design system</p>
-		<h1 class="headline format">{data.metadata.title}</h1>
-		<p class="subhead format">
-			{data.metadata.description}
-		</p>
-	</div>
-	<PageComponent />
-</div>
 
-<Toc toc={{ items: tocItems }} />
+<!-- <div class="my-typography-spacing-xl">
+	<p class="mb-0.5 text-xs uppercase tracking-widest text-color-surface-accent">Design system</p>
+	<h1 class="headline format">{data.metadata.title}</h1>
+	<p class="subhead format">
+		{data.metadata.description}
+	</p>
+</div> -->
+<BannerHeader title={data.metadata.title} subTitle={data.metadata.description} />
+<!-- <div class="container flex p-0"> -->
+<div class="container mx-auto">
+	<div class="flex space-x-24">
+		<div class="prose responsive w-full">
+			<PageComponent />
+		</div>
+		<Toc toc={{ items: tocItems }} />
+	</div>
+</div>
