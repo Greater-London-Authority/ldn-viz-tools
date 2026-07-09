84f3cfeec8e28f2aacd0e799267dabecaabccabc
James Scott-Brown <james.scott-brown@london.gov.uk>
2026-07-09 15:52:56 +0100

    auto-format with prettier

--- packages/ui/src/lib/breadcrumbs/Breadcrumbs.svelte
+++ packages/ui/src/lib/breadcrumbs/Breadcrumbs.svelte
@@ -26,7 +26,7 @@
 </script>
 
 {#if breadcrumbs && breadcrumbs.length > 0}
-	<div class="text-color-text-muted py-2 text-sm">
+	<div class="py-2 text-sm text-color-text-muted">
 		{#each breadcrumbs as c, i (c.label)}
 			{#if i == breadcrumbs.length - 1}
 				<span class="text-color-text">
