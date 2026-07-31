6cf8f1578b104d736216fa2854cee69fb0ac9bda
Chris Knight <chris.knight@london.gov.uk>
2026-07-31 12:13:42 +0100

    upgrade storybook

--- apps/storybook/eslint.config.js
+++ apps/storybook/eslint.config.js
@@ -1,8 +1,8 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { config } from '@ldn-viz/config-eslint/index.js';
 
-export default [
-	...config,
-	{
-		ignores: ['.storybook/**', '.svelte-kit/**', 'build/**', 'storybook-static/**']
-	}
-];
+export default [...config, {
+    ignores: ['.storybook/**', '.svelte-kit/**', 'build/**', 'storybook-static/**']
+}, ...storybook.configs["flat/recommended"]];
