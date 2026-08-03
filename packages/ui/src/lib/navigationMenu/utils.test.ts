31d2c2c41522004b02dae8f5bdfc34ce2ae98a1f
James Scott-Brown <james.scott-brown@london.gov.uk>
2026-08-03 10:46:58 +0100

    update a test name

--- packages/ui/src/lib/navigationMenu/utils.test.ts
+++ packages/ui/src/lib/navigationMenu/utils.test.ts
@@ -45,7 +45,7 @@ describe('findActiveItem', () => {
 		expect(findActiveItem(undefined as any, pageWith('/a'), identity)).toBeUndefined();
 	});
 
-	it('does not select an item that has no href (documents current behaviour)', () => {
+	it('selects an item that has no href, if there is no more specific match', () => {
 		const hrefless: NavigationMenuItemProps[] = [{ title: 'Home' }];
 		const active = findActiveItem(hrefless, pageWith('/anything'), identity);
 		expect(active).toBe(hrefless[0]);
