1a94e3e4b7310cd96c2f9e5521740d0202cb53c7
Chris Knight <chris.knight@london.gov.uk>
2026-08-17 14:04:56 +0100

    update banner heights. Todo: refactor

--- apps/web/src/lib/components/bannerHeader/BannerHeader.svelte
+++ apps/web/src/lib/components/bannerHeader/BannerHeader.svelte
@@ -39,7 +39,7 @@
 	const darkImage = $derived(getImage(constructPath(src, '-dark')));
 </script>
 
-<div class="relative h-[300px] bg-color-canvas-muted">
+<div class="relative h-[294px] bg-color-canvas-muted">
 	{#key page.url.pathname}
 		<div class="absolute inset-0" transition:fade={{ duration: 200 }}>
 			{#if theme.currentMode === 'light' && lightImage}
@@ -54,7 +54,7 @@
 		</div>
 	{/key}
 
-	<div class="relative z-10 flex">
+	<div class="relative z-10 flex h-full items-center">
 		<div class="container mx-auto">
 			<div class="prose flow-prose">
 				<p class="headline pt-4">{title}</p>
