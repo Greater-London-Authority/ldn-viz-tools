25867512e48c461f317a1abd93b574a5b9b579e9
Chris Knight <chris.knight@london.gov.uk>
2026-08-21 08:35:15 +0100

    scaffold

---
+++ packages/maps/src/lib/mapRadiusSearch/MapRadiusSearch.stories.svelte
@@ -0,0 +1,31 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { default as Map } from '../map/Map.svelte';
+	import { appendOSKeyToUrl } from '../map/util';
+	import MapControlGroup from '../mapControlGroup/MapControlGroup.svelte';
+	import MapRadiusSearch from './MapRadiusSearch.svelte';
+
+	const OS_KEY = 'vmRzM4mAA1Ag0hkjGh1fhA2hNLEM6PYP';
+
+	const { Story } = defineMeta({
+		title: 'Maps/Components/MapRadiusSearch',
+		component: MapRadiusSearch,
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Default">
+	{#snippet template()}
+		<div class="h-[100dvh] w-[100dvw]">
+			<Map
+				options={{
+					transformRequest: appendOSKeyToUrl(OS_KEY)
+				}}
+			>
+				<MapControlGroup position="TopLeft">
+					<MapRadiusSearch />
+				</MapControlGroup>
+			</Map>
+		</div>
+	{/snippet}
+</Story>
---
+++ packages/maps/src/lib/mapRadiusSearch/MapRadiusSearch.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { Button, Input } from '@ldn-viz/ui';
+	import {
+		// Circle,
+		MapPinSimpleArea
+	} from '@steeze-ui/phosphor-icons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+
+	// Activate
+
+	const handleClick = () => {
+		console.log('clicked');
+
+		// Deactivate tooltips
+		// Set cursor to cross hairs
+	};
+
+	// Drop a pin
+
+	// Move map to pin
+
+	// Extend circle out from pin
+
+	// Tie Radius to range slider
+
+	// Display Radius in M2
+
+	// Debounce callbacks/ Submit on done
+
+	//----//
+
+	let radiusValue = $state('5');
+</script>
+
+<Button
+	variant="square"
+	emphasis="secondary"
+	title="Radius Search"
+	class="pointer-events-auto"
+	onclick={handleClick}
+>
+	<Icon src={MapPinSimpleArea} class="h-8 w-8 p-1" />
+</Button>
+
+<div class="pointer-events-auto bg-color-container p-2">
+	<p>TODO: Radius feedback here</p>
+
+	<Input type="range" label="Radius" name="searchRadius" bind:value={radiusValue} />
+
+	<Input type="text" label="Radius M<sup>2</sup>" bind:value={radiusValue}></Input>
+</div>
